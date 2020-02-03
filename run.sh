7cae07316628597d1f7c864b560ecfc830254ee9
Rick Lamers <ricklamers@gmail.com>
2020-02-03 09:28:26 -0800

    updated run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@ source venv/bin/activate
 
 gnome-terminal -- "./run_orchest_api.sh"
 
+sensible-browser http://127.0.0.1:8000 &> /dev/null &
+
 ## Run orchest web interface
 cd /home/rick/workspace/orchest/orchest/webserver
 python app.py
-
