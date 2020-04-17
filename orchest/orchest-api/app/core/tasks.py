7cbfeb120315729cb35e186eee03a8fb622dc329
Yannick Perrenet <yannick.perrenet@gmail.com>
2020-04-17 13:34:45 +0200

    Initial cleanup of comments and variable names

--- orchest/orchest-api/app/core/tasks.py
+++ orchest/orchest-api/app/core/tasks.py
@@ -11,52 +11,77 @@ def add(x, y):
 
 
 # @celery.task
-def run_partial(uuids, type_of_run, pipeline_description):
+def run_partial(uuids, run_type, pipeline_description):
     """Runs a pipeline partially.
+
+    NOTE:
+        Running a pipeline fully can also be described as a partial run.
 
     Args:
         uuids (iterable): sequence of pipeline step UUIDS.
-        type_of_run (str): one of ("full", "selection", "incoming").
+        run_type (str): one of ("full", "selection", "incoming").
         pipeline_description (dict): json describing pipeline.
     """
-    # TODO: this function does not yet exist. It should return the graph
-    #       to be run given by the uuids, type of run and pipeline
-    #       description.
-    pipeline = get_pipeline_to_execute(uuids, type_of_run, pipeline_description)
+    # Get the pipeline to run according to the run_type.
+    pipeline = construct_pipeline(uuids, run_type, pipeline_description)
 
     # Run the subgraph in parallel.
-    asyncio.run(pipeline.execute())
+    asyncio.run(pipeline.run())
 
 
-def get_pipeline_to_execute(uuids, type_of_run, pipeline_description):
-    # Idea: use getattr to call the function we want.
-    # pipeline = Pipeline.from_json(pipeline_description)
-    # sub_pipeline = getattr(pipeline, type_of_run)(pipeline, uuids)
+def construct_pipeline(uuids, run_type, pipeline_description, config=None):
+    """
+    Kwargs:
+        config (dict or None): configuration for the run_type. Do you
+            want to wait on container completion, or do you want to
+            include the selection uuids when it calls incoming?
+    """
+    # Create a pipeline from the pipeline_description. And run the
+    # appropriate method based on the run_type.
+    pipeline = Pipeline.from_json(pipeline_description)
 
-    if type_of_run == 'full':
-        return Pipeline.from_json(pipeline_description)
+    if run_type == 'full':
+        return pipeline
 
-    if type_of_run == 'selection':
-        # Remove all steps from the pipeline, that are not including in
-        # the uuids iterable.
+    if run_type == 'selection':
+        return pipeline.get_subgraph(uuids)
 
-        # It is done like this because if a--> b is selected then the
-        # order should be kept in mind. We cannot simply execute a and
-        # b as if they are independent.
-        return Pipeline.from_json(pipeline_description).keep(uuids)
-
-    if type_of_run == 'incoming':
-        return Pipeline.from_json(pipeline_description).incoming(uuids)
+    if run_type == 'incoming':
+        return pipeline.incoming(uuids, inclusive=False)
 
 
-# OUTSIDE: allows for a selection of one step, to create the step and
-# execute it.
 class PipelineStep:
     def __init__(self, properties: dict, parents: list = None):
+        """
+            properties: properties of the step. See example description
+                in README.
+        """
         self.properties = properties
         self.parents = parents if parents is not None else []
 
+        # Keeping a list of children allows us to traverse the pipeline
+        # also in the other direction. This is helpful for certain
+        # Pipeline methods.
         self._children = []
+
+    async def run_self(self, docker, wait_on_completion=True):
+        config = {'Image': self.properties['image']['image_name']}
+
+        # Starts the container asynchronously, however, it does not wait
+        # for completion of the container (like the `docker run` CLI
+        # command does).
+        container = await docker.containers.run(config=config)
+
+        if wait_on_completion:
+            await container.wait()
+
+    async def run(self, docker):
+        # Before running the step itself, it has to run all incoming
+        # steps.
+        tasks = [asyncio.create_task(parent.run()) for parent in self.parents]
+        await asyncio.gather(*tasks)
+
+        await self.run_self(docker)
 
     def __eg__(self, other):
         return self.properties['uuid'] == other.properties['uuid']
@@ -78,40 +103,36 @@ class PipelineStep:
 
         return f'Pipelinestep(None)'
 
-    async def execute_self(self, docker, wait_on_completion=True):
-        config = {'Image': self.properties['image']['image_name']}
-
-        container = await docker.containers.run(config=config)
-
-        if wait_on_completion:
-            await container.wait()
-
-    async def execute(self, docker):
-        tasks = [asyncio.create_task(parent.execute()) for parent in self.parents]
-        await asyncio.gather(*tasks)
-
-        await self.execute_self(docker)
-
 
 class Pipeline:
     def __init__(self, steps=None):
         self.steps = steps
 
-        # TODO: when initializing from file, set the sentinel.
+        # Similarly to the implementation of a DLL, we add a sentinel node
+        # to the end of the pipeline (i.e. all steps that do not have
+        # children will be connected to the sentinel node). By having a
+        # pointer to the sentinel we can traverse the entire pipeline.
+        # This way we can start a run by "running" the sentinel node.
         self._sentinel: PipelineStep = None
 
     @classmethod
     def from_json(cls, description):
-        """
+        """Constructs a pipeline from a json-like description.
+
+        This is an alternative constructur the the __init__().
+
         Args:
             description (dict): json description of Pipeline.
+
+        Returns:
+            A pipeline object defined by the given description.
         """
-        # Can be thought of as an alternative constructor
-        steps = {uuid: PipelineStep(properties) for uuid, properties in description['steps'].items()}
+        # Create a mapping for all the steps from UUID to object.
+        steps = {uuid: PipelineStep(properties)
+                 for uuid, properties in description['steps'].items()}
 
-        for _, step in steps.items():
-            step.parents = [steps[uuid] for uuid in step.properties['incoming_connections']]
-
+        # For every step populate its parents and _children attributes.
+        for step in steps.values():
             for uuid in step.properties['incoming_connections']:
                 step.parents.append(steps[uuid])
                 steps[uuid]._children.append(step)
@@ -120,50 +141,76 @@ class Pipeline:
 
     @property
     def sentinel(self):
+        """
+
+        TODO: description of sentinel
+        """
         if self._sentinel is None:
             self._sentinel = PipelineStep(None)
             self._sentinel.parents = [step for step in self.steps if not step._children]
 
         return self._sentinel
 
-    def keep(self, selection):
-        # NOTE: Modifies inplace!
+    def convert_to_subgraph(self, selection):
+        """Converts the pipeline to a subpipeline.
 
-        # TODO: the connection from nodes to non-existing nodes should be
-        #       removed.
-        keep_steps = [step for step in self.steps if step.properties['uuid'] in selection]
-        for step in keep_steps:
-            step.parents = [s for s in step.parents if s in keep_steps]
-            step._children = [s for s in step._children if s in keep_steps]
+        Takes an induced subgraph of the pipeline formed by a subset of
+        its steps given by the selection (of UUIDs).
 
-        self.steps = keep_steps
+        NOTE:
+            It is done like this because if a--> b is selected then the
+            order should be kept in mind. We cannot simply run a and b
+            as if they are independent.
+
+        NOTE: Modifies inplace!
+        """
+        self.steps = [step for step in self.steps if step.properties['uuid'] in selection]
+
+        # Removing connection from steps to "non-existing" steps, i.e.
+        # steps that are not included in the selection.
+        for step in self.steps:
+            step.parents = [s for s in step.parents if s in self.steps]
+            step._children = [s for s in step._children if s in self.steps]
 
     def get_subgraph(self, selection):
-        # NOTE: Same as .keep() but returns new Pipeline object.
+        """Returns a new pipeline whos set of steps equal the selection.
+
+        NOTE:
+            Exactly the same as convert_to_subgraph except that it returns
+            a new pipeline object instead of modifying it inplace.
+        """
         keep_steps = [step for step in self.steps if step.properties['uuid'] in selection]
+
+        # Only keep connection to parents and children if these steps are
+        # also included in the selection.
         new_steps = []
         for step in keep_steps:
             # TODO: I guess these properties point to the same object now.
-            new_step = PipelineStep(step.propeties)
+            #       This should be fine since the properties itself do not
+            #       get changed.
+            new_step = PipelineStep(step.properties)
             new_step.parents = [s for s in step.parents if s in keep_steps]
             new_step._children = [s for s in step._children if s in keep_steps]
             new_steps.append(new_step)
 
         return Pipeline(steps=new_steps)
 
-    def incoming(self, selection):
-        # TODO: take a--> b --> c where selection=[b, c]. Then b would
-        #       also be in the steps to be executed. Do we want this?
+    def incoming(self, selection, inclusive=False):
+        """
+        TODO: take a--> b --> c where selection=[b, c]. Then b would
+              also be in the steps to be executed. Do we want this?
+        """
 
-        # Traverses the Pipeline starting at the nodes given by selection.
-        # BFS starting at multiple nodes.
-
+        # This set will be populated with all the steps that are ancestors
+        # of the sets given by the selection. Depending on the kwarg
+        # "inclusive" the steps from the selection itself will either be
+        # included or excluded.
         steps = set()
 
-        # Get steps that are given by the selection.
+        # Essentially a BFS where its stack gets initialized with multiple
+        # root nodes.
         stack = [step for step in self.steps if step.properties['uuid'] in selection]
 
-        # Get all the parents of all the steps in the selection.
         while stack:
             step = stack.pop()
             if step in steps:
@@ -172,14 +219,17 @@ class Pipeline:
             steps.add(step)
             stack.extend(step.parents)
 
-        # Note that the nodes in the selection are now also run.
+        if not inclusive:
+            steps = [step for step in steps if step.properties['uuid'] not in selection]
+
         return Pipeline(steps=list(steps))
 
-    async def execute(self):
+    async def run(self):
         # TODO: do we want to put this docker instance also inside the
-        #       connections file? Similar to the standard sdk instance?
+        #       connections.py file? Similar to the standard docker
+        #       sdk instance?
         docker = aiodocker.Docker()
-        await self.sentinel.execute(docker)
+        await self.sentinel.run(docker)
         await docker.close()
 
     def __repr__(self):
--- orchest/orchest-api/app/core/test_tasks.py
+++ orchest/orchest-api/app/core/test_tasks.py
@@ -1,22 +1,40 @@
-import json
-
 from tasks import Pipeline
 
 
-def main():
-    uuids = {
-        'Step 1': '6338e764-c862-4c83-adf3-43a8299b3e32',
-        'Step 2': '1eb19c11-52c6-418f-80cf-96b4ca4ebb83'
+def create_pipeline_json():
+    """
+        1 -- > 2
+    """
+    pipeline = {
+        'name': 'pipeline-name',
+        'steps': {
+            'uuid-1': {
+                'incoming_connections': [],
+                'name': 'step-1',
+                'uuid': 'uuid-1'
+            },
+            'uuid-2': {
+                'incoming_connections': ['uuid-1'],
+                'name': 'step-2',
+                'uuid': 'uuid-2'
+            }
+        }
     }
-    with open('pipeline.json', 'r') as f:
-        description = json.load(f)
+    return pipeline
+
+
+def main():
+    description = create_pipeline_json()
 
     pipeline = Pipeline.from_json(description)
 
     print(pipeline.sentinel)
 
-    print(pipeline.incoming(uuids['Step 1']))
-    print(pipeline.incoming(uuids['Step 2']))
+    print(pipeline.incoming('uuid-1'))
+    print(pipeline.incoming('uuid-2'))
+
+    print()
+    print(pipeline.get_subgraph(['uuid-2']))
 
 
 if __name__ == '__main__':
