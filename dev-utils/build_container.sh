217b5e05eb888ca342e131018d10ce46f28e07a6
Rick Lamers <ricklamers@gmail.com>
2020-05-26 18:20:16 +0200

    build script now accepts container name for selective build & fixed nginx proxy url encoded pass

--- dev-utils/build_container.sh
+++ dev-utils/build_container.sh
@@ -1,42 +1,61 @@
 #!/bin/bash
 
-# JupyterLab server
-docker build -t orchestsoftware/jupyter-server ../orchest/jupyter-server
+# # JupyterLab server
+if [[ ($1 == "jupyter-server") || ! $1 ]]; then
+    docker build -t orchestsoftware/jupyter-server ../orchest/jupyter-server
+fi
 
-docker build -t orchestsoftware/celery-worker -f ../orchest/orchest-api/Dockerfile_celery ../orchest/orchest-api
+if [[ ($1 == "celery-worker") || ! $1 ]]; then
+    docker build -t orchestsoftware/celery-worker -f ../orchest/orchest-api/Dockerfile_celery ../orchest/orchest-api
+fi
 
 # augmented images
-docker build -t orchestsoftware/scipy-notebook-augmented \
-    ../orchest/custom-images/scipy-notebook-augmented
+if [[ ($1 == "scipy-notebook-augmented") || ! $1 ]]; then
+    docker build -t orchestsoftware/scipy-notebook-augmented \
+        ../orchest/custom-images/scipy-notebook-augmented
+fi
 
-docker build -t orchestsoftware/r-notebook-augmented \
-    ../orchest/custom-images/r-notebook-augmented
+if [[ ($1 == "r-notebook-augmented") || ! $1 ]]; then
+    docker build -t orchestsoftware/r-notebook-augmented \
+        ../orchest/custom-images/r-notebook-augmented
+fi
 
 # runnable images
-docker build -t orchestsoftware/scipy-notebook-runnable \
-    -f ../orchest/custom-images/runnable-images/scipy-notebook-runnable/Dockerfile ../orchest/custom-images/runnable-images
+if [[ ($1 == "scipy-notebook-runnable") || ! $1 ]]; then
+    docker build -t orchestsoftware/scipy-notebook-runnable \
+        -f ../orchest/custom-images/runnable-images/scipy-notebook-runnable/Dockerfile ../orchest/custom-images/runnable-images
+fi
 
-docker build -t orchestsoftware/r-notebook-runnable \
-    -f ../orchest/custom-images/runnable-images/r-notebook-runnable/Dockerfile ../orchest/custom-images/runnable-images
+if [[ ($1 == "r-notebook-runnable") || ! $1 ]]; then
+    docker build -t orchestsoftware/r-notebook-runnable \
+        -f ../orchest/custom-images/runnable-images/r-notebook-runnable/Dockerfile ../orchest/custom-images/runnable-images
+fi
 
 # custom enterprise gateway kernel images
-docker build -t orchestsoftware/custom-base-kernel-py \
-    -f ../orchest/custom-images/custom-base-kernel-py/Dockerfile ../orchest/custom-images/scipy-notebook-augmented
+if [[ ($1 == "custom-base-kernel-py") || ! $1 ]]; then
+    docker build -t orchestsoftware/custom-base-kernel-py \
+        -f ../orchest/custom-images/custom-base-kernel-py/Dockerfile ../orchest/custom-images/scipy-notebook-augmented
+fi
 
-docker build -t orchestsoftware/custom-base-kernel-r \
-    -f ../orchest/custom-images/custom-base-kernel-r/Dockerfile ../orchest/custom-images/r-notebook-augmented
+if [[ ($1 == "custom-base-kernel-r") || ! $1 ]]; then
+    docker build -t orchestsoftware/custom-base-kernel-r \
+        -f ../orchest/custom-images/custom-base-kernel-r/Dockerfile ../orchest/custom-images/r-notebook-augmented
+fi
 
 # application images
-docker build -t orchestsoftware/orchest-api ../orchest/orchest-api/
 
-docker build -t orchestsoftware/orchest-ctl ../orchest/orchest-ctl/
+if [[ ($1 == "orchest-api") || ! $1 ]]; then
+    docker build -t orchestsoftware/orchest-api ../orchest/orchest-api/
+fi
 
-docker build -t orchestsoftware/orchest-webserver ../orchest/orchest-webserver/
+if [[ ($1 == "orchest-ctl") || ! $1 ]]; then
+    docker build -t orchestsoftware/orchest-ctl ../orchest/orchest-ctl/
+fi
 
-docker build -t orchestsoftware/nginx-proxy ../orchest/nginx-proxy/
+if [[ ($1 == "orchest-webserver") || ! $1 ]]; then
+    docker build -t orchestsoftware/orchest-webserver ../orchest/orchest-webserver/
+fi
 
-# RabbitMQ
-docker pull rabbitmq:3
-
-# Enterprise Gateway used for docker based kernels in JupyterLab
-docker pull elyra/enterprise-gateway:2.1.1
+if [[ ($1 == "nginx-proxy") || ! $1 ]]; then
+    docker build -t orchestsoftware/nginx-proxy ../orchest/nginx-proxy/
+fi
