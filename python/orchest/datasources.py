e6a144934da8a87fb353ac3b1801a1d3a3a42acc
Rick Lamers <ricklamers@gmail.com>
2020-06-26 16:59:12 +0200

    add remaining data sources

--- python/orchest/datasources.py
+++ python/orchest/datasources.py
@@ -1,10 +1,15 @@
 import requests
+import boto3
+import os
 
 from requests.exceptions import HTTPError
 from sqlalchemy import create_engine
 
 
 class DataSource():
+
+    connection_timeout = 5
+
     def __init__(self):
         pass
 
@@ -15,6 +20,12 @@ class DataSource():
             return HostDirectoryDataSource(datasource_json)
         elif datasource_json["source_type"] == "database-mysql":
             return MySQLDataSource(datasource_json)
+        elif datasource_json["source_type"] == "database-postgresql":
+            return PostgreSQLDataSource(datasource_json)
+        elif datasource_json["source_type"] == "database-aws-redshift":
+            return AWSRedshiftDataSource(datasource_json)
+        elif datasource_json["source_type"] == "objectstorage-aws-s3":
+            return AWSObjectStorageS3(datasource_json)
 
 
 class HostDirectoryDataSource(DataSource):
@@ -36,7 +47,9 @@ class MySQLDataSource(DataSource):
             connection_details["database_name"]
         )
 
-        self.engine = create_engine(self.connection_string)
+        self.engine = create_engine(self.connection_string, 
+            connect_args={'connect_timeout': self.connection_timeout})
+
         self.connection = self.engine.connect()
 
 
@@ -46,6 +59,73 @@ class MySQLDataSource(DataSource):
         except Exception as e:
             print(e)
 
+
+class PostgreSQLDataSource(DataSource):
+
+    def __init__(self, data):
+
+        connection_details = data["connection_details"]
+
+        self.connection_string = "postgresql://%s:%s@%s/%s" % (
+            connection_details["username"],
+            connection_details["password"],
+            connection_details["host"],
+            connection_details["database_name"]
+        )
+
+        self.engine = create_engine(self.connection_string,
+            connect_args={'connect_timeout': self.connection_timeout})
+
+        self.connection = self.engine.connect()
+
+
+    def __del__(self):
+        try:
+            self.connection.close()
+        except Exception as e:
+            print(e)
+
+
+class AWSRedshiftDataSource(DataSource):
+
+    def __init__(self, data):
+
+        connection_details = data["connection_details"]
+
+        self.connection_string = "redshift+psycopg2://%s:%s@%s/%s" % (
+            connection_details["username"],
+            connection_details["password"],
+            connection_details["host"],
+            connection_details["database_name"]
+        )
+
+        self.engine = create_engine(self.connection_string,
+            connect_args={'connect_timeout': self.connection_timeout})
+
+        self.connection = self.engine.connect()
+
+
+    def __del__(self):
+        try:
+            self.connection.close()
+        except Exception as e:
+            print(e)
+
+
+class AWSObjectStorageS3(DataSource):
+
+    def __init__(self, data):
+
+        self.s3 = boto3.resource('s3', 
+            aws_access_key_id=data["connection_details"]["access_key"],
+            aws_secret_access_key=data["connection_details"]["secret_key"])
+
+        self.client = boto3.client('s3',
+            aws_access_key_id=data["connection_details"]["access_key"],
+            aws_secret_access_key=data["connection_details"]["secret_key"])
+
+        self.bucket = self.s3.Bucket(data["connection_details"]["bucket"])
+    
 
 def get(name):
 
