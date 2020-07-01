1654fd4005340180d9a9f16de7c314141eb62c50
Yannick <yannick.perrenet@gmail.com>
2020-07-01 20:16:52 +0200

    Create Dockerfile

--- orchest/memory-store/app/config.py
+++ orchest/memory-store/app/config.py
@@ -1,3 +1,5 @@
 PIPELINE_DESCRIPTION_FILE = 'pipeline.json'
-STORE_MEMORY = 1000000000  # 1 GB
 STORE_SOCKET_NAME = '/tmp/plasma.sock'
+
+STORE_MEMORY = 60397977  # default max by docker
+# STORE_MEMORY = 1000000000  # 1 GB
