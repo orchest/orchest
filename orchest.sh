167e17eadf4ba6c2934a8eca520ae9a0fdf15139
Rick Lamers <ricklamers@gmail.com>
2020-07-17 11:27:44 +0200

    orchest.sh quote fix

--- orchest.sh
+++ orchest.sh
@@ -8,5 +8,5 @@ mkdir -p $HOST_CONFIG_DIR
 
 # orchest-ctl needs to bind to /userdir to make the duable queue directory automatically
 docker run --name orchest-ctl --rm \
-    -v /var/run/docker.sock:/var/run/docker.sock -e HOST_CONFIG_DIR="$HOST_CONFIG_DIR \
+    -v /var/run/docker.sock:/var/run/docker.sock -e HOST_CONFIG_DIR=$HOST_CONFIG_DIR \
     -e HOST_PWD=$PWD -e HOST_USER_DIR=$HOST_USER_DIR orchestsoftware/orchest-ctl "$@"
