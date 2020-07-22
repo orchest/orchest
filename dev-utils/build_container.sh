39627b1bcd4460f9059059dbfdb6c6d0926931fb
Yannick <yannick.perrenet@gmail.com>
2020-07-22 11:08:10 +0200

    Rewrite dockerfiles for broader context

--- dev-utils/build_container.sh
+++ dev-utils/build_container.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Use another Docker backend for building.
+export DOCKER_BUILDKIT=1
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 IMGS=()
 SDK_BRANCH="master"
@@ -77,17 +80,18 @@ do
         build=(docker build \
             -t orchestsoftware/jupyter-server \
             --no-cache=$NO_CACHE \
-            $DIR/../orchest/jupyter-server)
-        
+            -f $DIR/../orchest/jupyter-server/Dockerfile \
+            $DIR/../)
+
     fi
 
     if [ $IMG == "celery-worker" ]; then
-        
+
         build=(docker build \
             -t orchestsoftware/celery-worker \
-            -f $DIR/../orchest/orchest-api/Dockerfile_celery \
             --no-cache=$NO_CACHE \
-            $DIR/../orchest/orchest-api)
+            -f $DIR/../orchest/orchest-api/Dockerfile_celery \
+            $DIR/../)
 
     fi
 
@@ -98,7 +102,8 @@ do
             -t orchestsoftware/scipy-notebook-augmented \
             --build-arg sdk_branch=$SDK_BRANCH \
             --no-cache=$NO_CACHE \
-            $DIR/../orchest/custom-images/scipy-notebook-augmented)
+            -f $DIR/../orchest/custom-images/scipy-notebook-augmented/Dockerfile \
+            $DIR/../)
     fi
 
     if [ $IMG == "r-notebook-augmented" ]; then
@@ -106,7 +111,8 @@ do
             -t orchestsoftware/r-notebook-augmented \
             --build-arg sdk_branch=$SDK_BRANCH \
             --no-cache=$NO_CACHE \
-            $DIR/../orchest/custom-images/r-notebook-augmented)
+            -f $DIR/../orchest/custom-images/r-notebook-augmented/Dockerfile \
+            $DIR/../)
     fi
 
     # runnable images
@@ -114,14 +120,14 @@ do
         build=(docker build -t orchestsoftware/scipy-notebook-runnable \
             -f $DIR/../orchest/custom-images/runnable-images/scipy-notebook-runnable/Dockerfile \
             --no-cache=$NO_CACHE \
-            $DIR/../orchest/custom-images/runnable-images)
+            $DIR/../)
     fi
 
     if [ $IMG == "r-notebook-runnable" ]; then
         build=(docker build -t orchestsoftware/r-notebook-runnable \
             -f $DIR/../orchest/custom-images/runnable-images/r-notebook-runnable/Dockerfile \
             --no-cache=$NO_CACHE \
-            $DIR/../orchest/custom-images/runnable-images)
+            $DIR/../)
     fi
 
     # custom enterprise gateway kernel images
@@ -132,7 +138,7 @@ do
             -f $DIR/../orchest/custom-images/custom-base-kernel-py/Dockerfile \
             --build-arg sdk_branch=$SDK_BRANCH \
             --no-cache=$NO_CACHE \
-            $DIR/../orchest/custom-images/scipy-notebook-augmented)
+            $DIR/../)
     fi
 
     if [ $IMG == "custom-base-kernel-r" ]; then
@@ -141,7 +147,7 @@ do
             -f $DIR/../orchest/custom-images/custom-base-kernel-r/Dockerfile \
             --build-arg sdk_branch=$SDK_BRANCH \
             --no-cache=$NO_CACHE \
-            $DIR/../orchest/custom-images/r-notebook-augmented)
+            $DIR/../)
     fi
 
     # application images
@@ -150,28 +156,32 @@ do
         build=(docker build \
             -t orchestsoftware/orchest-api \
             --no-cache=$NO_CACHE \
-            $DIR/../orchest/orchest-api/)
+            -f $DIR/../orchest/orchest-api/Dockerfile \
+            $DIR/../)
     fi
 
     if [ $IMG == "orchest-ctl" ]; then
         build=(docker build \
             -t orchestsoftware/orchest-ctl \
             --no-cache=$NO_CACHE \
-            $DIR/../orchest/orchest-ctl/)
+            -f $DIR/../orchest/orchest-ctl/Dockerfile \
+            $DIR/../)
     fi
 
     if [ $IMG == "orchest-webserver" ]; then
         build=(docker build \
             -t orchestsoftware/orchest-webserver \
             --no-cache=$NO_CACHE \
-            $DIR/../orchest/orchest-webserver/)
+            -f $DIR/../orchest/orchest-webserver/Dockerfile \
+            $DIR/../)
     fi
 
     if [ $IMG == "nginx-proxy" ]; then
         build=(docker build \
             -t orchestsoftware/nginx-proxy \
             --no-cache=$NO_CACHE \
-            $DIR/../orchest/nginx-proxy/)
+            -f $DIR/../orchest/nginx-proxy/Dockerfile \
+            $DIR/../)
     fi
 
     # installs orchest-sdk
@@ -180,7 +190,8 @@ do
             -t orchestsoftware/memory-server \
             --build-arg sdk_branch=$SDK_BRANCH \
             --no-cache=$NO_CACHE \
-            $DIR/../orchest/memory-server/)
+            -f $DIR/../orchest/memory-server/Dockerfile \
+            $DIR/../)
     fi
 
     if [ -n "$build" ]; then
