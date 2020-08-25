f7508b87e01522609853a3473c66a6ddde3f1aaf
Yannick <yannick.perrenet@gmail.com>
2020-08-25 11:44:15 +0200

    Add flags to run_tests without creating virtualenvs

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# To display help run this script with the "--help" option.
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 SERVICES=()
 TRACEBACK="line"
 
-while getopts "s:t" opt; do
+# When running this script on GitHub Actions, we do not have to create a
+# virtualenv to install the dependencies in.
+USE_VENV=true
+
+while getopts "s:t-:" opt; do
   case ${opt} in
     s)
       SERVICES+=($OPTARG)
@@ -12,11 +18,35 @@ while getopts "s:t" opt; do
     t)
       TRACEBACK="auto"
       ;;
+    -)
+      if [ $OPTARG == "no-venv" ]; then
+          USE_VENV=false
+      fi
+      if [ $OPTARG == "help" ]; then
+          echo "Usage:"
+          echo "--help        Display help."
+          echo "--no-venv     Run without virtualenv."
+          echo "-s            Run test for a specific service."
+          echo "-t            Set --tb=auto for pytest."
+          exit 0
+      fi
+      ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
       ;;
   esac
 done
+
+# If no services are specified, then we want to run the tests for all of
+# them.
+if [ ${#SERVICES[@]} -eq 0 ]; then
+    SERVICES=(
+        "jupyter-server"
+        "orchest-api"
+        "memory-server"
+    )
+fi
+
 
 VENVS_DIR=$DIR/../.venvs
 mkdir -p $VENVS_DIR
@@ -25,34 +55,42 @@ mkdir -p $VENVS_DIR
 for SERVICE in ${SERVICES[@]}
 do
     VENV="$VENVS_DIR/$SERVICE"
-    if [ ! -d $VENV ]; then
+    if [ ! -d $VENV -a $USE_VENV ]; then
         echo "[$SERVICE]: Creating virtualenv..."
         # TODO: python3 should map to 3.7 specifically?
         virtualenv -p python3 "$VENV" > /dev/null 2>&1
     fi
 
-    source $VENV/bin/activate
+    if [ $USE_VENV ]; then
+        source $VENV/bin/activate
+    fi
 
-    # Install requirements.txt and run tests.
+    # Install requirements.txt
     echo "[$SERVICE]: Installing dependencies..."
 
     if [ $SERVICE == "jupyter-server" ]; then
-        cd $DIR/../orchest/jupyter-server/app
-        pip install -r requirements.txt pytest > /dev/null 2>&1
-        python -m pytest -v --disable-warnings --tb=$TRACEBACK
+        REQ_DIR=$DIR/../orchest/jupyter-server/app
+        TEST_DIR=$REQ_DIR
     fi
     if [ $SERVICE == "orchest-api" ]; then
-        cd $DIR/../orchest/orchest-api/app
-        pip install -r requirements.txt pytest > /dev/null 2>&1
-        python -m pytest -v --disable-warnings --tb=$TRACEBACK
+        REQ_DIR=$DIR/../orchest/orchest-api/app
+        TEST_DIR=$REQ_DIR
     fi
     if [ $SERVICE == "memory-server" ]; then
-        cd $DIR/../orchest/memory-server
-        pip install -r requirements.txt pytest > /dev/null 2>&1
-        python -m pytest -v --disable-warnings --tb=$TRACEBACK
+        REQ_DIR=$DIR/../orchest/memory-server
+        TEST_DIR=$REQ_DIR
     fi
 
+    cd $REQ_DIR
+    pip install -r requirements.txt pytest > /dev/null 2>&1
+
+    # Run tests.
+    cd $TEST_DIR
+    python -m pytest -v --disable-warnings --tb=$TRACEBACK
+
     # Deactivate the virtualenv.
-    deactivate
+    if [ $USE_VENV ]; then
+        deactivate
+    fi
     echo
 done
