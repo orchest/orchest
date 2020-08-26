e4552bd1fa9d7235f9c03381312c43ebfd24b1cd
Yannick <yannick.perrenet@gmail.com>
2020-08-26 18:59:18 +0200

    Fix security for github Actions when login to dockerhub

--- scripts/push_container.sh
+++ scripts/push_container.sh
@@ -16,7 +16,7 @@ while getopts "s:i:nve" opt; do
 done
 
 # Login to be able to push to DockerHub
-docker login --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password--stdin
 
 for IMG in ${IMGS[@]}
 do
@@ -25,20 +25,3 @@ done
 
 # TODO: remove .docker/config.json as it will store the password or does
 # GitHub Actions take care of this for us?
-
-# docker push orchestsoftware/jupyter-server
-
-# docker push orchestsoftware/celery-worker
-# docker push orchestsoftware/memory-server
-
-# # custom enterprise gateway kernel images
-# docker push orchestsoftware/orchest-ctl
-# docker push orchestsoftware/custom-base-kernel-py
-# docker push orchestsoftware/custom-base-kernel-r
-
-# # application images
-# docker push orchestsoftware/orchest-api
-# docker push orchestsoftware/auth-server
-# docker push orchestsoftware/orchest-webserver
-
-# docker push orchestsoftware/nginx-proxy
