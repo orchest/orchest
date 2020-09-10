fd5ead77795916b9c4437ba659c0bca5ad585457
Yannick <yannick.perrenet@gmail.com>
2020-09-10 12:22:20 +0200

    Initial restructure of application logic in orchest-ctl

---
+++ orchest/orchest-ctl/app/cmdline.py
@@ -0,0 +1,187 @@
+"""Options for the command line."""
+import logging
+
+from docker.types import Mount
+
+from config import (
+    DOCKER_NETWORK,
+    CONTAINER_MAPPING,
+    RUN_MODE,
+    ALL_IMAGES,
+)
+from connections import docker_client
+from utils import (
+    is_install_complete,
+    install_images,
+    install_network,
+    log_server_url,
+    clean_containers,
+    dev_mount_inject,
+)
+
+
+def get_available_cmds():
+    cmds = ["start", "help", "stop", "status", "update"]
+    return cmds
+
+
+def start():
+    logging.info("Starting Orchest...")
+
+    # TODO: put this in the right spot
+    if RUN_MODE == "dev":
+        dev_mount_inject(CONTAINER_MAPPING)
+
+    # check if all images are present
+    if is_install_complete():
+        # start by cleaning up old containers lingering
+        clean_containers()
+
+        # start containers from CONTAINER_MAPPING that haven't started
+        running_containers = docker_client.containers.list()
+
+        # NOTE: is the repo tag always the first tag in the Docker Engine API?
+        running_container_images = [
+            running_container.image.tags[0]
+            for running_container in running_containers
+            if len(running_container.image.tags) > 0
+        ]
+
+        images_to_start = [
+            image_name
+            for image_name in CONTAINER_MAPPING.keys()
+            if image_name not in running_container_images
+        ]
+
+        for container_image in CONTAINER_MAPPING.keys():
+
+            if container_image in images_to_start:
+
+                container_spec = CONTAINER_MAPPING[container_image]
+
+                mounts = []
+                if 'mounts' in container_spec:
+                    mounts = [
+                        Mount(
+                            target=mount['target'],
+                            source=mount['source'],
+                            type='bind'
+                        )
+                        for mount in container_spec['mounts']
+                    ]
+
+                environment = container_spec.get('environment', {})
+                ports = container_spec.get('ports', {})
+                hostname = container_spec.get('hostname')
+                command = container_spec.get('command')
+
+                logging.info("Starting image %s" % container_image)
+
+                docker_client.containers.run(
+                    image=container_image,
+                    command=command,
+                    name=container_spec['name'],
+                    detach=True,
+                    mounts=mounts,
+                    network=DOCKER_NETWORK,
+                    environment=environment,
+                    ports=ports,
+                    hostname=hostname,
+                )
+
+        log_server_url()
+
+    else:
+        logging.info("Installation required. Starting installer.")
+        install_images()
+        install_network()
+        logging.info("Installation finished. Attempting to start...")
+        start()
+
+
+def help():
+    cmds = get_available_cmds()
+
+    help_msg = {
+        "start": "Starts the Orchest application",
+        "help": "Shows this help menu",
+        "stop": "Stops the Orchest application",
+        "status": "Checks the current status of the Orchest application",
+        "update": ("Update Orchest to the latest version by pulling latest "
+                   "container images"),
+    }
+
+    for cmd in cmds:
+        print("{0:20}\t {1}".format(cmd, help_msg[cmd]), flush=True)
+
+
+def stop():
+    # TODO: shutting down can be done easier by just shutting down all the
+    #       containers inside the "orchest" docker network.
+    # shut down containers
+    running_containers = docker_client.containers.list()
+
+    container_names = [
+        CONTAINER_MAPPING[container_key]['name']
+        for container_key in CONTAINER_MAPPING
+    ]
+
+    for running_container in running_containers:
+        if (len(running_container.image.tags) and
+                running_container.image.tags[0] in ALL_IMAGES):
+            # don't kill orchest-ctl itself
+            if running_container.image.tags[0] == "orchestsoftware/orchest-ctl:latest":
+                continue
+
+            logging.info("Killing container %s" % running_container.name)
+            try:
+                running_container.kill()
+                running_container.remove()
+            except Exception as e:
+                print(e)
+        elif running_container.name in container_names:
+            logging.info("Killing container %s" % running_container.name)
+            try:
+                running_container.kill()
+                running_container.remove()
+            except Exception as e:
+                print(e)
+
+
+def status():
+    running_containers = docker_client.containers.list()
+
+    orchest_container_names = [
+        CONTAINER_MAPPING[container_key]['name']
+        for container_key in CONTAINER_MAPPING
+    ]
+
+    running_prints = ['']
+    not_running_prints = ['']
+
+    for container in running_containers:
+        if container.name in orchest_container_names:
+            running_prints.append("Container %s running." % container.name)
+            orchest_container_names.remove(container.name)
+
+    for container_name in orchest_container_names:
+        not_running_prints.append("Container %s not running." % container_name)
+
+    if len(running_prints) > 1:
+        logging.info('\n'.join(running_prints))
+
+    if len(not_running_prints) > 1:
+        logging.info('\n'.join(not_running_prints))
+
+
+def update():
+    logging.info("Updating Orchest...")
+
+    for image in ALL_IMAGES:
+        try:
+            logging.info("Pulling image `%s` ..." % image)
+            docker_client.images.pull(image)
+            logging.info("Pulled image `%s`." % image)
+        except Exception as e:
+            logging.error("Something went wrong while pulling image "
+                          "%s error: %s" % (image, e))
---
+++ orchest/orchest-ctl/app/config.py
@@ -0,0 +1,135 @@
+import os
+
+from errors import ENVVariableNotFound
+
+
+# Can either be "normal" or "dev"
+RUN_MODE = "normal"
+
+DOCKER_NETWORK = "orchest"
+
+# Configurations directly related to container specifications.
+DURABLE_QUEUES_DIR = ".orchest/rabbitmq-mnesia"
+
+# All the images that are used by Orchest.
+ALL_IMAGES = [
+    "elyra/enterprise-gateway:2.2.0rc2",
+    "orchestsoftware/jupyter-server:latest",
+    "orchestsoftware/custom-base-kernel-py:latest",
+    "orchestsoftware/custom-base-kernel-r:latest",
+    "orchestsoftware/memory-server:latest",
+    "orchestsoftware/orchest-ctl:latest",
+    "orchestsoftware/orchest-api:latest",
+    "orchestsoftware/orchest-webserver:latest",
+    "orchestsoftware/celery-worker:latest",
+    "orchestsoftware/auth-server:latest",
+    "orchestsoftware/nginx-proxy:latest",
+    "rabbitmq:3",
+]
+
+# Images to be run on start of Orchest.
+ON_START_IMAGES = [
+    "orchestsoftware/orchest-api:latest",
+    "orchestsoftware/orchest-webserver:latest",
+    "orchestsoftware/celery-worker:latest",
+    "orchestsoftware/auth-server:latest",
+    "orchestsoftware/nginx-proxy:latest",
+    "rabbitmq:3",
+]
+
+HOST_USER_DIR = os.environ.get("HOST_USER_DIR")
+if HOST_USER_DIR is None:
+    raise ENVVariableNotFound("HOST_USER_DIR cannot be found in the environment.")
+
+HOST_CONFIG_DIR = os.environ.get("HOST_CONFIG_DIR")
+if HOST_CONFIG_DIR is None:
+    raise ENVVariableNotFound("HOST_CONFIG_DIR cannot be found in the environment.")
+
+CONTAINER_MAPPING = {
+    "orchestsoftware/orchest-api:latest": {
+        "environment": {},
+        "command": None,
+        "name": "orchest-api",
+        "mounts": [
+            {
+                # TODO: I don't think the orchest-api needs the entire
+                #       userdir.
+                # Needed for persistent db.
+                "source": HOST_USER_DIR,
+                "target": "/userdir"
+            },
+            {
+                "source": "/var/run/docker.sock",
+                "target": "/var/run/docker.sock"
+            },
+        ],
+
+    },
+    "orchestsoftware/orchest-webserver:latest": {
+        "name": "orchest-webserver",
+        "environment": {
+            "HOST_USER_DIR": HOST_USER_DIR,
+        },
+        "mounts": [
+            {
+                "source": "/var/run/docker.sock",
+                "target": "/var/run/docker.sock"
+            },
+            {
+                "source": HOST_USER_DIR,
+                "target": "/userdir"
+            },
+            {
+                "source": HOST_CONFIG_DIR,
+                "target": "/config"
+            }
+        ],
+    },
+    "orchestsoftware/celery-worker:latest": {
+        "name": "celery-worker",
+        "mounts": [
+            {
+                "source": "/var/run/docker.sock",
+                "target": "/var/run/docker.sock"
+            },
+            {
+                # Mount is needed for copying the snapshot dir to
+                # pipeline run dirs for experiments.
+                "source": HOST_USER_DIR,
+                "target": "/userdir"
+            },
+        ]
+    },
+    "rabbitmq:3": {
+        "name": "rabbitmq-server",
+        "hostname": "rabbitmq-hostname",
+        "mounts": [
+            {
+                # Persisting RabbitMQ Queues.
+                "source": os.path.join(HOST_USER_DIR, DURABLE_QUEUES_DIR),
+                "target": "/var/lib/rabbitmq/mnesia",
+            }
+        ],
+    },
+    "orchestsoftware/auth-server:latest": {
+        "name": "auth-server",
+        "environment": {},
+        "mounts": [
+            {
+                "source": HOST_CONFIG_DIR,
+                "target": "/config"
+            },
+            {
+                "source": HOST_USER_DIR,
+                "target": "/userdir"
+            },
+        ],
+    },
+    "orchestsoftware/nginx-proxy:latest": {
+        "name": "nginx-proxy",
+        "ports": {
+            "80/tcp": 8000,
+            "443/tcp": 443,
+        }
+    },
+}
