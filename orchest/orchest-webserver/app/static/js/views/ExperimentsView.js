d8e7c0c7bea0f3a0ed1c7e7ad15471128bacdd8e
Rick Lamers <ricklamers@gmail.com>
2020-09-23 14:19:18 +0200

    postpone orchest-api experimente DELETE

--- orchest/orchest-webserver/app/static/js/views/ExperimentsView.js
+++ orchest/orchest-webserver/app/static/js/views/ExperimentsView.js
@@ -105,7 +105,8 @@ class ExperimentsView extends React.Component {
             );
 
             // don't wait for finish on orchest-api DELETE
-            makeRequest("DELETE", "/api-proxy/api/experiments/" + this.state.experiments[selectedRows[x]].uuid)
+            // TODO: wait until orchest-api endpoint is working
+            // makeRequest("DELETE", "/api-proxy/api/experiments/" + this.state.experiments[selectedRows[x]].uuid)
         }
 
         Promise.all(promises).then(() => {
