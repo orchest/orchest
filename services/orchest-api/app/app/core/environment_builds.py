042ee24eb3543049ab0520b54895805a811d0c3a
fruttasecca <gobbijacopo91@gmail.com>
2020-11-09 11:58:55 +0100

    move from requests.Session() to requests.session.Sessions(), fix function argument evaluation, apply black

--- services/orchest-api/app/app/core/environment_builds.py
+++ services/orchest-api/app/app/core/environment_builds.py
@@ -17,9 +17,7 @@ __ENV_BUILD_FULL_LOGS_DIRECTORY = "/environment_builds_logs"
 
 
 def update_environment_build_status(
-        status: str,
-        session: requests.session(),
-        environment_build_uuid,
+    status: str, session: requests.sessions.Session, environment_build_uuid,
 ) -> Any:
     """Update environment build status.
 
@@ -36,7 +34,9 @@ def update_environment_build_status(
         return response.json()
 
 
-def build_docker_image(image_name, context_path, dockerfile_path, user_logs_file_object, complete_logs_path):
+def build_docker_image(
+    image_name, context_path, dockerfile_path, user_logs_file_object, complete_logs_path
+):
     """Build a docker image with the given tag, context_path and docker file.
 
     Args:
@@ -52,13 +52,14 @@ def build_docker_image(image_name, context_path, dockerfile_path, user_logs_file
     with open(complete_logs_path, "w") as complete_logs_file_object:
 
         # connect to docker and issue the build
-        docker_client = docker.APIClient(base_url='unix://var/run/docker.sock')
+        docker_client = docker.APIClient(base_url="unix://var/run/docker.sock")
         generator = docker_client.build(
             path=context_path,
             dockerfile=dockerfile_path,
             tag=image_name,
             rm=True,
-            nocache=True)
+            nocache=True,
+        )
 
         flag = __DOCKERFILE_RESERVED_FLAG + "\n"
         found_beginning_flag = False
@@ -73,7 +74,7 @@ def build_docker_image(image_name, context_path, dockerfile_path, user_logs_file
                 break out of the while loop because the build needs to keep going, both for error reporting
                 and for actually allowing the build to keep going, which would not happen if we process exits.
                 """
-                if 'stream' in json_output:
+                if "stream" in json_output:
                     stream = json_output["stream"]
 
                     complete_logs_file_object.write(stream)
@@ -95,7 +96,11 @@ def build_docker_image(image_name, context_path, dockerfile_path, user_logs_file
                             if not found_ending_flag:
                                 user_logs_file_object.write(stream)
 
-                had_errors = had_errors or ("error" in json_output) or ("errorDetail" in json_output)
+                had_errors = (
+                    had_errors
+                    or ("error" in json_output)
+                    or ("errorDetail" in json_output)
+                )
 
             # build is done
             except StopIteration:
@@ -107,9 +112,11 @@ def build_docker_image(image_name, context_path, dockerfile_path, user_logs_file
                 had_errors = True
 
         if had_errors:
-            msg = "There was a problem building the image. " \
-                  "Either the base image does not exist or the " \
-                  "building script had a non 0 exit code, build failed"
+            msg = (
+                "There was a problem building the image. "
+                "Either the base image does not exist or the "
+                "building script had a non 0 exit code, build failed"
+            )
             user_logs_file_object.write(msg)
             complete_logs_file_object.write(msg)
             complete_logs_file_object.flush()
@@ -134,7 +141,9 @@ def write_environment_dockerfile(base_image, bash_script, flag, path):
     statements = []
     statements.append(f"FROM {base_image}")
     statements.append("COPY . .")
-    statements.append(f'RUN chmod +x {bash_script} && echo "{flag}" && bash {bash_script} && echo "{flag}"')
+    statements.append(
+        f'RUN chmod +x {bash_script} && echo "{flag}" && bash {bash_script} && echo "{flag}"'
+    )
     statements = "\n".join(statements)
 
     with open(path, "w") as dockerfile:
@@ -160,7 +169,9 @@ def check_environment_correctness(project_uuid, environment_uuid, project_path):
     if not os.path.exists(project_path):
         raise OSError(f"Project path {project_path}does not exist")
 
-    environment_path = os.path.join(project_path, f".orchest/environments/{environment_uuid}")
+    environment_path = os.path.join(
+        project_path, f".orchest/environments/{environment_uuid}"
+    )
     if not os.path.exists(environment_path):
         raise OSError(f"Environment path {environment_path}does not exist")
 
@@ -170,7 +181,9 @@ def check_environment_correctness(project_uuid, environment_uuid, project_path):
 
     environment_user_script = os.path.join(environment_path, __ENV_STARTUP_SCRIPT_NAME)
     if not os.path.isfile(environment_user_script):
-        raise OSError(f"Environment user script ({__ENV_STARTUP_SCRIPT_NAME}) not found")
+        raise OSError(
+            f"Environment user script ({__ENV_STARTUP_SCRIPT_NAME}) not found"
+        )
 
     with open(environment_properties) as json_file:
         environment_properties = json.load(json_file)
@@ -185,15 +198,21 @@ def check_environment_correctness(project_uuid, environment_uuid, project_path):
             raise ValueError("uuid not found in environment properties.json")
 
         if environment_properties["project_uuid"] != project_uuid:
-            raise ValueError(f"The environment properties project "
-                             f"uuid {environment_properties['project_uuid']} differs {project_uuid}")
+            raise ValueError(
+                f"The environment properties project "
+                f"uuid {environment_properties['project_uuid']} differs {project_uuid}"
+            )
 
         if environment_properties["uuid"] != environment_uuid:
-            raise ValueError(f"The environment properties environment "
-                             f"uuid {environment_properties['uuid']} differs {environment_uuid}")
+            raise ValueError(
+                f"The environment properties environment "
+                f"uuid {environment_properties['uuid']} differs {environment_uuid}"
+            )
 
 
-def prepare_build_context(dockerfile_name, project_uuid, environment_uuid, project_path):
+def prepare_build_context(
+    dockerfile_name, project_uuid, environment_uuid, project_path
+):
     """Prepares the docker build context for a given environment.
 
     Prepares the docker build context by taking a snapshot of the project directory, and using this
@@ -224,7 +243,9 @@ def prepare_build_context(dockerfile_name, project_uuid, environment_uuid, proje
     os.system("rm -rf %s" % snapshot_path)
     os.system("cp -R %s %s" % (project_path, snapshot_path))
     # take the environment from the snapshot
-    environment_path = os.path.join(snapshot_path, f".orchest/environments/{environment_uuid}")
+    environment_path = os.path.join(
+        snapshot_path, f".orchest/environments/{environment_uuid}"
+    )
 
     # build the docker file and move it to the context
     with open(os.path.join(environment_path, "properties.json")) as json_file:
@@ -233,12 +254,21 @@ def prepare_build_context(dockerfile_name, project_uuid, environment_uuid, proje
         # use the task_uuid to avoid clashing with user stuff
         docker_file_name = dockerfile_name
         bash_script_name = dockerfile_name + ".sh"
-        write_environment_dockerfile(environment_properties["base_image"], bash_script_name,
-                                     __DOCKERFILE_RESERVED_FLAG, os.path.join(snapshot_path, docker_file_name))
+        write_environment_dockerfile(
+            environment_properties["base_image"],
+            bash_script_name,
+            __DOCKERFILE_RESERVED_FLAG,
+            os.path.join(snapshot_path, docker_file_name),
+        )
 
         # move the startup script to the context
-        os.system("cp %s %s" % (
-            os.path.join(environment_path, __ENV_STARTUP_SCRIPT_NAME), os.path.join(snapshot_path, bash_script_name)))
+        os.system(
+            "cp %s %s"
+            % (
+                os.path.join(environment_path, __ENV_STARTUP_SCRIPT_NAME),
+                os.path.join(snapshot_path, bash_script_name),
+            )
+        )
     return snapshot_path
 
 
@@ -257,28 +287,29 @@ def build_environment_task(task_uuid, project_uuid, environment_uuid, project_pa
     Returns:
 
     """
-    with requests.session() as session:
+    with requests.sessions.Session() as session:
 
         try:
-            update_environment_build_status(
-                "STARTED",
-                session,
-                task_uuid
-            )
+            update_environment_build_status("STARTED", session, task_uuid)
 
             # prepare the project snapshot with the correctly placed dockerfile, scripts, etc.
-            build_context = prepare_build_context(task_uuid, project_uuid, environment_uuid, project_path)
+            build_context = prepare_build_context(
+                task_uuid, project_uuid, environment_uuid, project_path
+            )
 
             # TODO: change this to _config.ENVIRONMENT_IMAGE_NAME once this stuff gets into the
             #  feature-environment branch
             ENVIRONMENT_IMAGE_NAME = "orchest-env-{project_uuid}-{environment_uuid}"
-            docker_image_name = ENVIRONMENT_IMAGE_NAME.format(project_uuid=project_uuid,
-                                                              environment_uuid=environment_uuid)
+            docker_image_name = ENVIRONMENT_IMAGE_NAME.format(
+                project_uuid=project_uuid, environment_uuid=environment_uuid
+            )
 
             if not os.path.exists(__ENV_BUILD_FULL_LOGS_DIRECTORY):
                 os.mkdir(__ENV_BUILD_FULL_LOGS_DIRECTORY)
             # place the logs in the celery container
-            complete_logs_path = os.path.join(__ENV_BUILD_FULL_LOGS_DIRECTORY, docker_image_name)
+            complete_logs_path = os.path.join(
+                __ENV_BUILD_FULL_LOGS_DIRECTORY, docker_image_name
+            )
 
             status = SioStreamedTask.run(
                 # what we are actually running/doing in this task
@@ -287,7 +318,8 @@ def build_environment_task(task_uuid, project_uuid, environment_uuid, project_pa
                     build_context,
                     task_uuid,
                     user_logs_fo,
-                    complete_logs_path),
+                    complete_logs_path,
+                ),
                 identity=f"{project_uuid}-{environment_uuid}",
                 server=_config.ORCHEST_SOCKETIO_SERVER_ADDRESS,
                 namespace=_config.ORCHEST_SOCKETIO_ENV_BUILDING_NAMESPACE,
@@ -296,24 +328,17 @@ def build_environment_task(task_uuid, project_uuid, environment_uuid, project_pa
                 # to multithreading/processing, moreover,
                 # also just passing the task_uuid to this function is less information
                 # to rely on, which is good
-                abort_lambda=lambda: AbortableAsyncResult(task_uuid).is_aborted())
+                abort_lambda=lambda: AbortableAsyncResult(task_uuid).is_aborted(),
+            )
 
             # cleanup
             os.system("rm -rf %s" % build_context)
 
-            update_environment_build_status(
-                status,
-                session,
-                task_uuid
-            )
+            update_environment_build_status(status, session, task_uuid)
 
         # catch all exceptions because we need to make sure to set the build state to failed
         except Exception as e:
-            update_environment_build_status(
-                "FAILURE",
-                session,
-                task_uuid
-            )
+            update_environment_build_status("FAILURE", session, task_uuid)
             raise e
 
     return status
