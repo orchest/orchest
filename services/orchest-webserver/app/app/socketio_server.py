f53760990391bfd4f547caa946d8c4c436b467f9
Rick Lamers <ricklamers@gmail.com>
2020-11-10 10:03:23 +0100

    get rid of sleep by using an ack-message on the server on sio_streamed_task_finished

--- services/orchest-webserver/app/app/socketio_server.py
+++ services/orchest-webserver/app/app/socketio_server.py
@@ -41,6 +41,8 @@ def register_socketio_broadcast(db, socketio):
                     namespace="/environment_builds",
                 )
 
+    
+
 
     @socketio.on("sio_streamed_task_data", namespace="/environment_builds")
     def process_sio_streamed_task_data(data):
@@ -75,6 +77,14 @@ def register_socketio_broadcast(db, socketio):
                     "sio_streamed_task_data",
                     data,
                     include_self=False,
+                    namespace="/environment_builds",
+                )
+            elif data["action"] == "sio_streamed_task_finished":
+                
+                socketio.emit(
+                    "sio_streamed_task_data_finished_ack",
+                    {},
+                    room=request.sid,
                     namespace="/environment_builds",
                 )
 
