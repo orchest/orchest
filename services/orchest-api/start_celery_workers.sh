adc41a361bd713e5cafcab1b13f0714365b47e78
fruttasecca <gobbijacopo91@gmail.com>
2020-11-12 10:42:04 +0100

    set a different pid file for each celery worker

--- services/orchest-api/start_celery_workers.sh
+++ services/orchest-api/start_celery_workers.sh
@@ -8,6 +8,7 @@ celery worker -A app.core.tasks \
     --statedb /userdir/.orchest/celery-state.db \
     -f celery_experiments.log \
     --concurrency=1 \
+    --pidfile="worker-expriments.pid" \
     --detach
 
 # Interactive runs. "celery" is the default queue name.
@@ -17,6 +18,7 @@ celery worker -A app.core.tasks \
     -n worker-interactive \
     -f celery_interactive.log \
     --concurrency=1 \
+    --pidfile="worker-interactive.pid" \
     --detach
 
 
@@ -30,4 +32,5 @@ celery worker -A app.core.tasks \
     --statedb /userdir/.orchest/celery-environment-builds-state.db \
     -f celery_env_builds.log \
     --concurrency=1 \
+    --pidfile="worker-env-builds.pid" \
     --max-tasks-per-child 1
