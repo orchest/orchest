9fa71a0bc0834c9f74790d308f2eaddcf8d134e0
Yannick <yannick.perrenet@gmail.com>
2020-11-17 09:02:17 +0100

    Restore userdir of latest local git commit

--- scripts/clean_userdir.sh
+++ scripts/clean_userdir.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# This script cleans the userdir (while leaving the root .gitignore files
-# intact)
+# This script restores the userdir to the state in the git repository
+# (of the latest local commit).
 
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
@@ -12,13 +12,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo "[Cleaning up userdir]: ..."
 
-# clean up symlinks
-for component in .orchest/rabbitmq-mnesia .orchest/kernels projects experiments
-do
-find $DIR/../userdir/$component -mindepth 1 -maxdepth 1 ! -name ".gitignore" -exec rm -rf -- {} +
-done
-
-# clean up dbs
-rm $DIR/../userdir/.orchest/*.db
+# Removes the userdir and then restores the structure imposed on the
+# userdir by the git repository. Restoring all defaults such as
+# .gitignore files.
+rm -rf $DIR/../userdir
+git checkout $DIR/../userdir
 
 echo "[Cleaning up userdir]: succes"
