29c16a59b97514d7350d9793361eddbc94fc0825
Rick Lamers <ricklamers@gmail.com>
2020-11-20 15:24:16 +0100

    add production webpack config for container building of orchest-webserver

--- services/orchest-webserver/app/static/webpack.config.js
+++ services/orchest-webserver/app/static/webpack.config.js
@@ -1,39 +1,69 @@
 const path = require("path");
 
-module.exports = {
-  entry: "./js/main.js",
-  mode: "development",
-  watch: false,
-  devtool: "eval-cheap-source-map",
-  output: {
-    path: path.resolve(__dirname, "js/dist"),
-    filename: "main.bundle.js",
-  },
-  optimization: {
-    removeAvailableModules: false,
-    removeEmptyChunks: false,
-    splitChunks: false,
-  },
-  module: {
-    rules: [
-      {
-        use: [
-          {
-            loader: "cache-loader",
-          },
-          {
-            loader: "babel-loader",
-            query: {
-              presets: ["@babel/preset-env"],
+module.exports = [
+  {
+    entry: "./js/main.js",
+    mode: "development",
+    watch: false,
+    devtool: "eval-cheap-source-map",
+    output: {
+      path: path.resolve(__dirname, "js/dist"),
+      filename: "main.bundle.js",
+    },
+    optimization: {
+      removeAvailableModules: false,
+      removeEmptyChunks: false,
+      splitChunks: false,
+    },
+    module: {
+      rules: [
+        {
+          use: [
+            {
+              loader: "cache-loader",
             },
-          },
-        ],
-        include: path.resolve(__dirname, "js"),
-      },
-    ],
+            {
+              loader: "babel-loader",
+              query: {
+                presets: ["@babel/preset-env"],
+              },
+            },
+          ],
+          include: path.resolve(__dirname, "js"),
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".js"],
+      symlinks: false,
+    },
   },
-  resolve: {
-    extensions: [".js"],
-    symlinks: false,
+  {
+    entry: "./js/main.js",
+    mode: "production",
+    devtool: "source-map",
+    output: {
+      path: path.resolve(__dirname, "js/dist"),
+      filename: "main.bundle.js",
+    },
+    module: {
+      rules: [
+        {
+          use: [
+            {
+              loader: "babel-loader",
+              query: {
+                presets: ["@babel/preset-env"],
+              },
+            },
+          ],
+          include: path.resolve(__dirname, "js"),
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".js"],
+      symlinks: false,
+    },
   },
-};
+];
