1c323047e67efedc569a94b41edef5c4ea7445d3
Rick Lamers <ricklamers@gmail.com>
2020-11-24 16:30:54 +0100

    standardize font size in css and use it for help-list

--- services/orchest-webserver/app/static/js/views/HelpView.js
+++ services/orchest-webserver/app/static/js/views/HelpView.js
@@ -17,51 +17,65 @@ class HelpView extends React.Component {
 
   render() {
     return (
-      <div className={"view-page"}>
+      <div className={"view-page help-list"}>
         <h2>Looking for help, or want to know more?</h2>
         <p className="push-down">
           The documentation should get you up to speed, but feel free to get in
           touch through Slack or GitHub for any questions or suggestions.
         </p>
 
-        <li className="mdc-list">
+        <div className="mdc-list">
           <a
             className="mdc-list-item"
             href={this.state.readthedocs_quickstart_url}
+            target="_blank"
           >
             <i className="mdc-list-item__graphic" aria-hidden="true">
               <img src="public/image/readthedocs.png" width="100%" />
             </i>
             <span className="mdc-list-item__text">Quickstart</span>
           </a>
-          <a className="mdc-list-item" href={this.state.readthedocs_url}>
+          <a
+            className="mdc-list-item"
+            href={this.state.readthedocs_url}
+            target="_blank"
+          >
             <i className="mdc-list-item__graphic" aria-hidden="true">
               <img src="public/image/readthedocs.png" width="100%" />
             </i>
             <span className="mdc-list-item__text">Documentation</span>
           </a>
-          <a className="mdc-list-item" href={this.state.slack_url}>
+          <a
+            className="mdc-list-item"
+            href={this.state.slack_url}
+            target="_blank"
+          >
             <i className="mdc-list-item__graphic" aria-hidden="true">
               <img src="public/image/slack.png" width="100%" />
             </i>
             <span className="mdc-list-item__text">Slack</span>
           </a>
-          <a className="mdc-list-item" href={this.state.github_url}>
-            {/* <i className="material-icons mdc-list-item__graphic" aria-hidden="true"
-              >info</i
-            > */}
+          <a
+            className="mdc-list-item"
+            href={this.state.github_url}
+            target="_blank"
+          >
             <i className="mdc-list-item__graphic" aria-hidden="true">
               <img src="public/image/github.png" width="100%" />
             </i>
             <span className="mdc-list-item__text">GitHub</span>
           </a>
-          <a className="mdc-list-item" href={this.state.website_url}>
+          <a
+            className="mdc-list-item"
+            href={this.state.website_url}
+            target="_blank"
+          >
             <i className="mdc-list-item__graphic" aria-hidden="true">
               <img src="public/image/favicon.png" width="100%" />
             </i>
             <span className="mdc-list-item__text">Website</span>
           </a>
-        </li>
+        </div>
       </div>
     );
   }
