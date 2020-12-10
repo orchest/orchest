529447176cd3da3f76f7fdc7bd3e1d860574b935
fruttasecca <gobbijacopo91@gmail.com>
2020-12-10 19:57:37 +0100

    set metadata separator to ';<space>'

--- orchest-sdk/python/orchest/config.py
+++ orchest-sdk/python/orchest/config.py
@@ -45,7 +45,7 @@ class Config:
     CONN_NUM_RETRIES = 20
     # Separator for the metadata related to stored data, both to disk an
     # d to memory.
-    __METADATA_SEPARATOR__ = "__ORCHEST_METADATA_SEP__"
+    __METADATA_SEPARATOR__ = "; "
     # Reserved key of the aggregated unnamed outputs list in the diction
     # ary returned by get_inputs()
     _RESERVED_UNNAMED_OUTPUTS_STR = "unnamed_inputs"
