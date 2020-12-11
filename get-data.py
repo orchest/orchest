b89966f8c61abc58055bf1f8dc239c004eea4800
fruttasecca <gobbijacopo91@gmail.com>
2020-12-11 15:09:13 +0100

    update to new Orchest SDK api

--- get-data.py
+++ get-data.py
@@ -16,5 +16,5 @@ df_target = pd.DataFrame(data["target"], columns=["MedHouseVal"])
 
 # Output the housing data so the next steps can retrieve it.
 print("Outputting converted housing data...")
-orchest.output((df_data, df_target))
+orchest.output((df_data, df_target), name="data")
 print("Success!")
