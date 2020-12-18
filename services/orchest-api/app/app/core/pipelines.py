9fa23065d6e3dab9a32f462d889d9eeb6959059d
Rick Lamers <ricklamers@gmail.com>
2020-12-18 11:59:09 +0100

    groupadd for pipeline steps

--- services/orchest-api/app/app/core/pipelines.py
+++ services/orchest-api/app/app/core/pipelines.py
@@ -268,7 +268,7 @@ class PipelineStepRunner:
             "NetworkingConfig": {
                 "EndpointsConfig": {"orchest": {}}  # TODO: should not be hardcoded.
             },
-            "User": f':{os.environ.get("ORCHEST_HOST_GID")}',
+            "GroupAdd": [os.environ.get("ORCHEST_HOST_GID")],
             # NOTE: the `'tests-uuid'` key is only used for tests and
             # gets ignored by the `docker_client`.
             "tests-uuid": self.properties["uuid"],
