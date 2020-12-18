25488fab0f82cf65b590a08db5eeb1a7c133a1b0
Rick Lamers <ricklamers@gmail.com>
2020-12-18 13:56:48 +0100

    minor cleanups

--- services/orchest-ctl/app/utils.py
+++ services/orchest-ctl/app/utils.py
@@ -195,9 +195,10 @@ def clean_containers():
 
 
 def fix_userdir_permissions():
-    # setgid on all directories in userdir to make sure new files
-    # created by containers are read/write for sibling containers
-    # and the host user
+    """setgid on all directories in userdir to make sure new files
+    created by containers are read/write for sibling containers
+    and the host user"""
+    
     try:
         os.system("find /orchest-host/userdir -type d -exec chmod g+s {} \;")
     except Exception as e:
