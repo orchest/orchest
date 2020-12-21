98dd9ce15a4a17c649aaa76c7e0a1a377c9e64cb
jacopo gobbi <gobbijacopo91@gmail.com>
2020-12-21 09:46:39 +0100

    config to _config for improved consistency
    Co-authored-by: Yannick Perrenet <yannick.perrenet@gmail.com>

--- services/auth-server/app/app/connections.py
+++ services/auth-server/app/app/connections.py
@@ -1,9 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
-from _orchest.internals import config
+from _orchest.internals import config as _config
 
 # this will make it so that constraints and indexes follow a certain
 # naming pattern
-metadata = MetaData(naming_convention=config.database_naming_convention)
+metadata = MetaData(naming_convention=_config.database_naming_convention)
 db = SQLAlchemy(metadata=metadata)
