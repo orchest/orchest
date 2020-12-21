b5cb7b8b7b5691f3a442ccbdbb09434eef4d7b18
jacopo gobbi <gobbijacopo91@gmail.com>
2020-12-21 09:47:03 +0100

    config to _config for improved consistency 2
    Co-authored-by: Yannick Perrenet <yannick.perrenet@gmail.com>

--- services/orchest-webserver/app/app/connections.py
+++ services/orchest-webserver/app/app/connections.py
@@ -2,10 +2,9 @@ from flask_marshmallow import Marshmallow
 from sqlalchemy import MetaData
 from flask_sqlalchemy import SQLAlchemy
 
-from _orchest.internals import config
+from _orchest.internals import config as _config
 
 # this will make it so that constraints and indexes follow a certain
 # naming pattern
-metadata = MetaData(naming_convention=config.database_naming_convention)
-db = SQLAlchemy(metadata=metadata)
+metadata = MetaData(naming_convention=_config.database_naming_convention)
 ma = Marshmallow()
