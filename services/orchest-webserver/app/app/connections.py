81833917942ab082b892075556c0c28052a40708
fruttasecca <gobbijacopo91@gmail.com>
2020-12-21 10:04:21 +0100

    fix missing db instantiation

--- services/orchest-webserver/app/app/connections.py
+++ services/orchest-webserver/app/app/connections.py
@@ -7,4 +7,6 @@ from _orchest.internals import config as _config
 # this will make it so that constraints and indexes follow a certain
 # naming pattern
 metadata = MetaData(naming_convention=_config.database_naming_convention)
+db = SQLAlchemy(metadata=metadata)
+
 ma = Marshmallow()
