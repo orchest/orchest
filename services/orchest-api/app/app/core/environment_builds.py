c8075fe36f0cea1257357ff8e1744cf6ec877f73
Rick Lamers <ricklamers@gmail.com>
2020-12-23 18:10:04 +0100

    fix env build permissions

--- services/orchest-api/app/app/core/environment_builds.py
+++ services/orchest-api/app/app/core/environment_builds.py
@@ -232,11 +232,14 @@ def write_environment_dockerfile(
     # the bash script is removed so that the user won't be able to see it after the build is done
     statements.append(
         f'RUN cd "{os.path.join("/", work_dir)}" '
-        f'&& sudo chmod +x {bash_script} '
+        "&& sudo chown -R :$(id -g) . "
+        "&& sudo find . -type d -exec chmod g+rwxs {} \; "
+        "&& sudo find . -type f -exec chmod g+rwx {} \; "
+        "&& sudo chmod g+rwx . "
         f'&& echo "{flag}" '
-        f'&& bash {bash_script} '
+        f"&& bash {bash_script} "
         f'&& echo "{flag}" '
-        f'&& sudo rm {bash_script}'
+        f"&& sudo rm {bash_script}"
     )
     statements.append("LABEL _orchest_env_build_is_intermediate=0")
 
