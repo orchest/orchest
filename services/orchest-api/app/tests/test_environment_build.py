08e1241695ccf74d6fb2c96100295970871ecb0c
fruttasecca <gobbijacopo91@gmail.com>
2021-01-07 10:45:02 +0100

    improve test parametrization

--- services/orchest-api/app/tests/test_environment_build.py
+++ services/orchest-api/app/tests/test_environment_build.py
@@ -1,112 +1,14 @@
-import requests
 import json
+import os
 import time
 
 import docker
 import pytest
+import requests
 import socketio
 
-import app.core.environment_builds
 import app.connections
-
-
-@pytest.fixture(
-    params=[
-        {
-            "task_uuid": 5,
-            "image_in_local_environment": True,
-            "abort": False,
-            "build_events": [],
-        },
-        {
-            "task_uuid": 5,
-            "image_in_local_environment": True,
-            "abort": True,
-            "build_events": [],
-        },
-        {
-            "task_uuid": 5,
-            "image_in_local_environment": False,
-            "abort": True,
-            "build_events": [],
-        },
-        {
-            "task_uuid": 5,
-            "image_in_local_environment": False,
-            "abort": False,
-            "build_events": [],
-        },
-        {
-            "task_uuid": 5,
-            "image_in_local_environment": True,
-            "abort": False,
-            "build_events": [None],
-        },
-        {
-            "task_uuid": 5,
-            "image_in_local_environment": True,
-            "abort": True,
-            "build_events": [None],
-        },
-        {
-            "task_uuid": 5,
-            "image_in_local_environment": False,
-            "abort": True,
-            "build_events": [None],
-        },
-        {
-            "task_uuid": 5,
-            "image_in_local_environment": False,
-            "abort": False,
-            "build_events": [None],
-        },
-        {
-            "task_uuid": 1,
-            "image_in_local_environment": True,
-            "abort": False,
-            "build_events": [1, 2, 3, 4],
-        },
-        {
-            "task_uuid": 2,
-            "image_in_local_environment": True,
-            "abort": True,
-            "build_events": [1, 2, 3, 4],
-        },
-        {
-            "task_uuid": 3,
-            "image_in_local_environment": False,
-            "abort": True,
-            "build_events": [1, 2, 3, 4],
-        },
-        {
-            "task_uuid": 4,
-            "image_in_local_environment": False,
-            "abort": False,
-            "build_events": [1, 2, 3, 4],
-        },
-        {
-            "task_uuid": 5,
-            "image_in_local_environment": False,
-            "abort": False,
-            "build_events": [1, 2, 3, 4, None],
-        },
-    ]
-)
-def testio(request):
-    param = request.param
-    param["task_uuid"] = str(param["task_uuid"])
-    param["project_uuid"] = str(param["task_uuid"])
-    param["environment_uuid"] = str(param["task_uuid"])
-    param["project_path"] = str(param["task_uuid"])
-    events = []
-    for event in param["build_events"]:
-        if event is None:
-            events.append(event)
-        else:
-            events.append(str(event))
-    param["build_events"] = events
-
-    return param
+import app.core.environment_builds
 
 
 class MockRequestReponse:
@@ -124,7 +26,51 @@ class MockRequestReponse:
 _NOT_TO_BE_LOGGED = "_NOT_TO_BE_LOGGED"
 
 
-def test_environment_build(testio, monkeypatch):
+@pytest.mark.parametrize(
+    "task_uuid",
+    ["task_uuid"],
+    ids=["basic"],
+)
+@pytest.mark.parametrize(
+    "project_uuid",
+    ["project_uuid"],
+    ids=["basic"],
+)
+@pytest.mark.parametrize(
+    "environment_uuid",
+    ["environment_uuid"],
+    ids=["basic"],
+)
+@pytest.mark.parametrize(
+    "project_path",
+    ["project_path"],
+    ids=["basic"],
+)
+@pytest.mark.parametrize(
+    "image_in_local_environment",
+    [True, False],
+    ids=["image_in_env", "image_not_in_env"],
+)
+@pytest.mark.parametrize(
+    "abort",
+    [True, False],
+    ids=["abort_task", "do_not_abort_task"],
+)
+@pytest.mark.parametrize(
+    "build_events",
+    [[], [None], ["1", "2", "3", "4"], ["1", "2", "3", "4", None]],
+    ids=["[]", "[None]", "[1, 2, 3, 4]", "[1, 2, 3, 4, None]"],
+)
+def test_environment_build(
+    task_uuid,
+    project_uuid,
+    environment_uuid,
+    project_path,
+    image_in_local_environment,
+    abort,
+    build_events,
+    monkeypatch,
+):
     def mock_put_request(self, url, json=None, *args, **kwargs):
         put_requests.append(json["status"])
         return MockRequestReponse()
@@ -151,7 +97,7 @@ def test_environment_build(testio, monkeypatch):
             pass
 
         def is_aborted(self):
-            return testio["abort"]
+            return abort
 
     class MockSocketIOClient:
         def __init__(self, *args, **kwargs) -> None:
@@ -196,7 +142,7 @@ def test_environment_build(testio, monkeypatch):
 
         # Will be used as docker_client.images.get(...).
         def get(self, *args, **kwargs):
-            if not testio["image_in_local_environment"]:
+            if not image_in_local_environment:
                 raise docker.errors.ImageNotFound("error")
 
         # Will be used as docker_client.api.build(...).
@@ -207,7 +153,7 @@ def test_environment_build(testio, monkeypatch):
             events = (
                 [_NOT_TO_BE_LOGGED]
                 + ["_ORCHEST_RESERVED_FLAG_"]
-                + testio["build_events"]
+                + build_events
                 + ["_ORCHEST_RESERVED_FLAG_"]
                 + [_NOT_TO_BE_LOGGED]
             )
@@ -274,33 +220,33 @@ def test_environment_build(testio, monkeypatch):
     }
 
     app.core.environment_builds.build_environment_task(
-        testio["task_uuid"],
-        testio["project_uuid"],
-        testio["environment_uuid"],
-        testio["project_path"],
+        task_uuid,
+        project_uuid,
+        environment_uuid,
+        project_path,
     )
 
     assert len(put_requests) == 2
     assert put_requests[0] == "STARTED"
 
-    if testio["abort"]:
+    if abort:
         assert put_requests[1] == "ABORTED"
-    elif any([event is None for event in testio["build_events"]]):
+    elif any([event is None for event in build_events]):
         assert put_requests[1] == "FAILURE"
     else:
         assert put_requests[1] == "SUCCESS"
 
     assert len(delete_requests) == 1
-    assert delete_requests[0] == (testio["project_uuid"], testio["environment_uuid"])
+    assert delete_requests[0] == (project_uuid, environment_uuid)
 
     assert len(docker_cleanup_uuid_request) == 1
-    assert docker_cleanup_uuid_request[0] == testio["project_uuid"]
+    assert docker_cleanup_uuid_request[0] == task_uuid
 
     assert socketio_data["has_connected"]
     assert socketio_data["has_disconnected"]
 
-    if not testio["abort"]:
-        if not testio["image_in_local_environment"]:
+    if not abort:
+        if not image_in_local_environment:
             assert "Pulling image" in socketio_data["output_logs"][0]
 
         # We cannot rely on different messages being different elements
@@ -311,9 +257,17 @@ def test_environment_build(testio, monkeypatch):
         assert _NOT_TO_BE_LOGGED not in logged_events
 
         expected_events = []
-        for event in testio["build_events"]:
+        for event in build_events:
             if event is None:
                 break
             expected_events.append(event)
         expected_events = "\n".join(expected_events)
         assert expected_events in logged_events
+
+    # Successful tests can remove their log file.
+    os.remove(
+        os.path.join(
+            app.core.environment_builds.__ENV_BUILD_FULL_LOGS_DIRECTORY,
+            f"orchest-env-{project_uuid}-{environment_uuid}",
+        )
+    )
