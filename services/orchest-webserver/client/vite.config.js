4090a0c3f974f8d11fc56d24f5ecc8880d9c3831
joe-bell <joe@joebell.co.uk>
2021-04-08 17:39:26 +0300

    Add vite

---
+++ services/orchest-webserver/client/vite.config.js
@@ -0,0 +1,20 @@
+import path from "path";
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [reactRefresh()],
+  resolve: {
+    alias: {
+      "@material": path.resolve(__dirname, "../../../node_modules/@material/"),
+    },
+  },
+  css: {
+    preprocessOptions: {
+      scss: {
+        includePaths: ["../../../node_modules"],
+      },
+    },
+  },
+});
