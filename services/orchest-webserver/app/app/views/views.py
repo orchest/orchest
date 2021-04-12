d3666ba9f5998a9e09e98f24233c6987c4e1171d
Rick Lamers <ricklamers@gmail.com>
2021-04-12 20:23:04 +0200

    Remove unused import

--- services/orchest-webserver/app/app/views/views.py
+++ services/orchest-webserver/app/app/views/views.py
@@ -5,7 +5,7 @@ import uuid
 import docker
 import requests
 import sqlalchemy
-from flask import current_app, jsonify, render_template, request
+from flask import current_app, jsonify, request
 from flask_restful import Api, Resource
 from nbconvert import HTMLExporter
 
