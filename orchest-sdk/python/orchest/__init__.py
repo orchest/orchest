23bfc9b66f1ab73da9ccd0d3db6250eb714af774
gobbijacopo91@gmail.com <gobbijacopo91@gmail.com>
2021-04-13 15:50:11 +0200

    Do not expose get_params, update_params

--- orchest-sdk/python/orchest/__init__.py
+++ orchest-sdk/python/orchest/__init__.py
@@ -1,9 +1,7 @@
 from orchest.config import Config
 from orchest.parameters import (
-    get_params,
     get_pipeline_param,
     get_step_param,
-    update_params,
     update_pipeline_param,
     update_step_param,
 )
