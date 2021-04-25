d07a355a77cb9c7527e04f4143cd0774ec5d9345
Rick Lamers <ricklamers@gmail.com>
2021-04-26 00:29:15 +0200

    Make black/flake8 happy

--- services/jupyter-server/patch-pip.sh
+++ services/jupyter-server/patch-pip.sh
@@ -5,7 +5,7 @@ python_snippet=$(cat /pip-warning.py)
 
 if test -f "$pip_init_location"; then
     init_contents=$(cat $pip_init_location)
-    if ! [[ $init_contents == *"## PATCH MARKER: uh9999x ##"* ]]; then
+    if ! [[ $init_contents == *"# PATCH MARKER: uh9999x"* ]]; then
         # Patch
         echo -e "$python_snippet\n$(cat $pip_init_location)" > "$pip_init_location"
     fi
--- services/jupyter-server/pip-warning.py
+++ services/jupyter-server/pip-warning.py
@@ -1,9 +1,11 @@
-## PATCH MARKER: uh9999x ##
+# PATCH MARKER: uh9999x
 
 import atexit
 
+
 def orchest_notice():
-    print("""
+    print(
+        """
  \ \        / /             (_)
   \ \  /\  / /_ _ _ __ _ __  _ _ __   __ _
    \ \/  \/ / _` | '__| '_ \| | '_ \ / _` |
@@ -15,6 +17,8 @@ def orchest_notice():
 # Please use Orchest environments to install pip packages.
 # NOTE: This only applies to installing packages inside Jupyter
 # kernels, not when installing Jupyter extensions.
-""")
+"""
+    )
+
 
 atexit.register(orchest_notice)
