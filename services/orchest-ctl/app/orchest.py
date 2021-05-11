68c11c3163d054cae18001f1fed14b2d1fb4011e
gobbijacopo91@gmail.com <gobbijacopo91@gmail.com>
2021-05-11 13:26:08 +0200

    Do not cleanup dandling images on start

--- services/orchest-ctl/app/orchest.py
+++ services/orchest-ctl/app/orchest.py
@@ -84,8 +84,6 @@ class OrchestApp:
                 on start.
 
         """
-        self.resource_manager.remove_orchest_dangling_imgs()
-
         # Check whether the minimal set of images is present for Orchest
         # to be started.
         pulled_images = self.resource_manager.get_images()
