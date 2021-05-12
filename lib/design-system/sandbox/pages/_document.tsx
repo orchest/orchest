263b09e4167d9b8755b77689578db5ddb1782b0d
joe-bell <joe@joebell.co.uk>
2021-05-12 12:48:58 +0300

    Clean up fonts, add 600 weight

--- lib/design-system/sandbox/pages/_document.tsx
+++ lib/design-system/sandbox/pages/_document.tsx
@@ -11,6 +11,15 @@ export default class Document extends NextDocument {
             id="stitches"
             dangerouslySetInnerHTML={{ __html: getCssString() }}
           />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap"
+          />
         </Head>
         <body>
           <Main />
