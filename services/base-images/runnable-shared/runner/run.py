0b9d3e3f82825e60e377a028eb25665ae44a556a
Rick Lamers <ricklamers@gmail.com>
2021-05-26 14:13:43 +0200

    Change working directory to be relative to the file being executed.

--- services/base-images/runnable-shared/runner/run.py
+++ services/base-images/runnable-shared/runner/run.py
@@ -1,8 +1,5 @@
 import os
-import subprocess
 import sys
-
-import nbformat
 
 from runner.config import Config
 from runner.runners import NotebookRunner, ProcessRunner
@@ -26,19 +23,22 @@ def main():
     # index 1 contains filename
     if len(sys.argv) < 3:
         raise Exception(
-            "Should pass in the working directory (relative to the project dir) and filename (relative to the working directory) that you want to execute."
+            "Should pass in the working directory (relative to the project dir) and "
+            "filename (relative to the working directory) that you want to execute."
         )
 
     step_uuid = os.environ.get("ORCHEST_STEP_UUID")
     pipeline_uuid = os.environ.get("ORCHEST_PIPELINE_UUID")
 
-    # sys.argv[1] contains the working directory relative to the project dir
+    # sys.argv[1] contains the working directory relative to
+    # the project dir
     working_dir = os.path.join(Config.PROJECT_DIR, sys.argv[1])
 
-    # sys.argv[2] contains the relative file path (relative to the pipeline file)
+    # sys.argv[2] contains the relative file path
+    # (relative to the pipeline file)
     filename = sys.argv[2]
     file_extension = get_filename_extension(filename)
-    file_path = os.path.join(Config.PROJECT_DIR, working_dir, filename)
+    file_path = os.path.join(Config.PROJECT_DIR, filename)
 
     # check if file exists in working directory
     if not os.path.isfile(file_path):
@@ -60,7 +60,7 @@ def main():
         }
 
         pr = ProcessRunner(pipeline_uuid, step_uuid, working_dir)
-        sys.exit(pr.run(extension_script_mapping[file_extension], filename))
+        sys.exit(pr.run(extension_script_mapping[file_extension], file_path))
 
     else:
         raise Exception(
