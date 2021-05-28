d5a237fe4323258d62a62a1dcdb46ad44e9f3597
joe-bell <joe@joebell.co.uk>
2021-05-28 17:03:39 +0300

    Remove premature Stitches migration

--- services/orchest-webserver/client/src/views/FileManagerView.jsx
+++ services/orchest-webserver/client/src/views/FileManagerView.jsx
@@ -1,25 +1,9 @@
 // @ts-check
 import React from "react";
-import { css } from "@orchest/design-system";
-
-const fileManagerRoot = css({
-  include: "box",
-  padding: 0,
-  overflowY: "auto",
-  height: "100%",
-});
-
-const fileManagerIframe = css({
-  include: "box",
-  width: "100%",
-  height: "100%",
-  border: 0,
-  display: "block",
-});
 
 const FileManagerView = () => (
-  <div className={fileManagerRoot()}>
-    <iframe className={fileManagerIframe()} src="/container-file-manager" />
+  <div className="view-page no-padding">
+    <iframe className="borderless fullsize" src="/container-file-manager" />
   </div>
 );
 
