9455638d136087365c10906d4780bb8e0185b2f4
Yannick <yannick.perrenet@gmail.com>
2021-06-09 19:47:14 +0200

    Add version compatibility check in SDK on import

--- orchest-sdk/python/orchest/__init__.py
+++ orchest-sdk/python/orchest/__init__.py
@@ -1,3 +1,5 @@
+import os as __os
+
 from orchest._version import __version__
 from orchest.config import Config
 from orchest.parameters import (
@@ -7,3 +9,18 @@ from orchest.parameters import (
     update_step_param,
 )
 from orchest.transfer import get_inputs, output
+
+orchest_version = __os.getenv("ORCHEST_VERSION")
+if orchest_version is not None:
+    # Check for version compatibility between the orchest-sdk and the
+    # Orchest application.
+    if orchest_version < "v2021.05.1":  # starting point
+        pass
+    elif orchest_version >= "v2021.05.1" and __version__ < "0.2.0":
+        import warnings
+
+        warnings.warn(
+            "The Orchest SDK seems to have an incompatible version"
+            " with respect to the Orchest application. Please upgrade"
+            " the SDK version according to https://pypi.org/project/orchest/."
+        )
