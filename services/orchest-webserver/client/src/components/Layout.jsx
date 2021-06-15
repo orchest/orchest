1f02ddc71a67bb592f2ef258610f02d6b59cc912
joe-bell <joe@joebell.co.uk>
2021-06-15 16:38:13 +0300

    Add Onboarding Dialog foundations

--- services/orchest-webserver/client/src/components/Layout.jsx
+++ services/orchest-webserver/client/src/components/Layout.jsx
@@ -1,7 +1,47 @@
 // @ts-check
 import React from "react";
+import {
+  Box,
+  Dialog,
+  DialogBody,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  Flex,
+  IconButton,
+  IconChevronRightOutline,
+} from "@orchest/design-system";
 
 /** @type React.FC<{}> */
 export const Layout = (props) => {
-  return <React.Fragment>{props.children}</React.Fragment>;
+  return (
+    <React.Fragment>
+      {props.children}
+      <Dialog>
+        <Box css={{ backgroundColor: "$red100", padding: "$4" }}>
+          <DialogTrigger>Open Onboarding Dialog)</DialogTrigger>
+          <Box as="small" css={{ display: "block", paddingTop: "$2" }}>
+            (for development purposes only)
+          </Box>
+        </Box>
+        <DialogContent>
+          <DialogHeader css={{ justifyContent: "center" }}>
+            <DialogTitle>Discover Orchest</DialogTitle>
+          </DialogHeader>
+          <DialogBody css={{ textAlign: "center", justifyContent: "center" }}>
+            Find out more about the core concepts.
+          </DialogBody>
+          <DialogFooter css={{ justifyContent: "center" }}>
+            <Flex gap="2">
+              <IconButton rounded size="4" label="Next">
+                <IconChevronRightOutline />
+              </IconButton>
+            </Flex>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </React.Fragment>
+  );
 };
