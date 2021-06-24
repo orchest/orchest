eb45afb466a4c418e137fcd9af4487b5ed699eb4
Rick Lamers <ricklamers@gmail.com>
2021-06-24 17:02:14 +0200

    Handle min=max edge case.

--- services/orchest-webserver/client/src/utils/wrap-number.js
+++ services/orchest-webserver/client/src/utils/wrap-number.js
@@ -8,6 +8,10 @@
  * @returns number
  */
 export const wrapNumber = (min, max, index) => {
+  if (min == max) {
+    return min;
+  }
+
   const rangeSize = max - min;
   return ((((index - min) % rangeSize) + rangeSize) % rangeSize) + min;
 };
