3db911cbbbeed75e49906e9b65727971ebe7946b
joe-bell <joe@joebell.co.uk>
2021-07-20 16:33:47 +0300

    Use typed Vite config

--- services/auth-server/client/vite.config.ts
+++ services/auth-server/client/vite.config.ts
@@ -2,34 +2,29 @@ import path from "path";
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 
-// https://vitejs.dev/config/
-export default (conditions) =>
-  defineConfig({
-    plugins: [reactRefresh()],
-    base: "/login/",
-    server: {
-      host: "0.0.0.0",
-      port: 3001,
-      proxy: conditions.mode === "development" && {
-        "^/login/(server-config|submit|users)/*": "http://localhost:8000",
+export default defineConfig((env) => ({
+  plugins: [reactRefresh()],
+  base: "/login/",
+  server: {
+    host: "0.0.0.0",
+    port: 3001,
+    proxy: env.mode === "development" && {
+      "^/login/(server-config|submit|users)/*": "http://localhost:8000",
+    },
+  },
+  define: {
+    global: "window",
+  },
+  resolve: {
+    alias: {
+      "@material": path.resolve(__dirname, "../../../node_modules/@material/"),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        includePaths: ["node_modules"],
       },
     },
-    define: {
-      global: "window",
-    },
-    resolve: {
-      alias: {
-        "@material": path.resolve(
-          __dirname,
-          "../../../node_modules/@material/"
-        ),
-      },
-    },
-    css: {
-      preprocessorOptions: {
-        scss: {
-          includePaths: ["node_modules"],
-        },
-      },
-    },
-  });
+  },
+}));
--- services/orchest-webserver/client/vite.config.ts
+++ services/orchest-webserver/client/vite.config.ts
@@ -3,35 +3,33 @@ import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import { vitePluginDesignSystem } from "@orchest/design-system-vite-plugin";
 
-// https://vitejs.dev/config/
-export default (conditions) =>
-  defineConfig({
-    plugins: [reactRefresh(), vitePluginDesignSystem()],
-    server: {
-      host: "0.0.0.0",
-      proxy: conditions.mode === "development" && {
-        "^/(analytics|api|async|catch|container-file-manager|heartbeat|store|update-server)/*":
-          "http://localhost:8000",
-        "^/login/*": "http://localhost:3001",
+export default defineConfig((env) => ({
+  plugins: [reactRefresh(), vitePluginDesignSystem()],
+  server: {
+    host: "0.0.0.0",
+    proxy: env.mode === "development" && {
+      "^/(analytics|api|async|catch|container-file-manager|heartbeat|store|update-server)/*":
+        "http://localhost:8000",
+      "^/login/*": "http://localhost:3001",
+    },
+  },
+  resolve: {
+    alias: [
+      { find: "@", replacement: path.resolve(__dirname, "src") },
+      {
+        find: "@material",
+        replacement: path.resolve(
+          __dirname,
+          "../../../node_modules/@material/"
+        ),
+      },
+    ],
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        includePaths: ["node_modules"],
       },
     },
-    resolve: {
-      alias: [
-        { find: "@", replacement: path.resolve(__dirname, "src") },
-        {
-          find: "@material",
-          replacement: path.resolve(
-            __dirname,
-            "../../../node_modules/@material/"
-          ),
-        },
-      ],
-    },
-    css: {
-      preprocessorOptions: {
-        scss: {
-          includePaths: ["node_modules"],
-        },
-      },
-    },
-  });
+  },
+}));
