b22f0c7d818a36adf2a39aa4aac5563208efb896
joe-bell <joe@joebell.co.uk>
2021-07-20 12:46:34 +0300

    Explore possible proxy for development

--- services/orchest-webserver/client/vite.config.ts
+++ services/orchest-webserver/client/vite.config.ts
@@ -4,28 +4,35 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 import { vitePluginDesignSystem } from "@orchest/design-system-vite-plugin";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [reactRefresh(), vitePluginDesignSystem()],
-  server: {
-    host: "0.0.0.0",
-  },
-  resolve: {
-    alias: [
-      { find: "@", replacement: path.resolve(__dirname, "src") },
-      {
-        find: "@material",
-        replacement: path.resolve(
-          __dirname,
-          "../../../node_modules/@material/"
-        ),
-      },
-    ],
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        includePaths: ["node_modules"],
+export default (conditions) =>
+  defineConfig({
+    plugins: [reactRefresh(), vitePluginDesignSystem()],
+    server: {
+      host: "0.0.0.0",
+      proxy: conditions.mode === "development" && {
+        "^/async/*": {
+          target: "http://localhost:8000",
+          changeOrigin: true,
+        },
       },
     },
-  },
-});
+    resolve: {
+      alias: [
+        { find: "@", replacement: path.resolve(__dirname, "src") },
+        {
+          find: "@material",
+          replacement: path.resolve(
+            __dirname,
+            "../../../node_modules/@material/"
+          ),
+        },
+      ],
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          includePaths: ["node_modules"],
+        },
+      },
+    },
+  });
