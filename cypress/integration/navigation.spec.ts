eaa4517ca5594f1040777b0131f7533c1e0fd716
joe-bell <joe@joebell.co.uk>
2021-07-21 16:11:28 +0300

    Update navigation spec grouping

--- cypress/integration/navigation.spec.ts
+++ cypress/integration/navigation.spec.ts
@@ -3,22 +3,51 @@ describe("navigation", () => {
     // TBC
     // it("should go the default view on logo click", () => {});
 
-    it("should go to the help view on help icon click", () => {});
+    context("in pipeline view", () => {
+      context("regardless of read_only", () => {
+        it("should show the pipeline name", () => {});
+        it("should show the save indicator", () => {});
+      });
+
+      context("when read_only", () => {
+        it("should toggle the session", () => {});
+        it("should toggle the jupyterlab/pipeline view", () => {});
+      });
+    });
+
+    context("in any view", () => {
+      it("should logout", () => {});
+      it("should go to the help view", () => {});
+    });
   });
 
   context("project selector", () => {
-    it("should be visible on op views", () => {});
-    it("should not be visible on no-op views", () => {});
-    it("should change project when selected", () => {
-      // should stay in state after refresh
+    context("in any view", () => {
+      it("should change project when selected", () => {
+        // should stay in state after refresh
+      });
+    });
+
+    context("in no-op views", () => {
+      it("should not be visible", () => {
+        // should stay in state after refresh
+      });
+    });
+
+    context("in op views", () => {
+      it("should be visible", () => {
+        // should stay in state after refresh
+      });
     });
   });
 
   context("drawer", () => {
-    it("should toggled open by default", () => {});
+    context("visibility", () => {
+      it("should be open on first load", () => {});
 
-    it("should toggle", () => {
-      // Should stay in state after refresh
+      it("should toggle", () => {
+        // Should stay in state after refresh
+      });
     });
 
     context("links", () => {
@@ -26,7 +55,7 @@ describe("navigation", () => {
       //  Should have `aria-selected="true"`
       //  Should stay selected on reload
       //  Should still apply if drawer is closed/open
-      it("should show selected item on load", () => {});
+      it("should show selected item on first load", () => {});
       it("should visit selected item", () => {});
     });
   });
