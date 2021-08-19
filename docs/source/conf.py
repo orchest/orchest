69aaa99695e47612c546823e34f7b8cdc8f9a553
Rick Lamers <ricklamers@gmail.com>
2021-08-19 13:29:16 +0200

    Fix search box padding.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -77,4 +77,7 @@ html_theme_options = {
 # static files, so a file named "default.css" will overwrite the
 # builtin "default.css".
 html_static_path = ["_static"]
-html_static_path = []
+
+html_css_files = [
+    "css/custom_styles.css",
+]
