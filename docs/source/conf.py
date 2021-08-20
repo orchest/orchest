fb03004e7d1b445e91edeb7ff1b9a824c1ddb62f
Yannick <yannick.perrenet@gmail.com>
2021-08-20 14:28:20 +0200

    Initial try to integrate algolia search

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -71,6 +71,8 @@ html_theme_options = {
         ("Github", "https://github.com/orchest/orchest"),
     ]
 }
+html_sidebars = {"**": ["searchbox.html", "util/sidetoc.html"]}
+
 
 # Add any paths that contain custom static files (such as style sheets)
 # here, relative to this directory. They are copied after the builtin
