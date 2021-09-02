b3df9edfc12a1438dabf74f69c8e25af33990d16
Huang-Ming Chang <huang.m.chang@gmail.com>
2021-09-02 09:29:09 +0200

    add prettier/recommended

--- services/orchest-webserver/client/.eslintrc.js
+++ services/orchest-webserver/client/.eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   extends: [
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
