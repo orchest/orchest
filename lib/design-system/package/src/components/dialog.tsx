f4cc4fed8d9e08e2b86019613d497a4c3ad41d41
Rick Lamers <ricklamers@gmail.com>
2021-10-18 12:38:38 +0200

    Revert "Fix select dropdown in dialog gets cut off"

--- lib/design-system/package/src/components/dialog.tsx
+++ lib/design-system/package/src/components/dialog.tsx
@@ -1,10 +1,9 @@
+import * as React from "react";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
 import type * as Polymorphic from "@radix-ui/react-polymorphic";
-import * as React from "react";
-
+import { styled, keyframes } from "../core";
 import { ExtractVariants, ICSSProp } from "../types";
 import { Heading, THeadingComponent } from "./heading";
-import { keyframes, styled } from "../core";
 
 export enum DIALOG_ANIMATION_DURATION {
   IN = 200,
@@ -74,7 +73,8 @@ const DialogContentRoot = styled(DialogPrimitive.Content, {
   backgroundColor: "$background",
   borderRadius: "$md",
   boxShadow: "$2xl",
-  overflow: "visible",
+  overflowX: "hidden",
+  overflowY: "auto",
   "&:focus": {
     outline: "none",
   },
