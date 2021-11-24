11cde752af60a0e8b9e64ff77518cc7171b13304
Huang-Ming Chang <huang@orchest.io>
2021-11-24 22:15:28 +0100

    Upgrade React to 17.0.2

---
+++ services/orchest-webserver/client/src/theme.ts
@@ -0,0 +1,30 @@
+import { blue, grey, red } from "@mui/material/colors";
+import { createTheme } from "@mui/material/styles";
+import { ColorScale } from "./types";
+
+const ORCHEST_BLUE: ColorScale = {
+  50: "#e0eeff",
+  100: "#b0ccff",
+  200: "#7faaff",
+  300: "#4381ff",
+  400: "#1e65fe",
+  500: "#074ce5",
+  600: "#003bb3",
+  700: "#002a81",
+  800: "#001950",
+  900: "#000820",
+};
+
+const theme = createTheme({
+  palette: {
+    primary: { main: ORCHEST_BLUE["400"], ...blue },
+    secondary: { main: grey[900], ...grey },
+    error: { main: red.A400, ...red },
+    background: { default: grey[100] },
+  },
+  typography: {
+    fontFamily: '"Inter", "Helvetica", "Arial", sans-serif',
+  },
+});
+
+export default theme;
