1f3a209f1e15a64635ab21abf707028c3a1018be
Yannick <yannick.perrenet@gmail.com>
2021-12-07 18:57:12 +0100

    Remove ability to update params through SDK
    It does not make sense to expose functionality to update params through
    code because every pipeline run in a job get's its own project
    directory copy and so changing parameters inside jobs doesn't have any
    effect to begin with. The only use case that is left is using code to
    define and update parameters and deleting that code again, instead of
    using the UI to define the parameter.
    
    Due to how jobs are set up, there is no use case for dynamically
    updating parameters.

--- orchest-sdk/python/orchest/__init__.py
+++ orchest-sdk/python/orchest/__init__.py
@@ -2,12 +2,7 @@ import os as __os
 
 from orchest._version import __version__
 from orchest.config import Config
-from orchest.parameters import (
-    get_pipeline_param,
-    get_step_param,
-    update_pipeline_param,
-    update_step_param,
-)
+from orchest.parameters import get_pipeline_param, get_step_param
 from orchest.services import get_service, get_services
 from orchest.transfer import get_inputs, output
 
--- orchest-sdk/python/orchest/parameters.py
+++ orchest-sdk/python/orchest/parameters.py
@@ -4,10 +4,8 @@ Parameters are stored in the corresponding pipeline definition file,
 e.g. ``pipeline.orchest``.
 
 """
-import json
-from typing import Any, Optional, Tuple
+from typing import Any, Tuple
 
-from orchest.config import Config
 from orchest.error import StepUUIDResolveError
 from orchest.pipeline import Pipeline, PipelineStep
 from orchest.utils import get_pipeline, get_step_uuid
@@ -31,42 +29,6 @@ def get_params() -> Tuple[dict, dict]:
     pipeline = get_pipeline()
     step = _get_current_step(pipeline)
     return step.get_params(), pipeline.get_params()
-
-
-def update_params(
-    step_params: Optional[dict] = None, pipeline_params: Optional[dict] = None
-) -> None:
-    """Updates the parameters of the current step and of the pipeline.
-
-    Additionally, you can set new parameters by giving parameters that
-    do not yet exist in the current parameters, either of the step or of
-    the pipeline.
-
-    Internally the updating is done by calling the ``dict.update``
-    method. This further explains the behavior of this method.
-
-    Args:
-        step_params: The step parameters to update. Either updating
-            their values or adding new parameter keys.
-        pipeline_params: The pipeline parameters to update. Either
-            updating their values or adding new parameter keys.
-
-    Warning:
-        Updating the `pipeline_params` can lead to race conditions,
-        since different steps could be updating them at the same time.
-
-    """
-    pipeline = get_pipeline()
-
-    if pipeline_params is not None:
-        pipeline.update_params(pipeline_params)
-
-    if step_params is not None:
-        step = _get_current_step(pipeline)
-        step.update_params(step_params)
-
-    with open(Config.PIPELINE_DEFINITION_PATH, "w") as f:
-        json.dump(pipeline.to_dict(), f, indent=4, sort_keys=True)
 
 
 def get_step_param(name: str) -> Any:
@@ -96,34 +58,3 @@ def get_pipeline_param(name: str) -> Any:
     pipeline = get_pipeline()
     params = pipeline.get_params()
     return params[name]
-
-
-def update_step_param(name: str, value: Any) -> None:
-    """Updates or sets a step parameter.
-
-    Internally the updating is done by calling the ``dict.update``
-    method. This further explains the behavior of this method.
-
-    Args:
-        name: The step parameter to update/set.
-        value: The value that will be set.
-    """
-    return update_params(step_params={name: value})
-
-
-def update_pipeline_param(name: str, value: Any) -> None:
-    """Updates or sets a pipeline parameter.
-
-    Internally the updating is done by calling the ``dict.update``
-    method. This further explains the behavior of this method.
-
-    Args:
-        name: The pipeline parameter to update/set.
-        value: The value that will be set.
-
-    Warning:
-        Updating a pipeline parameter can lead to race conditions,
-        since different steps could be updating pipeline parameters at
-        the same time.
-    """
-    return update_params(pipeline_params={name: value})
