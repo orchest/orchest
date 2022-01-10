26f5aa8c664bf7d7a4295e5dacd3ff5740ac5006
Huang-Ming Chang <huang@orchest.io>
2022-01-10 10:46:58 +0100

    stringify value

--- services/orchest-webserver/client/src/job-view/common.ts
+++ services/orchest-webserver/client/src/job-view/common.ts
@@ -5,7 +5,9 @@ export const PARAMETERLESS_RUN = "Parameterless run";
 export const formatPipelineParams = (parameters: Record<string, Json>) => {
   return Object.values(parameters).map((parameter) => {
     return Object.entries(parameter)
-      .map(([key, value]) => `${key}: ${value}`)
+      .map(([key, value]) => {
+        return `${key}: ${JSON.stringify(value)}`;
+      })
       .join(", ");
   });
 };
