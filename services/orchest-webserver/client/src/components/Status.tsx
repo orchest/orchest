ae00a127a8e42022c0224812fff7e4f854370d42
Huang-Ming Chang <huang@orchest.io>
2022-01-10 13:30:03 +0100

    minor fixes: - restore the icon in JobStatus - DataTable default dense row height - get total run count from pagination data WIP: add cancel run, and delete in PipelineRunTable

--- services/orchest-webserver/client/src/components/Status.tsx
+++ services/orchest-webserver/client/src/components/Status.tsx
@@ -140,7 +140,7 @@ export const StatusGroup: React.FC<IStatusGroupProps> = ({
           height: (theme) => theme.spacing(3),
         }}
       >
-        {icon || statusMapping[status].icon}
+        {icon || statusMapping[status].icon()}
       </Box>
       <Typography
         component="span"
