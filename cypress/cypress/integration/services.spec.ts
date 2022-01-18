c1b075c73d19f1e7b55d226838f9ca3c18b1c24f
Huang-Ming Chang <huang@orchest.io>
2022-01-18 14:19:55 +0100

    fix flakiness in services.spec.ts

--- cypress/cypress/integration/services.spec.ts
+++ cypress/cypress/integration/services.spec.ts
@@ -93,7 +93,9 @@ describe("services", () => {
 
       it("tests for services connectivity in jobs", () => {
         cy.findByTestId(TEST_ID.JOB_RUN).click();
-        cy.findByTestId(`job-list`)
+        cy.url().should("include", "/jobs");
+        cy.reload();
+        cy.findByTestId(`job-list-row`)
           .first()
           .contains(SAMPLE_JOB_NAMES.J1)
           .click();
@@ -189,7 +191,12 @@ describe("services", () => {
     cy.findByTestId(TEST_ID.JOB_CREATE_NAME).type(SAMPLE_JOB_NAMES.J1);
     cy.findByTestId(TEST_ID.JOB_CREATE_OK).click();
     cy.findByTestId(TEST_ID.JOB_RUN).click();
-    cy.findByTestId(`job-list`).first().contains(SAMPLE_JOB_NAMES.J1).click();
+    cy.url().should("include", "/jobs");
+    cy.reload();
+    cy.findByTestId(`job-list-row`)
+      .first()
+      .contains(SAMPLE_JOB_NAMES.J1)
+      .click();
     waitForJobStatus(JOB_STATUS.SUCCESS);
     cy.intercept("GET", "/catch/api-proxy/api/jobs/*").as("jobData");
     cy.reload(true);
