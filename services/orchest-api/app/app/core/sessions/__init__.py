88a9a725d852b656b656cf0d1bfbb3f76ebb6b43
gobbijacopo91@gmail.com <gobbijacopo91@gmail.com>
2022-02-01 17:11:13 +0100

    Add module level docstring to sessions

--- services/orchest-api/app/app/core/sessions/__init__.py
+++ services/orchest-api/app/app/core/sessions/__init__.py
@@ -1,3 +1,16 @@
+"""Orchest session management, from start to stop and all in-between.
+
+The provided functions, generally speaking, work through the
+session_uuid as the only information required to interact with the
+session. They are, mostly, a wrapper around the k8s API combined with
+some internal business logic.
+
+Some functions allow to interact with a specific service or resource, it
+is responsibility of the caller to know what kind of service or resource
+a session was started with. Altough, implementation-wise, allowing for
+the retrieval of such information should be quite straightforward.
+
+"""
 from app.core.sessions._core import (
     SessionType,
     cleanup_resources,
