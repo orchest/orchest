4cc6d680804c595e95bb9dd2d4a29b4040a5f706
navid haghighat <navid.haghighat@gmail.com>
2022-03-03 20:12:49 +0100

    Add documentation and address reviews

--- services/node-agent/app/main.py
+++ services/node-agent/app/main.py
@@ -1,30 +1,54 @@
-import logging
-import asyncio
 import argparse
+import asyncio
+import logging
 
 from image_puller import ImagePuller, Policy
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    logger = logging.getLogger('node_agent')
+    logger = logging.getLogger("node_agent")
     logger.setLevel("INFO")
 
     logger.info("Starting node_agent.")
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--image-puller-log-level', dest='image_puller_log_level', nargs='?',
-                        help='Specifies log level, default "DEBUG".', default="DEBUG")
-    parser.add_argument('--image-puller-interval', dest='image_puller_interval', nargs='?',
-                        help='Specifies image puller interval in sec, default 60 sec.', 
-                        default=60, type=int)
-    parser.add_argument('--image-puller-policy', dest='image_puller_policy', nargs='?',
-                        help='Specifies image puller policy. default "IfNotPresent".', 
-                        default=Policy.IfNotPresent, type=Policy)
-    parser.add_argument('--image-puller-retries', dest='image_puller_retries', nargs='?',
-                        help='Specifies image puller number of retries. default 3.', 
-                        default=3, type=int)
-    parser.add_argument('--image-puller-images', dest='image_puller_images', nargs='+',
-                        help='Specifies image puller number of retries.')
+    parser.add_argument(
+        "--image-puller-log-level",
+        dest="image_puller_log_level",
+        nargs="?",
+        help='Specifies log level, default "DEBUG".',
+        default="DEBUG",
+    )
+    parser.add_argument(
+        "--image-puller-interval",
+        dest="image_puller_interval",
+        nargs="?",
+        help="Specifies image puller interval in sec, default 60 sec.",
+        default=60,
+        type=int,
+    )
+    parser.add_argument(
+        "--image-puller-policy",
+        dest="image_puller_policy",
+        nargs="?",
+        help='Specifies image puller policy. default "IfNotPresent".',
+        default=Policy.IfNotPresent,
+        type=Policy,
+    )
+    parser.add_argument(
+        "--image-puller-retries",
+        dest="image_puller_retries",
+        nargs="?",
+        help="Specifies image puller number of retries. default 3.",
+        default=3,
+        type=int,
+    )
+    parser.add_argument(
+        "--image-puller-images",
+        dest="image_puller_images",
+        nargs="+",
+        help="Specifies list of images to be pulled by image puller.",
+    )
 
     arguments = vars(parser.parse_args())
     image_puller = ImagePuller(**arguments)
