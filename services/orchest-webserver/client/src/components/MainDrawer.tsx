e43dc2c910728553036c0b640469b0a4cf0f7076
Huang-Ming Chang <huang@orchest.io>
2022-03-09 19:53:31 +0100

    highlight /jobs when viewing pipeline of a job run

--- services/orchest-webserver/client/src/components/MainDrawer.tsx
+++ services/orchest-webserver/client/src/components/MainDrawer.tsx
@@ -125,7 +125,7 @@ export const AppDrawer: React.FC<{ isOpen?: boolean }> = ({ isOpen }) => {
   const location = useLocation();
   const pathname = location.pathname;
 
-  const { navigateTo } = useCustomRoute();
+  const { navigateTo, jobUuid, runUuid } = useCustomRoute();
 
   const projectMenuItems = getProjectMenuItems(projectUuid);
 
@@ -140,8 +140,17 @@ export const AppDrawer: React.FC<{ isOpen?: boolean }> = ({ isOpen }) => {
 
   const isSelected = (path: string, exact = false) => {
     const route = routes.find((route) => route.path === pathname);
+
+    // this is a special case, we use "/pipeline" to present the pipeline of a job run
+    // this means that user is still viewing a job, so "/jobs" should be selected
+    const isJobRun = pathname === "/pipeline" && jobUuid && runUuid;
+
+    const pathToMatch = isJobRun
+      ? "/jobs"
+      : route?.root || route?.path || pathname;
+
     return (
-      matchPath(route?.root || route?.path || pathname, {
+      matchPath(pathToMatch, {
         path: path.split("?")[0],
         exact,
       }) !== null
