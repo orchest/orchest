2fe1face61b8b7b581ef7aeab386bd2ff69ac274
gobbijacopo91@gmail.com <gobbijacopo91@gmail.com>
2022-03-15 12:36:11 +0100

    Add a pipeline json migration system

--- services/orchest-webserver/app/app/compat.py
+++ services/orchest-webserver/app/app/compat.py
@@ -1,7 +1,23 @@
+"""Provides a simple migration layer for pipeline jsons.
+
+Given a pipeline version, a mapping will be used to establish if the
+pipeline should be migrated, that is, modified in place, until it is
+brought to the latest version used in Orchest.
+
+If you are to implement a new migration add another _migrate_...
+function following the given convention: 1.0.0 -> _migrate_1_0_0. Then
+add a mapping to the "_version_to_migration_function" dictionary to
+map the current version to the migration function and the version to
+which it will migrate.
+
+Note that the system only supports forward migrations.
+
+"""
 from _orchest.internals import utils as _utils
 
 
-def migrate_pipeline(pipeline):
+def _migrate_1_0_0(pipeline: dict) -> None:
+    """Pre-k8s migrations"""
 
     # Take care of old pipelines with no defined params.
     if "parameters" not in pipeline:
@@ -36,6 +52,20 @@ def migrate_pipeline(pipeline):
             tmp.add(valid_var)
         service["env_variables_inherit"] = list(tmp)
 
+    for step in pipeline["steps"].values():
+        if (
+            "kernel" in step
+            and "name" in step["kernel"]
+            and step["kernel"]["name"] == "ir"
+        ):
+            step["kernel"]["name"] = "r"
+
+
+def _migrate_1_1_0(pipeline: dict) -> None:
+    """Migrate from pre-k8s to k8s."""
+
+    services = pipeline.get("services", {})
+    for service in services.values():
         # Can't use a get() with a default value because it would imply
         # a different thing. Migrate from pre-k8s to k8s.
         if "entrypoint" in service:
@@ -45,21 +75,39 @@ def migrate_pipeline(pipeline):
 
             service["command"] = service["entrypoint"]
             del service["entrypoint"]
-        # Migrate from pre-k8s to k8s.
+
         if not service.get("ports", []):
             service["ports"] = [8080]
-        # Migrate from pre-k8s to k8s.
+
         service["exposed"] = service.get("exposed", False)
         service["requires_authentication"] = service.get(
             "requires_authentication", True
         )
 
-    for step in pipeline["steps"].values():
-        if (
-            "kernel" in step
-            and "name" in step["kernel"]
-            and step["kernel"]["name"] == "ir"
-        ):
-            step["kernel"]["name"] = "r"
 
-    return pipeline
+# version: (migration function, version to which it's migrated)
+_version_to_migration_function = {
+    "1.0.0": (_migrate_1_0_0, "1.1.0"),
+    "1.1.0": (_migrate_1_1_0, "1.2.0"),
+}
+
+# Make sure no forward version is repeated.
+__to_versions = [item[1] for item in _version_to_migration_function.items()]
+assert len(_version_to_migration_function) == len(__to_versions)
+# Make sure no migration function is repeated.
+__migration_functions = [item[0] for item in _version_to_migration_function.items()]
+assert len(__migration_functions) == len(__to_versions)
+
+
+def migrate_pipeline(pipeline: dict):
+    """Migrates a pipeline in place to the latest version."""
+
+    if not pipeline.get("version", ""):
+        pipeline["version"] = "1.0.0"
+
+    while pipeline["version"] in _version_to_migration_function:
+        migration_func, migrate_to_version = _version_to_migration_function[
+            pipeline["version"]
+        ]
+        migration_func(pipeline)
+        pipeline["version"] = migrate_to_version
