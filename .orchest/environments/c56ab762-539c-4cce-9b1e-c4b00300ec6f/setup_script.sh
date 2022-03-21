decf3857638c35f88cecaf4d8b5b192cc5a2a731
gobbijacopo91@gmail.com <gobbijacopo91@gmail.com>
2022-03-21 17:32:18 +0100

    Install required packages in setup script

--- .orchest/environments/c56ab762-539c-4cce-9b1e-c4b00300ec6f/setup_script.sh
+++ .orchest/environments/c56ab762-539c-4cce-9b1e-c4b00300ec6f/setup_script.sh
@@ -3,4 +3,4 @@
 # Install additional packages used throughout the pipeline. If
 # you want more granularity, you can use different environments 
 # for each step.
-# pip install vaex==4.2.0
+pip install pandas scikit-learn matplotlib
