1f8a9c50b0e7a5357176342fefc323aad94d5ddd
gobbijacopo91@gmail.com <gobbijacopo91@gmail.com>
2022-03-21 19:40:26 +0100

    Merge branching migrations

---
+++ services/orchest-api/app/migrations/versions/11462a4539f6_.py
@@ -0,0 +1,21 @@
+"""empty message
+
+Revision ID: 11462a4539f6
+Revises: 30aac32ff64b
+Create Date: 2022-03-21 18:38:14.311912
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "11462a4539f6"
+down_revision = "30aac32ff64b"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
