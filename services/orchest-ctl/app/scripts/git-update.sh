0b22acdc001e8323f6930536c647c1be68f4d1ab
gobbijacopo91@gmail.com <gobbijacopo91@gmail.com>
2022-04-01 09:37:50 +0200

    Fix pre-k8s update mechanism

--- services/orchest-ctl/app/scripts/git-update.sh
+++ services/orchest-ctl/app/scripts/git-update.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-[ "$(git rev-parse --abbrev-ref HEAD)" != "master" ] && exit 21
-
 # Get the user and group of the "orchest" shell script as this is most
 # likely also the user that should own the files in the repository.
 FILE_USER="$(ls -n /orchest-host/orchest | awk '{print $3}')"
@@ -14,6 +12,10 @@ FILE_GROUP="$(ls -n /orchest-host/orchest | awk '{print $4}')"
 git pull https://github.com/orchest/orchest.git --rebase
 # explicitly fetch tags
 git fetch https://github.com/orchest/orchest.git --tags
+
+git fetch origin stale/pre-k8s-orchest
+
+git checkout stale/pre-k8s-orchest
 
 # Change the user and group of all the files in the repository, except
 # for the userdir.
