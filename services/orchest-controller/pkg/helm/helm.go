73a77e3080c89998390b5a2aedea4cd822cdd216
navid haghighat <navid.haghighat@gmail.com>
2022-04-09 20:56:57 +0200

    add RemoveRelease method to helm module

--- services/orchest-controller/pkg/helm/helm.go
+++ services/orchest-controller/pkg/helm/helm.go
@@ -46,7 +46,6 @@ func GetReleaseConfig(ctx context.Context, name, namespace string) (Json, error)
 
 func DeployRelease(ctx context.Context, args []string) error {
 
-	return nil
 	cmd := exec.CommandContext(ctx, "helm", args...)
 
 	stdout := &bytes.Buffer{}
@@ -60,4 +59,29 @@ func DeployRelease(ctx context.Context, args []string) error {
 	}
 
 	return err
+}
+
+func RemoveRelease(ctx context.Context, name, namespace string) error {
+
+	klog.V(2).Infof("Attempting to remove the release: %s, namespace: %s", name, namespace)
+
+	args := NewHelmArgBuilder().
+		WithUnInstall().
+		WithName(name).
+		WithNamespace(namespace).
+		WithJsonOutput().Build()
+
+	cmd := exec.CommandContext(ctx, "helm", args...)
+
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
+	err := cmd.Run()
+	_, ok := err.(*exec.ExitError)
+	if ok {
+		return errors.Wrapf(err, "failed to remove helm deployment release: %s, namespace: %s", name, namespace)
+	}
+
+	return nil
 }
