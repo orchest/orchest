63f53cf9fcc990d69b1b2a3dca26d0a66e987aba
navid haghighat <navid.haghighat@gmail.com>
2022-04-13 01:20:42 +0200

    add arguments for configuring reconciler

--- services/orchest-controller/cmd/controller/cmd/controller.go
+++ services/orchest-controller/cmd/controller/cmd/controller.go
@@ -2,13 +2,14 @@ package cmd
 
 import (
 	"github.com/orchest/orchest/services/orchest-controller/pkg/manager"
+	"github.com/orchest/orchest/services/orchest-controller/pkg/reconciler/orchestcluster"
 	"github.com/spf13/cobra"
 	"k8s.io/klog/v2"
 )
 
 var (
-	deployDir string
-	inCluster bool
+	managerConfig    manager.ManagerConfig
+	reconcilerConfig orchestcluster.ReconcilerConfig
 )
 
 func NewControllerCommand() *cobra.Command {
@@ -25,8 +26,18 @@ func NewControllerCommand() *cobra.Command {
 		},
 	}
 
-	cmd.PersistentFlags().StringVar(&deployDir, "deployDir", "/deploy", "The directory which holds the deployment folders")
-	cmd.PersistentFlags().BoolVar(&inCluster, "inCluster", true, "In/Out cluster indicator")
+	cmd.PersistentFlags().StringVar(&reconcilerConfig.DeployDir, "deployDir", "/deploy", "The directory which holds the deployment folders")
+	cmd.PersistentFlags().StringVar(&reconcilerConfig.PostgresDefaultImage, "postgresImage", "postgres:13.1", "The default postgres image if not provided in CR")
+	cmd.PersistentFlags().StringVar(&reconcilerConfig.RabbitmqDefaultImage, "rabbitmqImage", "rabbitmq:3", "The default rabbitmq image if not provided in CR")
+	cmd.PersistentFlags().StringVar(&reconcilerConfig.UserdirDefaultVolumeSize, "userdirSize", "999Ti", "The default size for userdir pvc")
+	cmd.PersistentFlags().StringVar(&reconcilerConfig.ConfigdirDefaultVolumeSize, "configSize", "10Mi", "The default size for configdir pvc")
+	cmd.PersistentFlags().StringVar(&reconcilerConfig.BuilddirDefaultVolumeSize, "builderSize", "999Ti", "The default size for builddir pvc")
+	cmd.PersistentFlags().StringVar(&reconcilerConfig.OrchestDefaultTag, "defaultTag", "v2022.04.0", "The default tag for orchest components")
+	cmd.PersistentFlags().StringVar(&reconcilerConfig.CeleryWorkerImageName, "celeryImageName", "orchest/celery-worker", "The default celery-worker image name")
+	cmd.PersistentFlags().StringVar(&reconcilerConfig.OrchestApiImageName, "orchestApiImageName", "orchest/orchest-api", "The default orchest-api image name")
+	cmd.PersistentFlags().StringVar(&reconcilerConfig.OrchestWebserverImageName, "webserverImageName", "orchest/orchest-webserver", "The default orchest-webserver image name")
+	cmd.PersistentFlags().StringVar(&reconcilerConfig.AuthServerImageName, "authServerImageName", "999Ti", "The default auth server image name")
+	cmd.PersistentFlags().BoolVar(&managerConfig.InCluster, "inCluster", true, "In/Out cluster indicator")
 
 	return cmd
 }
@@ -34,7 +45,7 @@ func NewControllerCommand() *cobra.Command {
 func runControllerCmd() error {
 	klog.Info("running orchest controller")
 
-	mg, err := manager.NewManager(inCluster, deployDir)
+	mg, err := manager.NewManager(&managerConfig, &reconcilerConfig)
 	if err != nil {
 		klog.Fatal(err)
 	}
