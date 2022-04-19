b266f42e49d50e0b62d32394a80ebd8d3b9053ce
Huang-Ming Chang <huang@orchest.io>
2022-04-19 21:43:05 +0200

    use define

--- services/orchest-webserver/client/customTransformer.js
+++ services/orchest-webserver/client/customTransformer.js
@@ -6,25 +6,19 @@ const { createTransformer } = require("esbuild-jest");
 // https://mswjs.io/docs/getting-started/integrate/node#direct-usage
 // Therefore we need to replace the string literal `__BASE_URL__`
 // with testURL (NOTE: this `testURL` be the same as `testURL` in `jest.config.js`
-const testURL = "http://localhost:8080";
-
-const token = "__BASE_URL__";
-const regex = new RegExp(token, "g");
 
 const transformer = createTransformer({
   sourcemap: true,
   loaders: {
     ".test.ts": "tsx",
   },
+  define: {
+    __BASE_URL__: `"http://localhost:8080"`,
+  },
 });
 
 module.exports = {
   process(fileContent, filePath, jestConfig) {
-    return transformer.process(
-      fileContent.replace(regex, `"${testURL}"`),
-      fileContent,
-      filePath,
-      jestConfig
-    );
+    return transformer.process(fileContent, fileContent, filePath, jestConfig);
   },
 };
