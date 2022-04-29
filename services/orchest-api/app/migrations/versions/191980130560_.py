2c43f37979ce50fd5ea40b08399c7c0144fd3631
Yannick <yannick.perrenet@gmail.com>
2022-04-29 11:56:28 +0200

    Fix alembic revisions by merging heads

---
+++ services/orchest-api/app/migrations/versions/191980130560_.py
@@ -0,0 +1,23 @@
+"""empty message
+
+Revision ID: 191980130560
+Revises: 39faa4822903, 21f4696fb7af
+Create Date: 2022-04-29 09:49:36.179456
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "191980130560"
+down_revision = ("39faa4822903", "21f4696fb7af")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
