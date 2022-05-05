d7800ae9d6facb17d1903d1a707aaadb276cb874
Yannick <yannick.perrenet@gmail.com>
2022-05-05 17:35:44 +0200

    Fix development mode Flask services
    `orchest-api` and `auth-server` no longer use `flask run` and instead
    invoke through `python main.py`. `flask run` needs `FLASK_APP` env var
    to work which we are no longer passing.

--- services/orchest-api/app/main.py
+++ services/orchest-api/app/main.py
@@ -4,4 +4,4 @@ from config import CONFIG_CLASS
 app = create_app(config_class=CONFIG_CLASS, be_scheduler=True)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=80)
