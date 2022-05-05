9f3c518ecf746beb39f389a492d39fe7f8916dc6
navid haghighat <navid.haghighat@gmail.com>
2022-05-05 15:45:06 +0200

    reverse accidental change of BUILD_TAG and ORCHEST_VERSION

--- scripts/build_container.sh
+++ scripts/build_container.sh
@@ -7,8 +7,8 @@ SKIP_IMGS=()
 NO_CACHE=false
 VERBOSE=false
 ENABLE_SSL=false
-BUILD_TAG="v2022.04.1"
-ORCHEST_VERSION="v2022.04.1"
+BUILD_TAG="latest"
+ORCHEST_VERSION=$(git describe --tags)
 
 # Read flags.
 while getopts "s:i:t:no:vem" opt; do
