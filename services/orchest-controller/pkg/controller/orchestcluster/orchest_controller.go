b4c9ea126c035b10b84f73721393bd307b4a6e0c
navid haghighat <navid.haghighat@gmail.com>
2022-05-06 15:41:02 +0200

    fix update log message

--- services/orchest-controller/pkg/controller/orchestcluster/orchest_controller.go
+++ services/orchest-controller/pkg/controller/orchestcluster/orchest_controller.go
@@ -603,7 +603,7 @@ func (controller *OrchestClusterController) setDefaultIfNotSpecified(ctx context
 			return nil, errors.Wrapf(err, "failed to update orchest with default values  %q", orchest.Name)
 		}
 
-		klog.Info("OrchestCluster is updated with default values %s", orchest.Name)
+		klog.Infof("OrchestCluster is updated with default values %s", orchest.Name)
 		return result, nil
 
 	}
