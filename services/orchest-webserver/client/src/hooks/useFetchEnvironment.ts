7bad3ed566ef82f4ae57a0abd698fd9002bb86b2
Huang-Ming Chang <huang@orchest.io>
2022-05-23 18:00:28 +0200

    Apply useFetcher

--- services/orchest-webserver/client/src/hooks/useFetchEnvironment.ts
+++ services/orchest-webserver/client/src/hooks/useFetchEnvironment.ts
@@ -1,32 +1,22 @@
-import { useAsync } from "@/hooks/useAsync";
-import { Environment } from "@/types";
-import { fetcher } from "@orchest/lib-utils";
-import React from "react";
+import { useFetcher } from "./useFetcher";
 
 export const useFetchEnvironment = (
   props:
     | { projectUuid: string | undefined; environmentUuid: string | undefined }
     | undefined
 ) => {
-  const { run, data, setData, error, status } = useAsync<Environment>();
-
   const { projectUuid, environmentUuid } = props || {};
-
-  const sendRequest = React.useCallback(() => {
-    if (!projectUuid || !environmentUuid) return;
-    const url = `/store/environments/${projectUuid}/${environmentUuid}`;
-    return run(fetcher(url));
-  }, [run, projectUuid, environmentUuid]);
-
-  React.useEffect(() => {
-    sendRequest();
-  }, [sendRequest]);
+  const { data, setData, error, status, fetchData } = useFetcher(
+    projectUuid && environmentUuid
+      ? `/store/environments/${projectUuid}/${environmentUuid}`
+      : undefined
+  );
 
   return {
     environment: data,
     isFetchingEnvironment: status === "PENDING",
     error,
-    fetchEnvironment: sendRequest,
+    fetchEnvironment: fetchData,
     setEnvironment: setData,
   };
 };
