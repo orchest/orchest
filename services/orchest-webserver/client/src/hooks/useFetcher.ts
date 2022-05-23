640ff34a43213829e37c980fc46c155a70c50ef9
Huang-Ming Chang <huang@orchest.io>
2022-05-23 11:27:25 +0200

    useFetcher

---
+++ services/orchest-webserver/client/src/hooks/useFetcher.ts
@@ -0,0 +1,57 @@
+import { fetcher } from "@orchest/lib-utils";
+import React from "react";
+import { useAsync } from "./useAsync";
+import { useFocusBrowserTab } from "./useFocusBrowserTab";
+import { useHasChanged } from "./useHasChanged";
+
+export function useFetcher<FetchedValue, Data = FetchedValue>(
+  url: string | undefined,
+  params?: RequestInit & {
+    revalidateOnFocus?: boolean;
+    transform?: (data: FetchedValue) => Data;
+  }
+) {
+  const {
+    revalidateOnFocus = false,
+    transform = (fetchedValue: FetchedValue) =>
+      (fetchedValue as unknown) as Data,
+    ...fetchParams
+  } = React.useMemo(() => {
+    return params || {};
+  }, [params]);
+
+  const { run, data, setData, error, status } = useAsync<Data>();
+
+  const transformRef = React.useRef(transform);
+  React.useEffect(() => {
+    transformRef.current = transform;
+  }, [transform]);
+
+  const paramsRef = React.useRef(fetchParams);
+  React.useEffect(() => {
+    paramsRef.current = fetchParams;
+  }, [fetchParams]);
+
+  const isFocused = useFocusBrowserTab();
+  const hasBrowserFocusChanged = useHasChanged(isFocused);
+  const shouldRefetch =
+    revalidateOnFocus && hasBrowserFocusChanged && isFocused;
+
+  const fetchData = React.useCallback(() => {
+    if (!url) return;
+    return run(
+      fetcher<FetchedValue>(url, paramsRef.current).then(transformRef.current)
+    );
+  }, [run, url]);
+
+  const hasFetchedOnMount = React.useRef(false);
+
+  React.useEffect(() => {
+    if (!hasFetchedOnMount.current || shouldRefetch) {
+      hasFetchedOnMount.current = true;
+      fetchData();
+    }
+  }, [fetchData, shouldRefetch]);
+
+  return { data, setData, error, status, fetchData };
+}
