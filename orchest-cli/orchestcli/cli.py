618927ecd1e0f73b6a8e37305936ef23f160c736
navid haghighat <navid.haghighat@gmail.com>
2022-06-22 01:25:11 +0200

    edit socket_path help msg in cli

--- orchest-cli/orchestcli/cli.py
+++ orchest-cli/orchestcli/cli.py
@@ -183,7 +183,7 @@ def cli():
     "socket_path",  # name for arg
     default=None,
     show_default=True,
-    help="The path of the container runtime socket location.",
+    help="The absolute path of the container runtime socket on the host.",
 )
 @cli.command(cls=ClickCommonOptionsCmd)
 def install(
@@ -289,7 +289,7 @@ def update(
     "socket_path",  # name for arg
     default=None,
     show_default=True,
-    help="The path of the container runtime socket location.",
+    help="The absolute path of the container runtime socket on the host.",
 )
 def patch(
     dev: t.Optional[bool],
