031e4d69b2fd0d6a00627701cdf879c5badb287a
Yannick <26223174+yannickperrenet@users.noreply.github.com>
2022-06-23 12:18:06 +0200

    Add mdformat pre-commit hook
    Needs work...
    
    mdformat does not correctly format MyST files and has very limited
    configuration options.

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,15 +1,3 @@
 module.exports = {
   plugins: [require("prettier-plugin-organize-imports")],
-  overrides: [
-    {
-      files: "**.md",
-      options: {
-        printWidth: 100,
-        // Prettier markdown support doesn't support definition lists yet:
-        // https://github.com/prettier/prettier/issues/10701
-        // Afterwards can be put to "always"
-        proseWrap: "preserve",
-      },
-    },
-  ],
 };
