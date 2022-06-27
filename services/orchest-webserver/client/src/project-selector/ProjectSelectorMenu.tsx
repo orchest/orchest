5982cd59d5e6a870da40e1bda967d04b0405fa86
Huang-Ming Chang <huang@orchest.io>
2022-06-27 14:28:52 +0200

    Add SearchField

--- services/orchest-webserver/client/src/project-selector/ProjectSelectorMenu.tsx
+++ services/orchest-webserver/client/src/project-selector/ProjectSelectorMenu.tsx
@@ -1,17 +1,22 @@
+import Fade from "@mui/material/Fade";
 import Menu, { MenuProps } from "@mui/material/Menu";
 import React from "react";
 
-export const ProjectSelectorMenu: React.FC<{
-  open: boolean;
-  onClose: MenuProps["onClose"];
-}> = ({ children, open, onClose }) => {
+export const ProjectSelectorMenu = ({
+  children,
+  open,
+  onClose,
+  ...props
+}: MenuProps) => {
   return (
     <Menu
+      {...props}
       open={open}
       onClose={onClose}
       anchorReference="anchorPosition"
       marginThreshold={0}
       anchorPosition={{ top: 56, left: 0 }}
+      TransitionComponent={Fade}
       PaperProps={{
         style: {
           minHeight: "calc(100vh - 56px)",
