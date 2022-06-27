1bbcd795197a5d1415fb39b47a078aa1d280c10a
Huang-Ming Chang <huang@orchest.io>
2022-06-27 11:28:35 +0200

    Remove SessionToggle and Switch to JupyterLab btns

--- services/orchest-webserver/client/src/header-bar/HeaderBar.tsx
+++ services/orchest-webserver/client/src/header-bar/HeaderBar.tsx
@@ -1,64 +1,24 @@
 import { useAppContext } from "@/contexts/AppContext";
-import { useProjectsContext } from "@/contexts/ProjectsContext";
 import { useCustomRoute } from "@/hooks/useCustomRoute";
 import { useSessionsPoller } from "@/hooks/useSessionsPoller";
 import { siteMap } from "@/routingConfig";
-import StyledButtonOutlined from "@/styled-components/StyledButton";
-import DeviceHubIcon from "@mui/icons-material/DeviceHub";
 import HelpIcon from "@mui/icons-material/Help";
 import LogoutIcon from "@mui/icons-material/Logout";
-import ScienceIcon from "@mui/icons-material/Science";
 import AppBar from "@mui/material/AppBar";
 import Stack from "@mui/material/Stack";
 import Toolbar from "@mui/material/Toolbar";
 import React from "react";
-import { useRouteMatch } from "react-router-dom";
 import { IconButton } from "../components/common/IconButton";
-import SessionToggleButton from "../components/SessionToggleButton";
 import { ProjectSelector } from "../project-selector/ProjectSelector";
 import { NavigationTabs } from "./NavigationTabs";
 
 export const HeaderBar = () => {
-  const { navigateTo, pipelineUuid } = useCustomRoute();
-  const {
-    state: { projectUuid, pipeline, pipelineIsReadOnly },
-  } = useProjectsContext();
+  const { navigateTo } = useCustomRoute();
   const { user_config } = useAppContext();
   useSessionsPoller();
 
-  const matchPipeline = useRouteMatch({
-    path: siteMap.pipeline.path,
-    exact: true,
-  });
-
-  const matchFilePreview = useRouteMatch({
-    path: siteMap.filePreview.path,
-    exact: true,
-  });
-
-  const matchJupyter = useRouteMatch({
-    path: siteMap.jupyterLab.path,
-    exact: true,
-  });
-
   const showHelp = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     navigateTo(siteMap.help.path, undefined, e);
-  };
-
-  const showPipeline = (e: React.MouseEvent) => {
-    navigateTo(
-      siteMap.pipeline.path,
-      { query: { projectUuid, pipelineUuid } },
-      e
-    );
-  };
-
-  const showJupyter = (e: React.MouseEvent) => {
-    navigateTo(
-      siteMap.jupyterLab.path,
-      { query: { projectUuid, pipelineUuid } },
-      e
-    );
   };
 
   const logoutHandler = () => {
@@ -83,39 +43,6 @@ export const HeaderBar = () => {
         <ProjectSelector />
         <Stack spacing={2} direction="row" justifyContent="flex-end">
           <NavigationTabs />
-          {!matchFilePreview &&
-            pipeline &&
-            !pipelineIsReadOnly &&
-            projectUuid &&
-            pipelineUuid && (
-              <SessionToggleButton
-                pipelineUuid={pipelineUuid}
-                projectUuid={projectUuid}
-              />
-            )}
-          {pipeline && matchJupyter && (
-            <StyledButtonOutlined
-              variant="outlined"
-              color="secondary"
-              onClick={showPipeline}
-              onAuxClick={showPipeline}
-              startIcon={<DeviceHubIcon />}
-            >
-              Switch to Pipeline
-            </StyledButtonOutlined>
-          )}
-          {pipeline && !pipelineIsReadOnly && matchPipeline && (
-            <StyledButtonOutlined
-              variant="outlined"
-              color="secondary"
-              onClick={showJupyter}
-              onAuxClick={showJupyter}
-              startIcon={<ScienceIcon />}
-              data-test-id="switch-to-jupyterlab"
-            >
-              Switch to JupyterLab
-            </StyledButtonOutlined>
-          )}
           <Stack
             spacing={1}
             direction="row"
@@ -144,5 +71,3 @@ export const HeaderBar = () => {
     </AppBar>
   );
 };
-
-export default HeaderBar;
