aa594971fe8b35a9019c00ee1525e612754f3cb6
Huang-Ming Chang <huang@orchest.io>
2022-06-27 11:15:20 +0200

    remove isDrawerOpen state from AppContext

--- services/orchest-webserver/client/src/App.tsx
+++ services/orchest-webserver/client/src/App.tsx
@@ -36,9 +36,7 @@ $.fn.overflowing = function () {
 const App = () => {
   const [jupyter, setJupyter] = React.useState<Jupyter | null>(null);
   const { boot } = useIntercom();
-  const { setConfirm, isDrawerOpen, setIsDrawerOpen } = useAppContext();
-
-  const toggleDrawer = () => setIsDrawerOpen((currentValue) => !currentValue);
+  const { setConfirm } = useAppContext();
 
   // load server side config populated by flask template
   const {
@@ -103,7 +101,7 @@ const App = () => {
     >
       <AppInnerContextProvider>
         <Box sx={{ display: "flex", flex: 1, overflow: "hidden" }}>
-          <HeaderBar toggleDrawer={toggleDrawer} isDrawerOpen={isDrawerOpen} />
+          <HeaderBar />
           <Box
             component="main"
             sx={{
