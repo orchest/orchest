bf0f8bcf2e4d53df51ac65d20420c641ff6646ce
Huang-Ming Chang <huang@orchest.io>
2022-06-28 18:09:19 +0200

    Add TempLayout and TabPanel

---
+++ services/orchest-webserver/client/src/projects-view/ProjectTabPanel.tsx
@@ -0,0 +1,23 @@
+import Box, { BoxProps } from "@mui/material/Box";
+import React from "react";
+
+export const ProjectTabPanel: React.FC<
+  {
+    id: string;
+    index: number;
+    value: number;
+  } & BoxProps
+> = ({ value, index, id, children, sx, ...props }) => {
+  return (
+    <Box
+      role="tabpanel"
+      hidden={value !== index}
+      id={`${id}-tabpanel-${index}`}
+      aria-labelledby={`${id}-tab-${index}`}
+      sx={{ width: "100%", padding: (theme) => theme.spacing(4, 0), ...sx }}
+      {...props}
+    >
+      {value === index && children}
+    </Box>
+  );
+};
