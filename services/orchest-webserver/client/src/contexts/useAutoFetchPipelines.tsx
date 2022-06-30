707504f2b4afa1649a6c39526beb9e099ac3be5d
Huang-Ming Chang <huang@orchest.io>
2022-06-30 17:37:09 +0200

    Fix test for useAutoFetchPipelines

--- services/orchest-webserver/client/src/contexts/useAutoFetchPipelines.tsx
+++ services/orchest-webserver/client/src/contexts/useAutoFetchPipelines.tsx
@@ -2,17 +2,18 @@ import { useCustomRoute } from "@/hooks/useCustomRoute";
 import { useFetchPipelines } from "@/hooks/useFetchPipelines";
 import { useMatchRoutePaths } from "@/hooks/useMatchProjectRoot";
 import { withinProjectRoutes } from "@/routingConfig";
+import { hasValue } from "@orchest/lib-utils";
 import React from "react";
 import { useProjectsContext } from "./ProjectsContext";
 
-export const useAutoFetchPipelines = () => {
+export const useAutoFetchPipelinesBase = (
+  projectUuidFromRoute: string | undefined,
+  shouldFetch: boolean
+) => {
   const { state, dispatch } = useProjectsContext();
-  const { projectUuid: projectUuidFromRoute } = useCustomRoute();
-
-  const matchWithinProjectRoutes = useMatchRoutePaths(withinProjectRoutes);
 
   const { pipelines, status } = useFetchPipelines(
-    projectUuidFromRoute === state.projectUuid && matchWithinProjectRoutes
+    projectUuidFromRoute === state.projectUuid && shouldFetch
       ? projectUuidFromRoute
       : undefined
   );
@@ -22,6 +23,18 @@ export const useAutoFetchPipelines = () => {
       dispatch({ type: "LOAD_PIPELINES", payload: pipelines });
     }
   }, [status, pipelines, dispatch]);
+
+  return pipelines;
+};
+
+export const useAutoFetchPipelines = () => {
+  const { projectUuid: projectUuidFromRoute } = useCustomRoute();
+  const matchWithinProjectRoutes = useMatchRoutePaths(withinProjectRoutes);
+
+  const pipelines = useAutoFetchPipelinesBase(
+    projectUuidFromRoute,
+    hasValue(matchWithinProjectRoutes)
+  );
 
   return pipelines;
 };
