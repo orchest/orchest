b0bcf764acfc131475a52b25483b1fe7548dca76
mausworks <rasmuswennerstrom@gmail.com>
2022-07-11 00:24:29 +0200

    feat: memoize createStep in useCreateStep

--- services/orchest-webserver/client/src/pipeline-view/hooks/useCreateStep.tsx
+++ services/orchest-webserver/client/src/pipeline-view/hooks/useCreateStep.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { createStepAction } from "../action-helpers/eventVarsHelpers";
 import { usePipelineCanvasContext } from "../contexts/PipelineCanvasContext";
 import { usePipelineEditorContext } from "../contexts/PipelineEditorContext";
@@ -11,27 +12,31 @@ export const useCreateStep = () => {
   } = usePipelineEditorContext();
   const { pipelineCanvasState } = usePipelineCanvasContext();
 
-  return (fileName?: string) => {
-    if (!pipelineViewportRef.current) {
-      console.error("Failed to create step: pipeline viewport not set");
-      return;
-    }
+  // Use the first environment as the default:
+  // The user can change it later.
+  const [environment] = environments;
 
-    // Use the first environment as the default:
-    // The user it can be changed later.
-    const [environment] = environments;
+  const createStep = React.useMemo(
+    () => (fileName?: string) => {
+      if (pipelineViewportRef.current) {
+        // When new steps are successively created then we don't want
+        // them to be spawned on top of each other.
+        // NOTE: we use the same offset for X and Y position.
+        const { clientWidth, clientHeight } = pipelineViewportRef.current;
+        const [offsetX, offsetY] = pipelineCanvasState.pipelineOffset;
 
-    // When new steps are successively created then we don't want
-    // them to be spawned on top of each other.
-    // NOTE: we use the same offset for X and Y position.
-    const { clientWidth, clientHeight } = pipelineViewportRef.current;
-    const [offsetX, offsetY] = pipelineCanvasState.pipelineOffset;
+        const position = {
+          x: -offsetX + clientWidth / 2 - STEP_WIDTH / 2,
+          y: -offsetY + clientHeight / 2 - STEP_HEIGHT / 2,
+        };
 
-    const position = {
-      x: -offsetX + clientWidth / 2 - STEP_WIDTH / 2,
-      y: -offsetY + clientHeight / 2 - STEP_HEIGHT / 2,
-    };
+        dispatch(createStepAction(environment, position, fileName));
+      } else {
+        console.error("Failed to create step: pipeline viewport not set");
+      }
+    },
+    [pipelineCanvasState, dispatch, pipelineViewportRef, environment]
+  );
 
-    dispatch(createStepAction(environment, position, fileName));
-  };
+  return createStep;
 };
