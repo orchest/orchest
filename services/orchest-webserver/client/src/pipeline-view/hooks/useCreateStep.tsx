55f97502b6a2fe5ab739fa64e58ba3642e0bf4a7
mausworks <rasmuswennerstrom@gmail.com>
2022-07-12 10:11:57 +0200

    fix: ensure leading slashes are trimmed from step file paths
    The file browser will include it, but the step API does not support it.

--- services/orchest-webserver/client/src/pipeline-view/hooks/useCreateStep.tsx
+++ services/orchest-webserver/client/src/pipeline-view/hooks/useCreateStep.tsx
@@ -4,6 +4,12 @@ import { usePipelineCanvasContext } from "../contexts/PipelineCanvasContext";
 import { usePipelineEditorContext } from "../contexts/PipelineEditorContext";
 import { STEP_HEIGHT, STEP_WIDTH } from "../PipelineStep";
 
+/**
+ * Removes leading slashes from file paths as the step API does not support them.
+ */
+const stepPath = (path?: string) =>
+  path?.startsWith("/") ? path.substring(1) : path;
+
 export const useCreateStep = () => {
   const {
     dispatch,
@@ -19,7 +25,7 @@ export const useCreateStep = () => {
   const [environment] = environments;
 
   const createStep = React.useMemo(
-    () => (fileName?: string) => {
+    () => (filePath?: string) => {
       if (pipelineViewportRef.current) {
         // When new steps are successively created then we don't want
         // them to be spawned on top of each other.
@@ -32,7 +38,7 @@ export const useCreateStep = () => {
           y: -offsetY + clientHeight / 2 - STEP_HEIGHT / 2,
         };
 
-        dispatch(createStepAction(environment, position, fileName));
+        dispatch(createStepAction(environment, position, stepPath(filePath)));
       } else {
         console.error("Failed to create step: pipeline viewport not set");
       }
