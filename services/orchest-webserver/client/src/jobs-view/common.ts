c72180ca818ab9c61d3d6afea6f9a4d674c98853
Huang-Ming Chang <huang@orchest.io>
2022-07-14 18:18:23 +0200

    feat: finalize PipelineOperationsMenu

---
+++ services/orchest-webserver/client/src/jobs-view/common.ts
@@ -0,0 +1,28 @@
+import { Job } from "@/types";
+import { checkGate } from "@/utils/webserver-utils";
+import { fetcher, HEADER } from "@orchest/lib-utils";
+
+export const requestCreateJob = async (
+  projectUuid: string,
+  newJobName: string,
+  pipelineUuid: string,
+  pipelineName: string
+) => {
+  await checkGate(projectUuid);
+  return fetcher<Job>("/catch/api-proxy/api/jobs/", {
+    method: "POST",
+    headers: HEADER.JSON,
+    body: JSON.stringify({
+      pipeline_uuid: pipelineUuid,
+      project_uuid: projectUuid,
+      pipeline_name: pipelineName, // ? Question: why pipeline_name is needed when pipeline_uuid is given?
+      name: newJobName,
+      draft: true,
+      pipeline_run_spec: {
+        run_type: "full",
+        uuids: [],
+      },
+      parameters: [],
+    }),
+  });
+};
