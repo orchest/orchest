56d0aa90afa14ab371c8c5ae4f492cbc005c81da
Huang-Ming Chang <huang@orchest.io>
2022-07-15 14:51:57 +0200

    feat: add new operations to hotkeys.
    Also move scheduleJob into useRunSteps.

--- services/orchest-webserver/client/src/pipeline-view/hooks/useHotKeysInPipelineEditor.tsx
+++ services/orchest-webserver/client/src/pipeline-view/hooks/useHotKeysInPipelineEditor.tsx
@@ -1,23 +1,33 @@
 import { useHotKeys } from "@/hooks/useHotKeys";
 import React from "react";
-import { useInteractiveRunsContext } from "../contexts/InteractiveRunsContext";
 import { usePipelineEditorContext } from "../contexts/PipelineEditorContext";
+import { useRunSteps } from "../pipeline-canvas-header-bar/pipeline-operations/useRunSteps";
+
+const COMMANDS = {
+  SELECT_ALL: ["ctrl+a", "command+a"],
+  RUN_ALL: ["ctrl+shift+enter", "command+shift+enter"],
+  RUN_SELECTED: ["ctrl+enter", "command+enter"],
+  RUN_INCOMING: ["ctrl+i", "command+i"],
+  SCHEDULE_JOB: ["ctrl+j", "command+j"],
+};
+
+const commandsString = Object.values(COMMANDS).join(", ");
 
 export const useHotKeysInPipelineEditor = () => {
   const { eventVars, dispatch } = usePipelineEditorContext();
-  const { runSteps } = useInteractiveRunsContext();
   const [isHoverEditor, setIsHoverEditor] = React.useState(false);
-
-  // TODO: add the new operations in `PipelineOperationsMenu`.
+  const {
+    runAllSteps,
+    runSelectedSteps,
+    runIncomingSteps,
+    scheduleJob,
+  } = useRunSteps();
 
   const { setScope } = useHotKeys(
     {
       "pipeline-editor": {
-        "ctrl+a, command+a, ctrl+shift+enter, command+shift+enter, ctrl+enter, command+enter": (
-          e,
-          hotKeyEvent
-        ) => {
-          if (["ctrl+a", "command+a"].includes(hotKeyEvent.key)) {
+        [commandsString]: (e, hotKeyEvent) => {
+          if (COMMANDS.SELECT_ALL.includes(hotKeyEvent.key)) {
             e.preventDefault();
 
             dispatch({
@@ -25,19 +35,17 @@ export const useHotKeysInPipelineEditor = () => {
               payload: { uuids: Object.keys(eventVars.steps) },
             });
           }
-          if (
-            ["ctrl+shift+enter", "command+shift+enter"].includes(
-              hotKeyEvent.key
-            )
-          ) {
-            runSteps(Object.keys(eventVars.steps), "selection");
+          if (COMMANDS.RUN_ALL.includes(hotKeyEvent.key)) {
+            runAllSteps?.();
           }
-          if (
-            ["ctrl+shift+enter", "command+shift+enter"].includes(
-              hotKeyEvent.key
-            )
-          ) {
-            runSteps(eventVars.selectedSteps, "selection");
+          if (COMMANDS.RUN_SELECTED.includes(hotKeyEvent.key)) {
+            runSelectedSteps?.();
+          }
+          if (COMMANDS.RUN_INCOMING.includes(hotKeyEvent.key)) {
+            runIncomingSteps?.();
+          }
+          if (COMMANDS.SCHEDULE_JOB.includes(hotKeyEvent.key)) {
+            scheduleJob?.();
           }
         },
       },
