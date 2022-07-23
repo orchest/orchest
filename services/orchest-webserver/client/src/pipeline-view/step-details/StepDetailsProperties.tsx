ef9e8b9340183bb136ca98c1231b12fe5f0706ab
mausworks <rasmuswennerstrom@gmail.com>
2022-07-24 00:18:23 +0200

    feat: make the side details panel appear more like the new design

--- services/orchest-webserver/client/src/pipeline-view/step-details/StepDetailsProperties.tsx
+++ services/orchest-webserver/client/src/pipeline-view/step-details/StepDetailsProperties.tsx
@@ -1,20 +1,13 @@
 import ProjectFilePicker from "@/components/ProjectFilePicker";
-import { SortableStack } from "@/components/SortableStack";
 import { Step } from "@/types";
 import { hasExtension, join } from "@/utils/path";
-import { ellipsis } from "@/utils/styles";
 import { toValidFilename } from "@/utils/toValidFilename";
-import CloseOutlined from "@mui/icons-material/CloseOutlined";
-import DragHandleOutlined from "@mui/icons-material/DragHandleOutlined";
-import { IconButton } from "@mui/material";
-import Box from "@mui/material/Box";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
 import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
-import Typography from "@mui/material/Typography";
 import {
   ALLOWED_STEP_EXTENSIONS,
   kernelNameToLanguage,
@@ -23,11 +16,6 @@ import React from "react";
 import { SelectEnvironment } from "./SelectEnvironment";
 import { useStepDetailsContext } from "./StepDetailsContext";
 import { StepParameters } from "./StepParameters";
-
-export type ConnectionDict = Record<
-  string,
-  { title: string; file_path: string }
->;
 
 const KERNEL_OPTIONS = [
   { value: "python", label: "Python" },
@@ -46,18 +34,14 @@ export const StepDetailsProperties = ({
   pipelineCwd: string | undefined;
   readOnly: boolean;
   shouldAutoFocus: boolean;
-  onSave: (
-    payload: Partial<Step>,
-    uuid: string,
-    replace?: boolean
-  ) => Promise<void>;
+  onSave: (payload: Partial<Step>, uuid: string, replace?: boolean) => void;
   menuMaxWidth?: string;
 }) => {
-  const { step, connections } = useStepDetailsContext();
-  // Allows user to edit JSON while typing the text will not be valid JSON.
-  const [editableParameters, setEditableParameters] = React.useState(
-    JSON.stringify(step.parameters, null, 2)
-  );
+  const {
+    step,
+    doesStepFileExist,
+    isCheckingFileValidity,
+  } = useStepDetailsContext();
 
   const isNotebookStep = hasExtension(step.file_path, "ipynb");
   const titleInputRef = React.useRef<HTMLInputElement>();
@@ -121,29 +105,6 @@ export const StepDetailsProperties = ({
       onSave({ title: updatedTitle, ...filePathChange }, step.uuid);
   };
 
-  const swapConnectionOrder = (
-    oldConnectionIndex: number,
-    newConnectionIndex: number
-  ) => {
-    // check if there is work to do
-    if (oldConnectionIndex !== newConnectionIndex) {
-      // note it's creating a reference
-      const connections = [...step.incoming_connections];
-
-      let tmp = connections[oldConnectionIndex];
-      connections.splice(oldConnectionIndex, 1);
-      connections.splice(newConnectionIndex, 0, tmp);
-
-      onSave({ incoming_connections: connections }, step.uuid);
-    }
-  };
-
-  const removeConnection = (index: number) => {
-    const connections = step.incoming_connections.filter((_, i) => i !== index);
-
-    onSave({ incoming_connections: connections }, step.uuid, true);
-  };
-
   React.useEffect(() => {
     if (step.file_path.length === 0) {
       onChangeFilePath(toValidFilename(step.title));
@@ -157,124 +118,69 @@ export const StepDetailsProperties = ({
   });
 
   return (
-    <div className="detail-subview">
-      <Stack direction="column" spacing={3}>
+    <Stack direction="column" spacing={3}>
+      <TextField
+        autoFocus={shouldAutoFocus}
+        value={step.title}
+        onChange={(e) => onChangeTitle(e.target.value)}
+        label="Step name"
+        disabled={readOnly}
+        fullWidth
+        inputRef={titleInputRef}
+        data-test-id="step-title-textfield"
+      />
+      {readOnly ? (
         <TextField
-          autoFocus={shouldAutoFocus}
-          value={step.title}
-          onChange={(e) => onChangeTitle(e.target.value)}
-          label="Title"
+          value={step.file_path}
+          label="File path"
           disabled={readOnly}
           fullWidth
-          inputRef={titleInputRef}
-          data-test-id="step-title-textfield"
+          margin="normal"
+          data-test-id="step-file-name-textfield"
         />
-        {readOnly ? (
-          <TextField
-            value={step.file_path}
-            label="File name"
+      ) : (
+        <ProjectFilePicker
+          value={step.file_path}
+          allowedExtensions={ALLOWED_STEP_EXTENSIONS}
+          pipelineCwd={pipelineCwd}
+          onChange={onChangeFilePath}
+          menuMaxWidth={menuMaxWidth}
+          doesFileExist={doesStepFileExist}
+          isCheckingFileValidity={isCheckingFileValidity}
+        />
+      )}
+      {isNotebookStep && (
+        <FormControl fullWidth>
+          <InputLabel id="kernel-language-label">Kernel language</InputLabel>
+          <Select
+            label="Kernel language"
+            labelId="kernel-language-label"
+            id="kernel-language"
+            value={step.kernel.name}
             disabled={readOnly}
-            fullWidth
-            margin="normal"
-            data-test-id="step-file-name-textfield"
-          />
-        ) : (
-          <ProjectFilePicker
-            value={step.file_path}
-            allowedExtensions={ALLOWED_STEP_EXTENSIONS}
-            pipelineCwd={pipelineCwd}
-            onChange={onChangeFilePath}
-            menuMaxWidth={menuMaxWidth}
-            doesFileExist={doesStepFileExist}
-            isCheckingFileValidity={isCheckingFileValidity}
-          />
-        )}
-        {isNotebookStep && (
-          <FormControl fullWidth>
-            <InputLabel id="kernel-language-label">Kernel language</InputLabel>
-            <Select
-              label="Kernel language"
-              labelId="kernel-language-label"
-              id="kernel-language"
-              value={step.kernel.name}
-              disabled={readOnly}
-              onChange={(e) => onChangeKernel(e.target.value)}
-            >
-              {KERNEL_OPTIONS.map((option) => {
-                return (
-                  <MenuItem key={option.value} value={option.value}>
-                    {option.label}
-                  </MenuItem>
-                );
-              })}
-            </Select>
-          </FormControl>
-        )}
-        <SelectEnvironment
-          value={step.environment}
-          disabled={readOnly}
-          queryString={
-            isNotebookStep
-              ? `?language=${kernelNameToLanguage(step.kernel.name)}`
-              : ""
-          }
-          onChange={onChangeEnvironment}
-        />
-        <StepParameters isReadOnly={readOnly} onSave={onSave} />
-        {step.incoming_connections.length != 0 && (
-          <Box>
-            <Typography
-              component="h3"
-              variant="subtitle2"
-              sx={{ marginBottom: (theme) => theme.spacing(1) }}
-            >
-              Connections
-            </Typography>
-
-            <SortableStack
-              onUpdate={async (oldIndex, newIndex) =>
-                swapConnectionOrder(oldIndex, newIndex)
-              }
-            >
-              {step.incoming_connections.map((startNodeUUID, i) => (
-                <Stack
-                  flexDirection="row"
-                  key={startNodeUUID}
-                  alignItems="center"
-                  justifyContent="space-between"
-                  sx={{ width: "100%" }}
-                >
-                  <Stack
-                    flexShrink={1}
-                    flexDirection="row"
-                    gap={1.5}
-                    minWidth="0"
-                  >
-                    <DragHandleOutlined
-                      style={{ width: "24px", height: "24px" }}
-                    />
-                    <Typography sx={ellipsis()} variant="body2" fontSize={14}>
-                      {i + 1}. {connections[startNodeUUID].title}
-                    </Typography>
-                    <Typography
-                      sx={ellipsis()}
-                      variant="body2"
-                      fontSize={14}
-                      color="text.secondary"
-                    >
-                      {connections[startNodeUUID].file_path}
-                    </Typography>
-                  </Stack>
-
-                  <IconButton onClick={() => removeConnection(i)}>
-                    <CloseOutlined style={{ width: "20px", height: "20px" }} />
-                  </IconButton>
-                </Stack>
-              ))}
-            </SortableStack>
-          </Box>
-        )}
-      </Stack>
-    </div>
+            onChange={(e) => onChangeKernel(e.target.value)}
+          >
+            {KERNEL_OPTIONS.map((option) => {
+              return (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+      )}
+      <SelectEnvironment
+        value={step.environment}
+        disabled={readOnly}
+        queryString={
+          isNotebookStep
+            ? `?language=${kernelNameToLanguage(step.kernel.name)}`
+            : ""
+        }
+        onChange={onChangeEnvironment}
+      />
+      <StepParameters isReadOnly={readOnly} onSave={onSave} />
+    </Stack>
   );
 };
