b244cdad6e4ab9c8bd373d06e0b53606c862476b
Huang-Ming Chang <huang@orchest.io>
2022-07-25 18:09:57 +0200

    feat: finalize read-only banner for a job run.

---
+++ services/orchest-webserver/client/src/pipeline-view/ReadOnlyBanner.tsx
@@ -0,0 +1,77 @@
+import { useCustomRoute } from "@/hooks/useCustomRoute";
+import { siteMap } from "@/routingConfig";
+import VisibilityIcon from "@mui/icons-material/VisibilityOutlined";
+import { Alert, AlertTitle } from "@mui/material";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import Stack from "@mui/material/Stack";
+import { hasValue } from "@orchest/lib-utils";
+import React from "react";
+import { usePipelineDataContext } from "./contexts/PipelineDataContext";
+
+const generateReadOnlyMessage = (jobName?: string) =>
+  jobName ? (
+    <>
+      {`This is a read-only pipeline snapshot from `}
+      <b>{jobName}</b>
+      {` job. Make edits in the Pipeline editor.`}
+    </>
+  ) : null;
+
+export const ReadOnlyBanner = () => {
+  const { navigateTo } = useCustomRoute();
+
+  const {
+    job,
+    isReadOnly,
+    isJobRun,
+    projectUuid,
+    pipelineUuid,
+    pipeline,
+  } = usePipelineDataContext();
+
+  const goToPipeline = (e: React.MouseEvent) => [
+    navigateTo(
+      siteMap.pipeline.path,
+      { query: { projectUuid, pipelineUuid } },
+      e
+    ),
+  ];
+
+  const isShowing =
+    isJobRun && isReadOnly && hasValue(job) && hasValue(pipeline);
+
+  return isShowing ? (
+    <Box
+      className="pipeline-actions"
+      sx={{ padding: (theme) => theme.spacing(2.5) }}
+    >
+      <Alert
+        color="info"
+        icon={<VisibilityIcon />}
+        sx={{
+          width: "100%",
+          ".MuiAlert-message": { width: "100%" },
+        }}
+      >
+        <AlertTitle sx={{ width: "100%" }}>
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            sx={{ width: "100%" }}
+          >
+            Read-only: pipeline snapshot
+            <Link
+              sx={{ cursor: "pointer" }}
+              onClick={goToPipeline}
+              onAuxClick={goToPipeline}
+            >
+              {`VIEW ${pipeline.path}`}
+            </Link>
+          </Stack>
+        </AlertTitle>
+        {generateReadOnlyMessage(job.name)}
+      </Alert>
+    </Box>
+  ) : null;
+};
