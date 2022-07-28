e1465d87c1ba72b0a9e713767dde448c0b5ae693
mausworks <rasmuswennerstrom@gmail.com>
2022-07-28 23:22:12 +0200

    fix: minor type issue

--- services/orchest-webserver/client/src/pipeline-view/file-manager/common.tsx
+++ services/orchest-webserver/client/src/pipeline-view/file-manager/common.tsx
@@ -1,5 +1,5 @@
 import { Code } from "@/components/common/Code";
-import { StepState } from "@/types";
+import { StepData } from "@/types";
 import {
   basename,
   dirname,
@@ -243,7 +243,7 @@ export const removeLeadingSymbols = (filePath: string) =>
 
 // user might enter "./foo.ipynb", but it's equivalent to "foo.ipynb".
 // this function cleans up the leading "./"
-export const getStepFilePath = (step: StepState) =>
+export const getStepFilePath = (step: StepData) =>
   removeLeadingSymbols(step.file_path);
 
 export const searchFilePathsByExtension = ({
@@ -303,7 +303,7 @@ export const findFilesByExtension = async ({
  */
 export const validateFiles = (
   currentStepUuid: string | undefined,
-  steps: Record<string, StepState> | undefined,
+  steps: Record<string, StepData> | undefined,
   selectedFiles: string[]
 ) => {
   const allNotebookFileSteps = Object.values(steps || {}).reduce(
@@ -314,7 +314,7 @@ export const validateFiles = (
       }
       return all;
     },
-    [] as StepState[]
+    [] as StepData[]
   );
 
   return selectedFiles.reduce(
