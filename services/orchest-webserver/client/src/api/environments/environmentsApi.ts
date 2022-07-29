e7a278f6e57c8b10cc83217318aaee0d3d600e2a
Huang-Ming Chang <huang@orchest.io>
2022-07-29 23:34:22 +0200

    fix: useFetchEnvironments and useGetEnvironments.
    Place useFetchEnvironments at the entrypoint of the views, and
    place useGetEnvironments to slice `environments` from the state.
    Make useFetchEnvironments refetch when tab regains focus.
    Make validate refetch environments if the given state is outdated.

--- services/orchest-webserver/client/src/api/environments/environmentsApi.ts
+++ services/orchest-webserver/client/src/api/environments/environmentsApi.ts
@@ -2,45 +2,85 @@ import { queryArgs } from "@/pipeline-view/file-manager/common";
 import {
   Environment,
   EnvironmentSpec,
+  EnvironmentState,
   EnvironmentValidationData,
 } from "@/types";
-import { fetcher, HEADER } from "@orchest/lib-utils";
+import { fetcher, FetchError, HEADER } from "@orchest/lib-utils";
+import produce from "immer";
 
-export const environmentsApi = {
-  getAll: (projectUuid: string, language?: string) => {
-    const queryString = language ? `?${queryArgs({ language })}` : "";
-    return fetcher<Environment[]>(
-      `/store/environments/${projectUuid}${queryString}`
-    );
-  },
-  post: (projectUuid: string, name: string, spec: EnvironmentSpec) =>
-    fetcher<Environment>(`/store/environments/${projectUuid}/new`, {
+const fetchAll = (projectUuid: string, language?: string) => {
+  const queryString = language ? `?${queryArgs({ language })}` : "";
+  return fetcher<Environment[]>(
+    `/store/environments/${projectUuid}${queryString}`
+  );
+};
+const post = (projectUuid: string, name: string, spec: EnvironmentSpec) =>
+  fetcher<Environment>(`/store/environments/${projectUuid}/new`, {
+    method: "POST",
+    headers: HEADER.JSON,
+    body: JSON.stringify({
+      environment: { ...spec, uuid: "new", name },
+    }),
+  });
+const put = (projectUuid: string, environment: Environment) =>
+  fetcher<Environment>(
+    `/store/environments/${projectUuid}/${environment.uuid}`,
+    {
+      method: "PUT",
+      headers: HEADER.JSON,
+      body: JSON.stringify({ environment }),
+    }
+  );
+const remove = (projectUuid: string, environmentUuid: string) =>
+  fetcher<void>(`/store/environments/${projectUuid}/${environmentUuid}`, {
+    method: "DELETE",
+  });
+const validate = async (projectUuid: string) =>
+  fetcher<EnvironmentValidationData>(
+    `/catch/api-proxy/api/validations/environments`,
+    {
       method: "POST",
       headers: HEADER.JSON,
-      body: JSON.stringify({
-        environment: { ...spec, uuid: "new", name },
-      }),
-    }),
-  put: (projectUuid: string, environment: Environment) =>
-    fetcher<Environment>(
-      `/store/environments/${projectUuid}/${environment.uuid}`,
-      {
-        method: "PUT",
-        headers: HEADER.JSON,
-        body: JSON.stringify({ environment }),
+      body: JSON.stringify({ project_uuid: projectUuid }),
+    }
+  );
+
+const validateLatest = async (
+  projectUuid: string,
+  existingEnvironments: EnvironmentState[] = []
+): Promise<[EnvironmentState[], EnvironmentValidationData] | FetchError> => {
+  try {
+    const response = await validate(projectUuid);
+    const envMap = new Map(existingEnvironments.map((env) => [env.uuid, env]));
+    const updatedEnvironmentMap = produce(envMap, (draft) => {
+      for (const [index, action] of response.actions.entries()) {
+        const uuid = response.fail[index];
+        const environment = draft.get(uuid);
+        if (environment) {
+          environment.action = action;
+        } else {
+          throw "refetch";
+        }
       }
-    ),
-  delete: (projectUuid: string, environmentUuid: string) =>
-    fetcher<void>(`/store/environments/${projectUuid}/${environmentUuid}`, {
-      method: "DELETE",
-    }),
-  validate: async (projectUuid: string) =>
-    fetcher<EnvironmentValidationData>(
-      `/catch/api-proxy/api/validations/environments`,
-      {
-        method: "POST",
-        headers: HEADER.JSON,
-        body: JSON.stringify({ project_uuid: projectUuid }),
-      }
-    ),
+    });
+    const environments = Array.from(
+      updatedEnvironmentMap,
+      ([, value]) => value
+    );
+    return [environments, response];
+  } catch (error) {
+    if (error === "refetch") {
+      const latestEnvironments = await fetchAll(projectUuid);
+      return validateLatest(projectUuid, latestEnvironments);
+    }
+    return new FetchError(error);
+  }
+};
+
+export const environmentsApi = {
+  fetchAll,
+  post,
+  put,
+  delete: remove,
+  validate: validateLatest,
 };
