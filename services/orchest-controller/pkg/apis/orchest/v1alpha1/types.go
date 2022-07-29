f2458cbf4da84c6fb8b6595087da878208a51665
navid haghighat <navid.haghighat@gmail.com>
2022-07-29 19:49:49 +0200

    make singleNode=true default and pass it through cli

--- services/orchest-controller/pkg/apis/orchest/v1alpha1/types.go
+++ services/orchest-controller/pkg/apis/orchest/v1alpha1/types.go
@@ -217,7 +217,7 @@ type ApplicationSpec struct {
 type OrchestClusterSpec struct {
 	// Wether Orchest is Single Node or not, if specified, all pods of the orchest
 	// including session pods will be scheduled on the same node.
-	SingleNode bool `json:"singleNode,omitempty"`
+	SingleNode *bool `json:"singleNode,omitempty"`
 
 	// NodeSelector is a selector which must be true for the pod to fit on a node.
 	// Selector which must match a node's labels for the pod to be scheduled on that node.
