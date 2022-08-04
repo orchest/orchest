274e5fd352cdcdac6057a0bdc3820031e6850f93
Huang-Ming Chang <huang@orchest.io>
2022-08-04 12:10:07 +0200

    fix: env on edit not updated on clicking build.
    environmentOnEdit should get updated only if latestBuild.status
    changes.

--- services/orchest-webserver/client/src/environments-view/stores/useEnvironmentOnEdit.tsx
+++ services/orchest-webserver/client/src/environments-view/stores/useEnvironmentOnEdit.tsx
@@ -1,10 +1,17 @@
+import { useEnvironmentsApi } from "@/api/environments/useEnvironmentsApi";
+import { useHasChanged } from "@/hooks/useHasChanged";
 import { EnvironmentState } from "@/types";
+import React from "react";
 import create from "zustand";
 
-export const useEnvironmentOnEdit = create<{
+const useEnvironmentOnEditStore = create<{
   environmentOnEdit?: EnvironmentState;
   initEnvironmentOnEdit: (payload: EnvironmentState | undefined) => void;
-  setEnvironmentOnEdit: (payload: Partial<EnvironmentState>) => void;
+  setEnvironmentOnEdit: (
+    payload:
+      | Partial<EnvironmentState>
+      | ((state: EnvironmentState) => Partial<EnvironmentState>)
+  ) => void;
 }>((set) => ({
   initEnvironmentOnEdit: (value) => {
     set({ environmentOnEdit: value });
@@ -12,9 +19,49 @@ export const useEnvironmentOnEdit = create<{
   setEnvironmentOnEdit: (value) => {
     set((state) => {
       if (!state.environmentOnEdit) return state;
+      const updatedPayload =
+        value instanceof Function ? value(state.environmentOnEdit) : value;
       return {
-        environmentOnEdit: { ...state.environmentOnEdit, ...value },
+        environmentOnEdit: { ...state.environmentOnEdit, ...updatedPayload },
       };
     });
   },
 }));
+
+/**
+ * Manages the state of Environment on Edit, and serves as the source of truth for updating an Environment.
+ * Gets updated if latestBuild.status changes.
+ */
+export const useEnvironmentOnEdit = () => {
+  const {
+    environmentOnEdit,
+    setEnvironmentOnEdit,
+    initEnvironmentOnEdit,
+  } = useEnvironmentOnEditStore();
+  const { environments } = useEnvironmentsApi();
+  const environmentOnEditFromStore = environments?.find(
+    (env) => env.uuid === environmentOnEdit?.uuid
+  );
+
+  const hasLatestBuildStatusChanged = useHasChanged(
+    environmentOnEditFromStore,
+    (prev, curr) => {
+      if (!prev || !curr) return false;
+      return prev.latestBuild?.status !== curr.latestBuild?.status;
+    }
+  );
+
+  React.useEffect(() => {
+    if (hasLatestBuildStatusChanged) {
+      setEnvironmentOnEdit({
+        latestBuild: environmentOnEditFromStore?.latestBuild,
+      });
+    }
+  }, [
+    setEnvironmentOnEdit,
+    hasLatestBuildStatusChanged,
+    environmentOnEditFromStore,
+  ]);
+
+  return { environmentOnEdit, setEnvironmentOnEdit, initEnvironmentOnEdit };
+};
