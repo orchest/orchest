48fbfbab90a61ff378bc6c15b24d3f24c1aba1d3
Rick Lamers <ricklamers@gmail.com>
2022-08-04 13:03:00 +0200

    Refactor build to allow inlining CSS.

--- lib/javascript/build-frontend.sh
+++ lib/javascript/build-frontend.sh
@@ -4,18 +4,37 @@ set -e
 NODE_MODULES_PATH=node_modules
 BIN_PATH="$(npm bin)"
 PATH="$BIN_PATH:$PATH"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 
 function index_template_fill() {
   # Template transform index.html
   JS_PATH_PLACEHOLDER="{{JS_PATH}}"
-  CSS_PATH_PLACEHOLER="{{CSS_PATH}}"
+  CSS_BLOCK_PLACEHOLDER="{{CSS_BLOCK}}"
   JS_HASH_PLACEHOLDER="{{JS_HASH}}"
   JS_FILE_NAME="$1"
   CSS_FILE_NAME="$2"
 
-  cat public/index.html | sed "s/$JS_PATH_PLACEHOLDER/$JS_FILE_NAME/g" |
-    sed "s/$CSS_PATH_PLACEHOLER/$CSS_FILE_NAME/g" |
-    sed "s/$JS_HASH_PLACEHOLDER/$JS_FILE_NAME/g" >dist/index.html
+  CSS_BLOCK_FILE="dist/css-block.html"
+
+  if [ $3 == "inject-css" ]; then
+    echo "<style>$(cat ${CSS_FILE_NAME})</style>" > $CSS_BLOCK_FILE
+  else
+    echo "<link href=\"${CSS_FILE_NAME}\" rel=\"stylesheet\" />" > $CSS_BLOCK_FILE
+  fi
+  
+  # Replace JS_FILE
+  RES=$($DIR/replace.py public/index.html "$JS_PATH_PLACEHOLDER" "$JS_FILE_NAME")
+  echo "$RES" > dist/index.html
+  # Replace JS_HASH
+  RES=$($DIR/replace.py dist/index.html "$JS_HASH_PLACEHOLDER" "$JS_FILE_NAME")
+  echo "$RES" > dist/index.html
+  # Replace CSS_BLOCK
+  RES=$($DIR/replace.py dist/index.html "$CSS_BLOCK_PLACEHOLDER" "$CSS_BLOCK_FILE" file)
+  echo "$RES" > dist/index.html
+
+  # Cleanup
+  rm $CSS_BLOCK_FILE
 }
 
 WATCH=false
@@ -51,7 +70,7 @@ if $WATCH; then
   DATE_UNIX=$(date +%s)
   DEBUG_JS_FILENAME="main.js?hash=${DATE_UNIX}"
   DEBUG_CSS_FILENAME="style.css?hash=${DATE_UNIX}"
-  index_template_fill $DEBUG_JS_FILENAME $DEBUG_CSS_FILENAME
+  index_template_fill $DEBUG_JS_FILENAME $DEBUG_CSS_FILENAME "watch"
   sass $SASS_ARGS --watch &
   esbuild $ESBUILD_ARGS --watch --sourcemap # only generate sourcemap when pnpm run dev
 else
@@ -67,10 +86,8 @@ else
   fi
 
   JS_BUNDLE_HASH=$($md5_hash_command $JS_BUNDLE_PATH | cut -d ' ' -f 1)
-  CSS_BUNDLE_HASH=$($md5_hash_command $CSS_BUNDLE_PATH | cut -d ' ' -f 1)
 
   JS_FILE_NAME="main.js?hash=$JS_BUNDLE_HASH"
-  CSS_FILE_NAME="style.css?hash=$CSS_BUNDLE_HASH"
 
-  index_template_fill "$JS_FILE_NAME" "$CSS_FILE_NAME"
+  index_template_fill "$JS_FILE_NAME" "$CSS_BUNDLE_PATH" "inject-css"
 fi
