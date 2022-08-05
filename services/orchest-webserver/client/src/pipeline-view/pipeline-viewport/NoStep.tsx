e30edb1d9c2394da65b6e29fce02f7b6d808e523
mausworks <rasmuswennerstrom@gmail.com>
2022-08-05 22:25:37 +0200

    feat: add ViewportCenterMessage.

--- services/orchest-webserver/client/src/pipeline-view/pipeline-viewport/NoStep.tsx
+++ services/orchest-webserver/client/src/pipeline-view/pipeline-viewport/NoStep.tsx
@@ -1,38 +1,15 @@
-import { ViewDocsLink } from "@/components/common/ViewDocsLink";
-import Box from "@mui/material/Box";
-import Stack from "@mui/material/Stack";
-import Typography from "@mui/material/Typography";
+import { StackProps } from "@mui/material/Stack";
 import React from "react";
+import { ViewportCenterMessage } from "./ViewportCenterMessage";
 
-export const NoStep = () => {
+export const NoStep = (props: StackProps) => {
   return (
-    <Stack
-      direction="column"
-      alignItems="center"
-      sx={{ marginTop: (theme) => theme.spacing(4) }}
-    >
-      <Box
-        component="img"
-        src="/image/no-step.svg"
-        sx={{
-          width: "24%",
-          maxWidth: (theme) => theme.spacing(40),
-          margin: (theme) => theme.spacing(2, 0, 0, 0),
-        }}
-      />
-      <Typography variant="h5">No Pipeline Steps</Typography>
-      <Typography
-        variant="body1"
-        align="center"
-        sx={{
-          width: (theme) => theme.spacing(44),
-          marginTop: (theme) => theme.spacing(1),
-        }}
-      >
-        A Pipeline Step is an executable file running in its own isolated
-        environment. Drag & drop files from the side panel to get started.
-      </Typography>
-      <ViewDocsLink sx={{ marginTop: (theme) => theme.spacing(4) }} />
-    </Stack>
+    <ViewportCenterMessage
+      imgSrc="/image/no-step.svg"
+      title="No Pipeline Steps"
+      description={`A Pipeline Step is an executable file running in its own isolated 
+      environment. Drag & drop files from the side panel to get started.`}
+      {...props}
+    />
   );
 };
