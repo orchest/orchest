042fd3274a32edcb1a5adc3280cede414a98e492
mausworks <rasmuswennerstrom@gmail.com>
2022-08-05 22:28:30 +0200

    chore: enable dev mode and remove MUI warnings.

--- services/orchest-webserver/client/src/components/common/IconButton.tsx
+++ services/orchest-webserver/client/src/components/common/IconButton.tsx
@@ -2,9 +2,9 @@ import MuiIconButton, { IconButtonProps } from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
 import React from "react";
 
-export const IconButton: React.FC<IconButtonProps> = (props) => {
-  return props.title ? (
-    <Tooltip title={props.title}>
+export const IconButton: React.FC<IconButtonProps> = ({ title, ...props }) => {
+  return title ? (
+    <Tooltip title={title}>
       <MuiIconButton {...props} />
     </Tooltip>
   ) : (
