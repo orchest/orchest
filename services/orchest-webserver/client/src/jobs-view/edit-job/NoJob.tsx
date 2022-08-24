e5905a88d3b84757d719279e31145cbfb1dd545a
Huang-Ming Chang <huang@orchest.io>
2022-08-24 10:10:06 +0200

    fix: Add svg for NoJob; update dockPath.

--- services/orchest-webserver/client/src/jobs-view/edit-job/NoJob.tsx
+++ services/orchest-webserver/client/src/jobs-view/edit-job/NoJob.tsx
@@ -2,14 +2,14 @@ import { ViewportCenterMessage } from "@/pipeline-view/pipeline-viewport/compone
 import Stack, { StackProps } from "@mui/material/Stack";
 import React from "react";
 
-export const NoEnvironment = (props: StackProps) => {
+export const NoJob = (props: StackProps) => {
   return (
     <Stack justifyContent="center" alignItems="center" sx={{ height: "100%" }}>
       <ViewportCenterMessage
-        imgSrc="/image/no-environment.svg"
-        title="No Environments"
-        description={`Environments define the conditions in which Pipeline steps execute scripts and kernels.`}
-        docPath="https://docs.orchest.io/en/stable/fundamentals/environments.html"
+        imgSrc="/image/no-job.svg"
+        title="No Jobs"
+        description={`Jobs are a way to schedule one-off or recurring Pipelines runs. Jobs can run multiple iterations of the same Pipeline; taking different parameters as inputs.`}
+        docPath="https://docs.orchest.io/en/stable/fundamentals/jobs.html"
         {...props}
       />
     </Stack>
