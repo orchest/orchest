507023713faca481bfe8dbd7aa3f692e6572445c
Huang-Ming Chang <huang@orchest.io>
2022-08-24 10:38:10 +0200

    fix: extract useAutoSave

---
+++ services/orchest-webserver/client/src/jobs-view/hooks/useAutoSaveJob.tsx
@@ -0,0 +1,25 @@
+import { shallowEqualByKey } from "@/environments-view/common";
+import { useAutoSave } from "@/hooks/useAutoSave";
+import type { JobChangesData } from "@/types";
+
+const isJobChanged = (prev: JobChangesData, curr: JobChangesData) =>
+  !shallowEqualByKey(prev, curr, [
+    "name",
+    "schedule",
+    "env_variables",
+    "max_retained_pipeline_runs",
+    "parameters",
+  ]);
+
+export const useAutoSaveJob = (
+  value: JobChangesData | undefined,
+  save: () => void
+) => {
+  useAutoSave(value, save, (prev, curr) => {
+    const isLoadingPage = !prev || !curr;
+    if (isLoadingPage) return false;
+    const isRedirectingToAnotherJob = prev.uuid !== curr.uuid;
+    if (isRedirectingToAnotherJob) return false;
+    return isJobChanged(prev, curr);
+  });
+};
