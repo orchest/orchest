c84fe4f4318bfd5cb3c2ae86bf1136dd326caf12
Huang-Ming Chang <huang@orchest.io>
2022-08-25 17:03:49 +0200

    fix: EditJobPipeline, not yet able to save in BE.
    BE currently doesn't allow jobs to change pipeline_uuid. Need to work
    around it by deleting and creating a new job.

--- services/orchest-webserver/client/src/jobs-view/edit-job/JobOverview.tsx
+++ services/orchest-webserver/client/src/jobs-view/edit-job/JobOverview.tsx
@@ -3,6 +3,7 @@ import Typography from "@mui/material/Typography";
 import React from "react";
 import { JobAccordion, useJobAccordions } from "./components/JobAccordion";
 import { EditJobName } from "./EditJobName";
+import { EditJobPipeline } from "./EditJobPipeline";
 
 export const JobOverview = () => {
   const { isOverviewOpen, setIsOverviewOpen } = useJobAccordions();
@@ -21,8 +22,16 @@ export const JobOverview = () => {
           Overview
         </Typography>
       </AccordionSummary>
-      <AccordionDetails sx={{ paddingTop: (theme) => theme.spacing(2) }}>
+      <AccordionDetails
+        sx={{
+          paddingTop: (theme) => theme.spacing(2),
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+        }}
+      >
         <EditJobName />
+        <EditJobPipeline />
       </AccordionDetails>
     </JobAccordion>
   );
