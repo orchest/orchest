836847ed1a7a99a7d4a2e1694f6c7dc9fb3d14d1
mausworks <rasmuswennerstrom@gmail.com>
2022-09-01 11:58:54 +0200

    fix: improve the find predicate and tests

--- services/orchest-webserver/client/src/utils/__tests__/array.test.ts
+++ services/orchest-webserver/client/src/utils/__tests__/array.test.ts
@@ -1,69 +1,69 @@
 import { deduplicates, replaces } from "../array";
 
-describe("replacing array entries", () => {
-  it("replaces the first entry that matches the predicate", () => {
-    const replace = replaces((entry) => entry === "x", "ignore");
+describe("replacing array items", () => {
+  it("replaces the first item that matches the predicate", () => {
+    const replace = replaces((item) => item === "x", "ignore");
 
     expect(replace(["a", "x", "c", "x"], "r")).toEqual(["a", "r", "c", "x"]);
   });
 
-  it("calls the predicate for each entry when none match", () => {
-    const entries = ["a", "b", "c"];
+  it("calls the predicate for each item when none match", () => {
+    const items = ["a", "b", "c"];
     const predicate = jest.fn(() => false);
     const replace = replaces<string>(predicate, "ignore");
 
-    replace(entries, "_");
+    replace(items, "_");
 
-    expect(predicate).toHaveBeenCalledTimes(entries.length);
+    items.forEach((item, i) =>
+      expect(predicate).toHaveBeenNthCalledWith(i + 1, item, i, items)
+    );
+    expect(predicate).toHaveBeenCalledTimes(items.length);
   });
 
   it("does not modify the source array", () => {
-    const entries = ["a", "b", "c"];
-    const replace = replaces<string>((entry) => entry === "b", "ignore");
+    const source = ["a", "b", "c"];
+    const replace = replaces<string>((item) => item === "b", "ignore");
 
-    expect(replace(entries, "r")).toEqual(["a", "r", "c"]);
-    expect(entries).toEqual(["a", "b", "c"]);
+    expect(replace(source, "r")).toEqual(["a", "r", "c"]);
+    expect(source).toEqual(["a", "b", "c"]);
   });
 });
 
 describe("replacement fallback strategies", () => {
   it("does nothing when set to 'ignore'", () => {
-    const replace = replaces<string>((entry) => entry === null, "ignore");
+    const replace = replaces<string>((item) => item === null, "ignore");
 
     expect(replace(["a", "b", "c"], "r")).toEqual(["a", "b", "c"]);
   });
 
   it("uses 'ignore' by default", () => {
-    const replace = replaces<string>((entry) => entry === null, "x" as any);
+    const replace = replaces<string>((item) => item === null, "x" as any);
 
     expect(replace(["a", "b", "c"], "r")).toEqual(["a", "b", "c"]);
   });
 
   it("adds the item in the back when set to 'push'", () => {
-    const replace = replaces<string>((entry) => entry === null, "push");
+    const replace = replaces<string>((item) => item === null, "push");
 
     expect(replace(["a", "b", "c"], "r")).toEqual(["a", "b", "c", "r"]);
   });
 
   it("adds the item in the front when set to 'unshift'", () => {
-    const replace = replaces<string>((entry) => entry === null, "unshift");
+    const replace = replaces<string>((item) => item === null, "unshift");
 
     expect(replace(["a", "b", "c"], "r")).toEqual(["r", "a", "b", "c"]);
   });
 });
 
 describe("deduplicating arrays", () => {
-  it("removes entries that share the same key", () => {
-    const deduplicate = deduplicates<string>((s) => s);
-    const input = ["a", "b", "c", "b", "c", "a", "b", "c"];
+  it("it merges arrays and omits items that share the same key", () => {
+    const deduplicate = deduplicates<string>((key) => key);
+    const input = [
+      ["a", "b", "c", "b"],
+      ["c", "a", "b", "c"],
+    ];
     const expected = ["a", "b", "c"];
 
-    expect(deduplicate(input)).toEqual(expected);
-  });
-
-  it("merges multiple arrays", () => {
-    const deduplicate = deduplicates<string>((s) => s);
-
-    expect(deduplicate(["a", "b"], ["c", "d"])).toEqual(["a", "b", "c", "d"]);
+    expect(deduplicate(...input)).toEqual(expected);
   });
 });
