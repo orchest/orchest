c8fba7d1f2416d0a811b7226a68395346a34d71d
Huang-Ming Chang <huang@orchest.io>
2022-09-01 14:54:16 +0200

    fix: reset jobChanges when switching jobs

--- services/orchest-webserver/client/src/jobs-view/hooks/useSelectJob.tsx
+++ services/orchest-webserver/client/src/jobs-view/hooks/useSelectJob.tsx
@@ -1,22 +1,24 @@
 import { useCustomRoute } from "@/hooks/useCustomRoute";
 import { siteMap } from "@/routingConfig";
 import React from "react";
+import { useEditJob } from "../stores/useEditJob";
 
 /**
  * Provides `selectJob` that navigate to the Environment.
  */
 export const useSelectJob = () => {
   const { navigateTo, projectUuid } = useCustomRoute();
-
+  const resetJobChanges = useEditJob((state) => state.resetJobChanges);
   const selectJob = React.useCallback(
     (jobUuid: string) => {
       if (projectUuid) {
+        resetJobChanges();
         navigateTo(siteMap.jobs.path, {
           query: { projectUuid, jobUuid },
         });
       }
     },
-    [navigateTo, projectUuid]
+    [navigateTo, projectUuid, resetJobChanges]
   );
 
   return { selectJob };
