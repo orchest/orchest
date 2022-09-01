0eb9ec4e6324055374f73cbf14e70f8be75cfb11
Huang-Ming Chang <huang@orchest.io>
2022-09-01 17:39:27 +0200

    fix: move useUpdateJobOnUnmount to JobMenuList

--- services/orchest-webserver/client/src/jobs-view/hooks/useSelectJob.tsx
+++ services/orchest-webserver/client/src/jobs-view/hooks/useSelectJob.tsx
@@ -1,24 +1,24 @@
 import { useCustomRoute } from "@/hooks/useCustomRoute";
 import { siteMap } from "@/routingConfig";
 import React from "react";
-import { useEditJob } from "../stores/useEditJob";
+import { useUpdateJobOnUnmount } from "./useUpdateJobOnUnmount";
 
 /**
  * Provides `selectJob` that navigate to the Environment.
  */
 export const useSelectJob = () => {
   const { navigateTo, projectUuid } = useCustomRoute();
-  const resetJobChanges = useEditJob((state) => state.resetJobChanges);
+  const { updateJobAndReset } = useUpdateJobOnUnmount();
   const selectJob = React.useCallback(
     (jobUuid: string) => {
       if (projectUuid) {
-        resetJobChanges();
+        updateJobAndReset();
         navigateTo(siteMap.jobs.path, {
           query: { projectUuid, jobUuid },
         });
       }
     },
-    [navigateTo, projectUuid, resetJobChanges]
+    [navigateTo, projectUuid, updateJobAndReset]
   );
 
   return { selectJob };
