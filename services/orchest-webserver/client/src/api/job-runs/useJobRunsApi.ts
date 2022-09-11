546ff548b882d6782c090167e4fbf9b487d33fb7
mausworks <rasmuswennerstrom@gmail.com>
2022-09-12 00:44:17 +0200

    fix: ensure page is updated

--- services/orchest-webserver/client/src/api/job-runs/useJobRunsApi.ts
+++ services/orchest-webserver/client/src/api/job-runs/useJobRunsApi.ts
@@ -1,9 +1,7 @@
-import { TStatus } from "@/components/Status";
 import { createJobStore } from "@/store/scoped";
-import { JobRunsPage, PipelineRun } from "@/types";
+import { JobRunsPage, PipelineRun, PipelineRunStatus } from "@/types";
 import { choke } from "@/utils/promise";
-import produce from "immer";
-import { jobRunsApi, StatusUpdate, StepStatusUpdate } from "./jobRunsApi";
+import { jobRunsApi, JobRunsPageQuery, StatusUpdate } from "./jobRunsApi";
 
 export type JobRunsApi = {
   /** A list of all the currently fetched runs, in no particular order. */
@@ -15,76 +13,71 @@ export type JobRunsApi = {
   /** Fetches a single job run and adds (or replaces) it in `runs`. */
   fetchOne: (runUuid: string) => Promise<void>;
   /** Fetches a job runs page and updates the `page` property. */
-  fetchPage: (page: number, pageSize?: number) => Promise<void>;
-  /** Cancels a job run and updates the it in `runs` (if it includes it). */
+  fetchPage: (query: JobRunsPageQuery) => Promise<void>;
+  /** Cancels a job run and updates the it in `runs` & `page` (if it includes it). */
   cancel: (runUuid: string) => Promise<void>;
-  /** Updates the status of a job run in the back-end and updates it in `runs` (if it includes it). */
+  /** Updates the status of a job run in the back-end and updates it in `runs` & `page` (if it includes it). */
   setStatus: (update: StatusUpdate) => Promise<void>;
-  /** Updates the status of a job run step in the back-end and updates it in `runs` (if it includes it). */
-  setStepStatus: (update: StepStatusUpdate) => Promise<void>;
 };
 
 /** A state container for job runs within the active job scope. */
 export const useJobRunsApi = createJobStore<JobRunsApi>((set, get) => {
-  const setRunStatus = (runUuid: string, status: TStatus) =>
-    set((state) => ({
-      runs: produce(state.runs || [], (draft) => {
-        draft.forEach(
-          (run) => (run.status = run.uuid === runUuid ? status : run.status)
-        );
-      }),
-    }));
+  const setRunStatus = (
+    runs: PipelineRun[],
+    runUuid: string,
+    status: PipelineRunStatus
+  ) => runs.map((run) => (run.uuid === runUuid ? { ...run, status } : run));
 
-  const replaceOrAddRun = (newRun: PipelineRun) => {
-    const { runs = [] } = get();
-
-    if (runs.find((run) => run.uuid === newRun.uuid)) {
+  const replaceOrAddRun = (
+    runs: PipelineRun[] | undefined,
+    newRun: PipelineRun
+  ) => {
+    if (runs?.find((run) => run.uuid === newRun.uuid)) {
       return runs?.map((run) => (run.uuid === newRun.uuid ? newRun : run));
     } else {
-      return [...runs, newRun];
+      return [...(runs || []), newRun];
     }
   };
 
+  const updateStatus = (runUuid: string, status: PipelineRunStatus) => {
+    set((state) => ({
+      runs: state.runs ? setRunStatus(state.runs, runUuid, status) : state.runs,
+      page: state.page
+        ? {
+            ...state.page,
+            pipeline_runs: setRunStatus(
+              state.page.pipeline_runs,
+              runUuid,
+              status
+            ),
+          }
+        : state.page,
+    }));
+  };
+
   return {
-    runs: undefined,
     page: undefined,
+    runs: undefined,
     fetchOne: choke(async (runUuid) => {
       const newRun = await jobRunsApi.fetchOne(get().jobUuid, runUuid);
 
-      set({ runs: replaceOrAddRun(newRun) });
+      set((state) => ({ runs: replaceOrAddRun(state.runs, newRun) }));
     }),
-    fetchPage: choke(async (page, pageSize = 10) => {
-      set({
-        page: await jobRunsApi.fetchPage(get().jobUuid, { page, pageSize }),
-      });
+    fetchPage: choke(async (query) => {
+      set({ page: await jobRunsApi.fetchPage(get().jobUuid, query) });
     }),
     fetchAll: choke(async () => {
-      set({
-        runs: await jobRunsApi.fetchAll(get().jobUuid),
-      });
+      set({ runs: await jobRunsApi.fetchAll(get().jobUuid) });
     }),
     cancel: async (runUuid) => {
       await jobRunsApi.cancel(get().jobUuid, runUuid);
 
-      setRunStatus(runUuid, "ABORTED");
+      updateStatus(runUuid, "ABORTED");
     },
     setStatus: async (update) => {
       await jobRunsApi.setStatus(update);
 
-      setRunStatus(update.runUuid, update.status);
-    },
-    setStepStatus: async (update) => {
-      await jobRunsApi.setStepStatus(update);
-
-      set((state) => ({
-        runs: produce(state.runs || [], (draft) => {
-          const step = draft
-            .find((run) => run.uuid === update.runUuid)
-            ?.pipeline_steps.find((step) => step.step_uuid === update.stepUuid);
-
-          if (step) step.status = update.status;
-        }),
-      }));
+      updateStatus(update.runUuid, update.status);
     },
   };
 });
