aa0cdd072d86144e388c0ad1d35af3c1fd3efa92
Yannick <26223174+yannickperrenet@users.noreply.github.com>
2022-09-13 16:02:17 +0200

    Allow silencing `echo` output in CLI
    Invoke like: `SILENCE_OUTPUT=true orchest version`
    
    Useful in case you are using the CLI in CI pipelines.

--- orchest-cli/orchestcli/cmds.py
+++ orchest-cli/orchestcli/cmds.py
@@ -41,13 +41,16 @@ def echo(*args, **kwargs) -> None:
         the `--json` flag.
 
     """
+    if os.getenv("SILENCE_OUTPUT", "false") == "true":
+        return
+
     click_ctx = click.get_current_context(silent=True)
 
     if click_ctx is None:
         return click.echo(*args, **kwargs)
 
     json_flag = click_ctx.params.get("json_flag")
-    if json_flag and json_flag is not None:
+    if json_flag is not None and json_flag:
         return
     else:
         return click.echo(*args, **kwargs)
