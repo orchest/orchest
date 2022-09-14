12989131be4f39eed5ec96f7010999b4d2567523
Huang-Ming Chang <huang@orchest.io>
2022-09-14 15:41:58 +0200

    fix: no environment image flashing issue.

--- services/orchest-webserver/client/src/environments-view/edit-environment/EditEnvironmentContainer.tsx
+++ services/orchest-webserver/client/src/environments-view/edit-environment/EditEnvironmentContainer.tsx
@@ -1,5 +1,6 @@
 import { useEnvironmentsApi } from "@/api/environments/useEnvironmentsApi";
 import Stack from "@mui/material/Stack";
+import { hasValue } from "@orchest/lib-utils";
 import React from "react";
 import { useSaveEnvironmentChanges } from "../hooks/useSaveEnvironmentChanges";
 import { useUpdateBuildStatusInEnvironmentChanges } from "../hooks/useUpdateBuildStatusInEnvironmentChanges";
@@ -19,10 +20,11 @@ export const EditEnvironmentContainer = ({
   useSaveEnvironmentChanges();
   useUpdateBuildStatusInEnvironmentChanges();
 
-  const hasNoEnvironment = useEnvironmentsApi(
-    (state) => state.environments?.length === 0
+  const hasNoEnvironment = useEnvironmentsApi((state) =>
+    !hasValue(state.environments) ? undefined : state.environments.length === 0
   );
 
+  if (!hasValue(hasNoEnvironment)) return null;
   return hasNoEnvironment ? (
     <NoEnvironment />
   ) : (
