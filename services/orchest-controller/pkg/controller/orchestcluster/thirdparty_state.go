8bd4ff30ed365239e79586cee2e5a4dbdc962cab
Navid Haghighat <navid.haghighat@gmail.com>
2022-09-16 18:11:50 +0200

    consolidate thirdparty state

--- services/orchest-controller/pkg/controller/orchestcluster/thirdparty_state.go
+++ services/orchest-controller/pkg/controller/orchestcluster/thirdparty_state.go
@@ -3,8 +3,9 @@ package orchestcluster
 import (
 	"context"
 
+	"github.com/orchest/orchest/services/orchest-controller/pkg/addons"
 	orchestv1alpha1 "github.com/orchest/orchest/services/orchest-controller/pkg/apis/orchest/v1alpha1"
-	"github.com/orchest/orchest/services/orchest-controller/pkg/controller"
+	"github.com/orchest/orchest/services/orchest-controller/pkg/utils"
 )
 
 type DeployThirdPartyState struct{}
@@ -19,20 +20,21 @@ func (state *DeployThirdPartyState) To(ctx context.Context, stateMachine *Orches
 
 func (state *DeployThirdPartyState) Do(ctx context.Context, stateMachine *OrchestStateMachine, orchest *orchestv1alpha1.OrchestCluster) error {
 
+	deployedApps := 0
 	for _, application := range orchest.Spec.Applications {
 
-		if ok := stateMachine.containsCondition(controller.GetDeployingEvent(application.Name)); !ok {
-			stateMachine.Deploy(ctx, application.Name, &application)
-		}
-
-		/*
-			err = occ.addonManager.Get(application.Name).Enable(ctx, preInstallHooks, orchest.Namespace, &application)
+		if ok := stateMachine.containsCondition(addons.LogEvent(utils.GetDeployingEvent(application.Name))); !ok {
+			err := stateMachine.Deploy(ctx, application.Name, &application)
 			if err != nil {
-				klog.Error(err)
 				return err
 			}
-		*/
+		} else if stateMachine.containsCondition(addons.LogEvent(utils.GetDeployedEvent(application.Name))) {
+			deployedApps++
+		}
+	}
 
+	if deployedApps == len(orchest.Spec.Applications) {
+		stateMachine.toState(ctx, orchestv1alpha1.DeployingOrchest)
 	}
 	return nil
 }
