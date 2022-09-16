e1d8b4f467e000a8896e20a8ff599d04f5205ae3
Navid Haghighat <navid.haghighat@gmail.com>
2022-09-16 18:15:39 +0200

    use channel in component

--- services/orchest-controller/pkg/addons/components.go
+++ services/orchest-controller/pkg/addons/components.go
@@ -28,18 +28,34 @@ func RegisterAddon(name string, addon Addon) {
 
 type Message any
 
+type Event interface {
+	String() string
+}
+
+type LogEvent (string)
+
+func (event LogEvent) String() string {
+	return string(event)
+}
+
+type ErrorEvent (string)
+
+func (event ErrorEvent) String() string {
+	return string(event)
+}
+
 type Addon interface {
 	// Updates the deployed component, install if not deployed yet
-	Update(ctx context.Context, namespace string, message Message) error
+	Update(ctx context.Context, namespace string, message Message, eventChan chan Event)
 
 	// Stops the deployed component
-	Stop(ctx context.Context, namespace string, message Message) error
+	Stop(ctx context.Context, namespace string, message Message, eventChan chan Event)
 
 	// Starts the stopped component
-	Start(ctx context.Context, namespace string, message Message) error
+	Start(ctx context.Context, namespace string, message Message, eventChan chan Event)
 
 	// Deletes the deployed component.
-	Delete(ctx context.Context, namespace string, message Message) error
+	Delete(ctx context.Context, namespace string, message Message, eventChan chan Event)
 }
 
 type AddonRegistry struct {
@@ -62,15 +78,16 @@ func InitThirdPartyAddons(client kubernetes.Interface, config AddonsConfig) {
 
 }
 
-func (registery *AddonRegistry) Deploy(ctx context.Context, name, namespace string, message Message) error {
+func (registery *AddonRegistry) Deploy(ctx context.Context, name, namespace string,
+	message Message, eventChan chan Event) {
 	addon, ok := registery.addons[name]
 
 	if !ok {
 		err := errors.New(fmt.Sprintf("Component %s is not registered with the component registry", name))
 		klog.Error(err)
-		return err
+		eventChan <- ErrorEvent(err.Error())
+		return
 	}
 
-	addon.Update(ctx, namespace, message)
-	return nil
+	addon.Update(ctx, namespace, message, eventChan)
 }
