d5f930e53dbbaba818d80977b9a26093b0259259
Huang-Ming Chang <huang@orchest.io>
2022-09-18 20:47:56 +0200

    fix: documentation for useAutoFocusStepName

--- services/orchest-webserver/client/src/pipeline-view/step-details/store/useAutoFocusStepName.tsx
+++ services/orchest-webserver/client/src/pipeline-view/step-details/store/useAutoFocusStepName.tsx
@@ -21,6 +21,15 @@ export const useSetShouldAutoFocusStepName = () => {
   return { setShouldAutoFocusStepName };
 };
 
+/**
+ * Step name field in StepDetails should be auto-focused ONLY when creating a new step.
+ * Otherwise, deleting a step becomes cumbersome, that is, user needs to
+ * 1. Click the step
+ * 2. Click somewhere in StepDetails, to remove the focus on the Step Name field.
+ * 3. Press "Delete" to delete the step.
+ * Normally, we would autofocus the first field in a form to enhance UX, but not for this specific case.
+ * Pulling this state out to its own store, so that  we won't accidentally change it back to always auto-focus.
+ */
 export const useAutoFocusStepName = () => {
   const shouldAutoFocusStepName = useShouldAutoFocusStepName(
     (state) => state.shouldAutoFocusStepName
