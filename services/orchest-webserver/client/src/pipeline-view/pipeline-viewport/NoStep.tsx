c646777c1cccc6cdd224adb4c8f2f42d88cb5e5b
mausworks <rasmuswennerstrom@gmail.com>
2022-09-19 19:48:21 +0200

    feat: new empty state with actions for NoStep

--- services/orchest-webserver/client/src/pipeline-view/pipeline-viewport/NoStep.tsx
+++ services/orchest-webserver/client/src/pipeline-view/pipeline-viewport/NoStep.tsx
@@ -1,16 +1,70 @@
-import { StackProps } from "@mui/material/Stack";
+import { UploadFilesForm } from "@/components/UploadFilesForm";
+import { useCancelableFetch } from "@/hooks/useCancelablePromise";
+import { useUploader } from "@/hooks/useUploader";
+import { NoteAddOutlined, UploadFileOutlined } from "@mui/icons-material";
+import Button from "@mui/material/Button";
 import React from "react";
+import { usePipelineDataContext } from "../contexts/PipelineDataContext";
+import { lastSelectedFolderPath } from "../file-manager/common";
+import { CreateFileDialog } from "../file-manager/CreateFileDialog";
+import { useFileManagerContext } from "../file-manager/FileManagerContext";
+import { useCreateStep } from "../hooks/useCreateStep";
 import { ViewportCenterMessage } from "./components/ViewportCenterMessage";
 
-export const NoStep = (props: StackProps) => {
+export const NoStep = () => {
+  const { selectedFiles, fetchFileTrees } = useFileManagerContext();
+  const { isReadOnly, pipeline, projectUuid } = usePipelineDataContext();
+  const createStep = useCreateStep();
+  const [isFileDialogOpen, setIsFileDialogOpen] = React.useState(false);
+  const { cancelableFetch } = useCancelableFetch();
+  const uploader = useUploader({
+    projectUuid,
+    root: "/project-dir",
+    fetch: cancelableFetch,
+  });
+
+  const handleFileUpload = (files: FileList | File[]) =>
+    uploader
+      .uploadFiles(lastSelectedFolderPath(selectedFiles), files)
+      .then(() => fetchFileTrees());
+
   return (
-    <ViewportCenterMessage
-      imgSrc="/image/no-step.svg"
-      title="No Pipeline Steps"
-      description={`A Pipeline Step is an executable file running in its own isolated 
-      environment. Drag & drop files from the file manager to get started.`}
-      docPath="/fundamentals/pipelines.html"
-      {...props}
-    />
+    <>
+      <ViewportCenterMessage
+        imgSrc="/image/files.svg"
+        title="No scripts or notebooks"
+        description={`There are no files in this Project yet. Create or upload a file to get started.`}
+        actions={
+          <>
+            <Button
+              onClick={() => setIsFileDialogOpen(true)}
+              startIcon={<NoteAddOutlined />}
+              variant="contained"
+            >
+              New file
+            </Button>
+            <UploadFilesForm multiple upload={handleFileUpload}>
+              {(onClick) => (
+                <Button onClick={onClick} startIcon={<UploadFileOutlined />}>
+                  Upload files
+                </Button>
+              )}
+            </UploadFilesForm>
+          </>
+        }
+      />
+      <CreateFileDialog
+        isOpen={!isReadOnly && isFileDialogOpen}
+        canCreateStep={Boolean(pipeline)}
+        root="/project-dir"
+        onClose={() => setIsFileDialogOpen(false)}
+        onSuccess={(file) => {
+          if (file.shouldCreateStep) {
+            createStep(file.projectPath);
+          }
+          fetchFileTrees();
+        }}
+      />
+    </>
   );
 };
