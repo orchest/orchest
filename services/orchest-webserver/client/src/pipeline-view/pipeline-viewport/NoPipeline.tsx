07ba4d88522453fcfaca6fb5282132f776f2d56b
mausworks <rasmuswennerstrom@gmail.com>
2022-09-20 11:49:12 +0200

    feat: add empty state actions to pipeline

--- services/orchest-webserver/client/src/pipeline-view/pipeline-viewport/NoPipeline.tsx
+++ services/orchest-webserver/client/src/pipeline-view/pipeline-viewport/NoPipeline.tsx
@@ -1,15 +1,31 @@
-import { StackProps } from "@mui/material/Stack";
+import AddOutlined from "@mui/icons-material/AddOutlined";
+import Button from "@mui/material/Button";
 import React from "react";
+import { CreatePipelineDialog } from "../CreatePipelineDialog";
 import { ViewportCenterMessage } from "./components/ViewportCenterMessage";
 
-export const NoPipeline = (props: StackProps) => {
+export const NoPipeline = () => {
   return (
     <ViewportCenterMessage
       imgSrc="/image/no-pipeline.svg"
       title="No Pipelines in Project"
       description="Pipelines are an interactive tool for creating and experimenting with
     your data workflow. They are made up of Steps and connections."
-      {...props}
+      actions={
+        <CreatePipelineDialog>
+          {(createPipeline) => (
+            <>
+              <Button
+                startIcon={<AddOutlined />}
+                variant="contained"
+                onClick={createPipeline}
+              >
+                New pipeline
+              </Button>
+            </>
+          )}
+        </CreatePipelineDialog>
+      }
     />
   );
 };
