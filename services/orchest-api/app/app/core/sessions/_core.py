43184f3a09c42867e0dccc31cb3d32a093f8deaf
gobbijacopo91@gmail.com <gobbijacopo91@gmail.com>
2022-09-29 14:20:39 +0200

    Warn when failing to query kernels for idle check

--- services/orchest-api/app/app/core/sessions/_core.py
+++ services/orchest-api/app/app/core/sessions/_core.py
@@ -336,7 +336,12 @@ def has_busy_kernels(session_uuid: str) -> bool:
     try:
         response = session.get(url, timeout=3.0)
     # Might fail under heavy load.
-    except (requests.ConnectionError, requests.Timeout, requests.HTTPError) as e:
+    except (
+        requests.ConnectionError,
+        requests.Timeout,
+        requests.HTTPError,
+        requests.exceptions.RetryError,
+    ) as e:
         logger.warning(f"Failed to query kernels: {e}. Will be considered as busy.")
         return True
 
