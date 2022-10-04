c1a293e2191bdae8008387bcf940235c0b279fc6
Navid Haghighat <navid.haghighat@gmail.com>
2022-10-04 17:31:21 +0200

    finalize deployment state

--- services/orchest-controller/pkg/controller/orchestcluster/deploy_state.go
+++ services/orchest-controller/pkg/controller/orchestcluster/deploy_state.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	orchestv1alpha1 "github.com/orchest/orchest/services/orchest-controller/pkg/apis/orchest/v1alpha1"
+	"github.com/orchest/orchest/services/orchest-controller/pkg/utils"
 )
 
 type DeployOrchestState struct{}
@@ -17,5 +18,33 @@ func (state *DeployOrchestState) To(ctx context.Context, stateMachine *OrchestSt
 }
 
 func (state *DeployOrchestState) Do(ctx context.Context, stateMachine *OrchestStateMachine, orchest *orchestv1alpha1.OrchestCluster) error {
+
+	for _, stage := range deploymentStages {
+
+		deployedApps := 0
+		for _, component := range stage {
+			deployingEvent := utils.GetDeployingEvent(component)
+
+			if ok := stateMachine.containsCondition(deployingEvent); !ok {
+				template, err := GetComponentTemplate(component, orchest)
+				if err != nil {
+					return err
+				}
+				err = stateMachine.Deploy(ctx, component, deployingEvent, deployTimeOut, deployRetry, template)
+				if err != nil {
+					return err
+				}
+			}
+
+			if stateMachine.containsCondition(utils.GetDeployedEvent(component)) {
+				deployedApps++
+			}
+		}
+
+		// All components of this stage are not deployed yet, so we can not move to the next stage,
+		if deployedApps != len(stage) {
+			break
+		}
+	}
 	return nil
 }
