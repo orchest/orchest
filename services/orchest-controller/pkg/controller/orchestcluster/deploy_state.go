5293663786a4516699bb65cf970f084bd998df51
Navid Haghighat <navid.haghighat@gmail.com>
2022-10-16 17:43:06 +0200

    make deploy state more robust and use expectation

--- services/orchest-controller/pkg/controller/orchestcluster/deploy_state.go
+++ services/orchest-controller/pkg/controller/orchestcluster/deploy_state.go
@@ -6,7 +6,6 @@ import (
 
 	orchestv1alpha1 "github.com/orchest/orchest/services/orchest-controller/pkg/apis/orchest/v1alpha1"
 	"github.com/orchest/orchest/services/orchest-controller/pkg/controller"
-	"github.com/orchest/orchest/services/orchest-controller/pkg/utils"
 )
 
 type DeployOrchestState struct{}
@@ -34,26 +33,23 @@ func (state *DeployOrchestState) Do(ctx context.Context, stateMachine *OrchestSt
 	generation := fmt.Sprint(orchest.Generation)
 
 	// First we make sure resources are created
-	// {controller.Resources},
-	creatingEvent := utils.GetCreatingEvent(controller.Resources)
-	if ok := stateMachine.containsCondition(creatingEvent); !ok {
-		err := stateMachine.Create(ctx, controller.Resources, creatingEvent, deployTimeOut, deployRetry, orchest)
-		if err != nil {
-			return err
-		}
+	if !stateMachine.expectCreation(controller.Resources) {
+		err := stateMachine.Create(ctx, controller.Resources, deployTimeOut, deployRetry, orchest)
+		return err
 	}
-	// if resources are not created yet, we move on
-	if !stateMachine.containsCondition(utils.GetCreatedEvent(controller.Resources)) {
+
+	if !stateMachine.isCreated(controller.Resources) {
 		return nil
 	}
 
 	for i, stage = range creationStages {
 
-		deployedApps := 0
+		createdApps := 0
 		for _, componentName := range stage {
-			creatingEvent := utils.GetCreatingEvent(componentName)
 
-			if ok := stateMachine.containsCondition(creatingEvent); !ok {
+			if stateMachine.isCreated(componentName) {
+				createdApps++
+			} else if !stateMachine.expectCreation(componentName) {
 				template, err := GetComponentTemplate(componentName, orchest)
 				if err != nil {
 					return err
@@ -61,19 +57,16 @@ func (state *DeployOrchestState) Do(ctx context.Context, stateMachine *OrchestSt
 
 				component := getOrchestComponent(componentName, generation, template, orchest)
 
-				err = stateMachine.Create(ctx, componentName, creatingEvent, deployTimeOut, deployRetry, component)
+				err = stateMachine.Create(ctx, componentName, deployTimeOut, deployRetry, component)
 				if err != nil {
 					return err
 				}
 			}
 
-			if stateMachine.containsCondition(utils.GetCreatedEvent(componentName)) {
-				deployedApps++
-			}
 		}
 
-		// All components of this stage are not deployed yet, so we can not move to the next stage,
-		if deployedApps != len(stage) {
+		// All components of this stage are not created yet, so we can not move to the next stage,
+		if createdApps != len(stage) {
 			return nil
 		}
 	}
