931593c2f8b30a7a0dcd0c9bbec31a2233670de0
mausworks <rasmuswennerstrom@gmail.com>
2022-10-31 13:04:57 +0100

    fix: ensure that the correct overrides are used
    Eg. Only load job run files when a pipeline UUID is available

--- services/orchest-webserver/client/src/hooks/useFetchFileRoots.ts
+++ services/orchest-webserver/client/src/hooks/useFetchFileRoots.ts
@@ -1,21 +1,38 @@
 import { FileApiOverrides, useFileApi } from "@/api/files/useFileApi";
 import React from "react";
+import { useScopeParameters } from "./useScopeParameters";
 
 export const useFetchFileRoots = ({
   pipelineUuid,
   jobUuid,
   runUuid,
 }: FileApiOverrides = {}) => {
+  const scope = useScopeParameters();
   const roots = useFileApi((api) => api.roots);
   const init = useFileApi((api) => api.init);
 
-  React.useEffect(() => {
-    if (Object.keys(roots).length === 0) init();
-  }, [init, roots]);
+  React.useEffect(
+    function reload() {
+      const hasPipeline = Boolean(pipelineUuid ?? scope.pipelineUuid);
 
-  React.useEffect(() => {
-    init(2, { pipelineUuid, jobUuid, runUuid });
-  }, [init, pipelineUuid, jobUuid, runUuid]);
+      const overrides = {
+        pipelineUuid: pipelineUuid ?? scope.pipelineUuid,
+        jobUuid: hasPipeline ? jobUuid ?? scope.jobUuid : undefined,
+        runUuid: hasPipeline ? runUuid ?? scope.runUuid : undefined,
+      };
+
+      init(2, overrides);
+    },
+    [
+      init,
+      scope.jobUuid,
+      scope.runUuid,
+      scope.pipelineUuid,
+      pipelineUuid,
+      jobUuid,
+      runUuid,
+    ]
+  );
 
   return roots;
 };
