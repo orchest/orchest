dbc314dfc6e4dafeaeafbef112ca441330f861ff
mausworks <rasmuswennerstrom@gmail.com>
2022-11-01 13:48:03 +0100

    fix: some bad documentation

--- services/orchest-webserver/client/src/pipeline-view/file-manager/useCreateFile.tsx
+++ services/orchest-webserver/client/src/pipeline-view/file-manager/useCreateFile.tsx
@@ -2,17 +2,12 @@ import { useFileApi } from "@/api/files/useFileApi";
 import { combinePath, FileRoot } from "@/utils/file";
 import React from "react";
 
-/**
- * Creates a file and returns the absolute path of it.
- *
- * If a `projectUuid` is not found in the current route,
- * this function will throw an error.
- */
+/** Creates a file and returns the absolute path of it. */
 type FileCreator = (path: string) => Promise<string>;
 
 /**
  * Returns a function which creates a file relative to the provided root.
- * The returned path will be an absolute project path, starting with `project-dir:/` or `data:/`.
+ * The returned path will be a combined project path, starting with `project-dir:/` or `data:/`.
  */
 export const useCreateFile = (root: FileRoot): FileCreator => {
   const create = useFileApi((api) => api.create);
