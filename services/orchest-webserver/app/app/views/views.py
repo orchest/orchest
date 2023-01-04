d7f06957c3fb71991e54b4649e1a53690f80ef82
Huang-Ming Chang <huang.m.chang@gmail.com>
2023-01-04 15:09:28 +0100

    fix: remove the extra slash

--- services/orchest-webserver/app/app/views/views.py
+++ services/orchest-webserver/app/app/views/views.py
@@ -384,10 +384,7 @@ def register_views(app, db):
     def git_import_post():
         """See the orchest-api swagger docs for git-import. #CLOUD"""
         resp = requests.post(
-            (
-                f"http://{current_app.config['ORCHEST_API_ADDRESS']}/api/"
-                "/git-imports/"
-            ),
+            (f"http://{current_app.config['ORCHEST_API_ADDRESS']}/api" "/git-imports/"),
             json=request.get_json(),
         )
         return resp.content, resp.status_code, resp.headers.items()
