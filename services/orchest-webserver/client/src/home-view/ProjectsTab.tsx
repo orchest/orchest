ebd91e9b483887c2e170b63cbf18b98391812da4
mausworks <rasmuswennerstrom@gmail.com>
2023-01-04 11:49:05 +0100

    feat: display snackbar if project fetching fails

--- services/orchest-webserver/client/src/home-view/ProjectsTab.tsx
+++ services/orchest-webserver/client/src/home-view/ProjectsTab.tsx
@@ -1,14 +1,32 @@
+import { SnackBar } from "@/components/common/SnackBar";
 import { useFetchProjects } from "@/hooks/useFetchProjects";
+import Button from "@mui/material/Button";
+import blue from "@mui/material/colors/blue";
+import { hasValue } from "@orchest/lib-utils";
 import React from "react";
 import { ProjectsEmptyState } from "./components/ProjectsEmptyState";
 import { ProjectTable } from "./components/ProjectTable";
 
 export const ProjectsTab = () => {
-  const { isFetched, isEmpty } = useFetchProjects();
+  const { isFetched, isFetching, isEmpty, error, refresh } = useFetchProjects();
 
-  if (isFetched && isEmpty) {
-    return <ProjectsEmptyState />;
-  } else {
-    return <ProjectTable />;
-  }
+  return (
+    <>
+      {isFetched && isEmpty && <ProjectsEmptyState />}
+      {!isEmpty && <ProjectTable />}
+      <SnackBar
+        open={hasValue(error)}
+        message="Failed to fetch projects"
+        action={
+          <Button
+            sx={{ color: blue[200] }}
+            onClick={() => refresh()}
+            disabled={isFetching}
+          >
+            Retry
+          </Button>
+        }
+      />
+    </>
+  );
 };
