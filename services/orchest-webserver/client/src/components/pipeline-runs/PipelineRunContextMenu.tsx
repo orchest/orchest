6f33f073e4d340e5700b772e2a71f076315d4415
mausworks <rasmuswennerstrom@gmail.com>
2023-01-16 10:21:29 +0100

    feat: add "open" to run context menu

--- services/orchest-webserver/client/src/components/pipeline-runs/PipelineRunContextMenu.tsx
+++ services/orchest-webserver/client/src/components/pipeline-runs/PipelineRunContextMenu.tsx
@@ -1,6 +1,7 @@
 import { useCancelPipelineRun } from "@/hooks/useCancelPipelineRun";
+import { useNavigate } from "@/hooks/useCustomRoute";
 import { PipelineRun } from "@/types";
-import { canCancelRun } from "@/utils/pipeline-run";
+import { canCancelRun, isJobRun } from "@/utils/pipeline-run";
 import Menu, { MenuProps } from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import React from "react";
@@ -14,6 +15,7 @@ export const PipelineRunContextMenu = ({
   ...menuProps
 }: PipelineRunContextMenuProps) => {
   const cancelRun = useCancelPipelineRun(run);
+  const navigate = useNavigate();
 
   const onCancelRun = () => {
     if (canCancelRun(run)) cancelRun();
@@ -24,13 +26,32 @@ export const PipelineRunContextMenu = ({
     menuProps.onClose?.({}, "escapeKeyDown");
   };
 
+  const jobRun = isJobRun(run);
+
+  const onClickOpen = (event: React.MouseEvent) => {
+    navigate({
+      route: jobRun ? "jobRun" : "pipeline",
+      query: {
+        projectUuid: run.project_uuid,
+        pipelineUuid: run.pipeline_uuid,
+        jobUuid: jobRun ? run.job_uuid : undefined,
+        runUuid: jobRun ? run.uuid : undefined,
+      },
+      sticky: false,
+      event,
+    });
+  };
+
   return (
     <Menu
-      id="pipeline-settings-menu"
+      data-test-id="pipeline-run-settings-menu"
       anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
       transformOrigin={{ vertical: "top", horizontal: "right" }}
       {...menuProps}
     >
+      <MenuItem onClick={onClickOpen}>
+        {jobRun ? "Open Job Run" : "Open Pipeline"}
+      </MenuItem>
       <MenuItem
         onClick={() => closeAfter(onCancelRun)}
         disabled={!canCancelRun(run)}
