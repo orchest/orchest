5f3d7343419e56bb8a1f0e494c6a0c14ca0fb520
mausworks <rasmuswennerstrom@gmail.com>
2023-01-16 14:09:06 +0100

    feat: implement job runs API in all runs

--- services/orchest-webserver/client/src/home-view/AllRunsTab.tsx
+++ services/orchest-webserver/client/src/home-view/AllRunsTab.tsx
@@ -7,6 +7,8 @@ import { PipelineRunsTable } from "@/components/pipeline-runs/PipelineRunsTable"
 import { useFetchJobs } from "@/hooks/useFetchJobs";
 import { useFetchPipelines } from "@/hooks/useFetchPipelines";
 import { usePollRunningPipelineRuns } from "@/hooks/usePollRunningPipelineRuns";
+import { useJobRunsPage } from "@/jobs-view/job-view/hooks/useJobRunsPage";
+import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import React from "react";
@@ -19,12 +21,14 @@ export const AllRunsTab = () => {
 
   const [filter, setFilter] = React.useState<RunFilterState>(DEFAULT_FILTER);
   const interactiveRuns = usePollRunningPipelineRuns();
+  const [page, setPage] = React.useState<number>(1);
+  const { runs: jobRuns, pagination } = useJobRunsFromFilter(filter, page);
 
   return (
-    <Stack spacing={2}>
+    <Stack spacing={2} minHeight={625} width="100%">
       <RunFilters onChange={setFilter} />
 
-      <Accordion>
+      <Accordion defaultExpanded={true}>
         <AccordionSummary>
           <Typography color="text.secondary" variant="subtitle1">
             Interactive runs
@@ -38,6 +42,49 @@ export const AllRunsTab = () => {
           />
         </AccordionDetails>
       </Accordion>
+
+      <Accordion defaultExpanded={true}>
+        <AccordionSummary>
+          <Typography color="text.secondary" variant="subtitle1">
+            Job runs
+          </Typography>
+        </AccordionSummary>
+
+        <AccordionDetails>
+          <Stack minHeight="390px">
+            <PipelineRunsTable breadcrumbs runs={jobRuns ?? []} />
+          </Stack>
+
+          <Stack alignItems="center" marginTop={2}>
+            {pagination?.total_pages && (
+              <Pagination
+                color="primary"
+                count={pagination.total_pages}
+                page={page}
+                onChange={(_, pageNumber) => setPage(pageNumber)}
+              />
+            )}
+          </Stack>
+        </AccordionDetails>
+      </Accordion>
     </Stack>
   );
+};
+
+const useJobRunsFromFilter = (filter: RunFilterState, page: number) => {
+  const query = React.useMemo(() => {
+    return {
+      page,
+      pageSize: 5,
+      projectUuids: filter.projects.length
+        ? filter.projects.map((project) => project.uuid)
+        : undefined,
+      pipelineUuids: filter.pipelines.length
+        ? filter.pipelines.map((pipeline) => pipeline.uuid)
+        : undefined,
+      statuses: filter.statuses.length ? filter.statuses : undefined,
+    };
+  }, [filter.pipelines, filter.projects, filter.statuses, page]);
+
+  return useJobRunsPage(query);
 };
