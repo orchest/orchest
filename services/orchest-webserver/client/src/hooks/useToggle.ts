16a046a0edadefeb0d7c4541f94dc7a7f1fa9d9e
mausworks <rasmuswennerstrom@gmail.com>
2023-01-17 04:55:05 +0100

    fix: wrap `newState` in `hasValue`

--- services/orchest-webserver/client/src/hooks/useToggle.ts
+++ services/orchest-webserver/client/src/hooks/useToggle.ts
@@ -1,10 +1,11 @@
+import { hasValue } from "@orchest/lib-utils";
 import React from "react";
 
 export const useToggle = (initialState = false) => {
   const [toggled, setToggled] = React.useState(initialState);
 
   const toggle = React.useCallback((newState?: boolean) => {
-    if (newState) setToggled(newState);
+    if (hasValue(newState)) setToggled(newState);
     else setToggled((current) => !current);
   }, []);
 
